d40bb1b8828c88b136efac6ddb9f2a3fe4308859
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-04-03 01:04:00 +0200

    initial commit. wingolfsplattform via ruby and rails. sebastian fiedlschuster, 2012-04-02.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wingolfsplattform::Application.config.secret_token = 'eb2c4eaf247e9e9b0c5091653212f90f62c50dbec92c6411a982c3a7bb9a7e140c3e5d3953fbbf442decf10bde805c06e1eddb58c7052ed69451e448a53a4fd7'
