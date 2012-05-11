8ef2a1697930ca18c0bf6260305510fe90760074
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-05-11 21:04:49 +0200

    Pages mit best_in_place versehen.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,23 @@
 class PagesController < ApplicationController
+
+  before_filter :find_page
+  respond_to :html, :json
+
   def show
-    @page = Page.find_by_id( params[ :id ] ) if params[ :id ]
     if @page
       @title = @page.title
       @navable = @page
     end
+  end
+
+  def update
+    @page.update_attributes params[ :page ]
+    respond_with @page
+  end
+
+  private
+
+  def find_page
+    @page = Page.find params[ :id ]
   end
 end
