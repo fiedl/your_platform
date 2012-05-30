472e038d9ab2c10be0f16cfb0a5bab471da41e4c
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-05-30 18:52:47 +0200

    Temporär: HTTP-Authentication erforderlich, solange das System im Test-Zustand ist.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter      :http_authenticate
   before_filter      :initialize_session
 
   helper_method      :logged_in?
 
   protected
+
+  def http_authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "aki" && password == "deleted-string"
+    end
+  end
 
   def initialize_session
     @session = Session.new( session, cookies, request )   # in controller/sessions_controller.rb
