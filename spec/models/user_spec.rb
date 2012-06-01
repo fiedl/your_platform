4039795ffd42d80a83445aa8ebf6fa6f02c05cf6
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-06-01 23:34:07 +0200

    rspec funktioniert.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,21 @@
 require 'spec_helper'
 
 describe User do
-#  
-#  before do
-#    @user = User.new( first_name: "Max", last_name: "Mustermann", alias: "m.mustermann", 
-#                      email: "max.mustermann@example.com", create_account: true )
-#  end
-#
-#  subject { @user }
-#
-#  it { should respond_to( :first_name ) }
-#  it { should respond_to( :last_name ) }
-#  it { should respond_to( :name ) }
-#  it { should respond_to( :alias ) }
-#  it { should respond_to( :email ) }
-#  it { should respond_to( :create_account) }
-#
-#  it { should be_valid }
-#
+  
+  before do
+    @user = User.new( first_name: "Max", last_name: "Mustermann", alias: "m.mustermann",
+                      email: "max.mustermann@example.com", create_account: true )
+  end
+
+  subject { @user }
+
+  it { should respond_to( :first_name ) }
+  it { should respond_to( :last_name ) }
+  it { should respond_to( :name ) }
+  it { should respond_to( :alias ) }
+  it { should respond_to( :email ) }
+  it { should respond_to( :create_account) }
+
+  it { should be_valid }
+
 end
