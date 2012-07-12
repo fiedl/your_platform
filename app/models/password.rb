fdd75014da3aaf2673610805f25bfd60d4fdda17
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-07-12 21:27:21 +0200

    Password class. (See previous commit, forgot to add the modified file.)

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
-class UserPassword < UserPropertyString
+
+class Password < String
+
+  # This simply creates a new password using `pwgen`.
+  # Example: 
+  #     new_password = Password.generate
   def self.generate
-    UserPassword.new( `pwgen -N 1 -n -c -B`.to_s[0..-2], :user => @user )
+    Password.new( `pwgen -N 1 -n -c -B`.to_s[0..-2] )
   end
   
+  # Example:
+  #    new_password = Password.new
+  #    new_password.generate!
   def generate!
     replace self.class.generate
   end
+
 end
 
 
