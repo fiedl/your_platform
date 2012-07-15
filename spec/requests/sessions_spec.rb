1cefa2eb324021d813f65bbb7bdb23c2248f5c20
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-07-15 16:12:52 +0200

    basic session specs

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Sessions" do
-  describe "New Session Page", js: true do
+  describe "New Session Page" do  # to show the browser while testing, e.g. use `js: true`.
     
     before do
       visit new_session_path
@@ -12,6 +12,22 @@ describe "Sessions" do
     describe "Form elements" do
       it { should have_field( 'login_name' ) }
       it { should have_field( 'password' ) }
+    end
+
+    it "should allow to create a new session" do
+      
+      @user = User.create( first_name: "John", last_name: "Doe", email: "j.doe@example.com", :alias => "j.doe" )
+      @user.create_account = true
+      @user.save
+      @password = @user.account.password
+
+      fill_in 'login_name', with: "John Doe"
+      fill_in 'password', with: @password
+      click_button I18n.t( "login" )
+      
+      page.should have_content( "John Doe" )
+      page.should have_content( I18n.t( :logout ) )
+
     end
 
 
