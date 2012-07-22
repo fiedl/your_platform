24650d5cb7a90bebd50fb53674497f88e45ca8f7
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-07-22 17:04:33 +0200

    rewriting the convert_to_content_box helper in order to not convert the first node after an h1 (issue fixing).

--- app/helpers/box_helper.rb
+++ app/helpers/box_helper.rb
@@ -1,27 +1,33 @@
+# -*- coding: utf-8 -*-
 module BoxHelper
 
-  def content_box( args, &block ) # :heading
-    content = with_output_buffer( &block )
-    html_code = render :partial => "shared/box", :locals => { :heading => args[:heading], :content => content }
-    return html_code
+  def content_box( options = {} )
+    heading = options[ :heading ]
+    content = options[ :content ]
+    content = yield unless content
+
+    render partial: 'shared/box', locals: { heading: heading, content: content }
   end
 
-  def convert_to_content_box ( &block )
-    html_code_to_convert = yield
-    if html_code_to_convert.start_with? "<h1>"
-      html_code_to_return = ""
-      doc = Nokogiri::HTML( html_code_to_convert ) 
-      # Node-HTML-Struktur, siehe http://stackoverflow.com/questions/3449767/find-and-replace-entire-html-nodes-with-nokogiri
-      # http://www.engineyard.com/blog/2010/getting-started-with-nokogiri/
-      doc.xpath('//h1').each do |h1_node|
-        heading = h1_node.inner_html.html_safe
-        content = h1_node.next_element.inner_html.html_safe #http://nokogiri.org/Nokogiri/XML/Node.html#method-i-next_element
-        html_code_to_return += content_box( :heading => heading ) { concat content }
-      end
-      return html_code_to_return.html_safe
-    else
-      return html_code_to_convert
-    end
+  def convert_to_content_box( html_code = nil )
+    html_code = yield unless html_code
+    html_convert_h1_to_boxes( html_code )
+  end
+
+  def html_convert_h1_to_boxes( html_code )
+    return html_code if not html_code.start_with? "<h1>"
+    
+    # Further Nokogiri Reference
+    # * http://stackoverflow.com/questions/3449767/find-and-replace-entire-html-nodes-with-nokogiri 
+    # * http://www.engineyard.com/blog/2010/getting-started-with-nokogiri/
+    # * http://nokogiri.org/Nokogiri/XML/Node.html#method-i-next_element
+    doc = Nokogiri::HTML( html_code )
+
+    doc.xpath( '//h1' ).collect do |h1_node|
+      heading = h1_node.inner_html.html_safe
+      content = h1_node.next_element.to_html.html_safe
+      content_box( heading: heading, content: content )
+    end.join.html_safe
   end
 
 end
