109f99c768bd8bd5e944d0c414e7648e360c3603
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-08-20 22:19:16 +0200

    replacing add-plus and remove-minus icons by twitter bootstrap buttons

--- app/helpers/tool_buttons_helper.rb
+++ app/helpers/tool_buttons_helper.rb
@@ -4,23 +4,26 @@ module ToolButtonsHelper
   def remove_button( object )
     title = t( :remove )
     title += ": " + object.title if object.respond_to? :title
-    link_to( image_tag( 'tools/remove.png', 
-                        alt: title, title: title
-                        ).html_safe,
+    link_to( content_tag( :i, "", :class => "icon-trash icon-white" ),
+         #    image_tag( 'tools/remove.png', 
+         #               alt: title, title: title
+         #               ).html_safe,
              object,
              method: 'delete',
-             :class => 'remove_button tool show_only_in_edit_mode'
+             :class => 'remove_button tool show_only_in_edit_mode btn btn-danger',
+             :title => title
            )
   end
 
   def add_button( url ) # TODO: Was braucht er für minimale Informationen?
     title = t( :add )
-    link_to(
-            image_tag( 'tools/add.png',
-                       alt: title, title: title
-                       ).html_safe,
+    link_to( content_tag( :i, "", :class => "icon-plus icon-white" ) +
+             title,
+        #    image_tag( 'tools/add.png',
+        #               alt: title, title: title
+        #               ).html_safe,
             url.to_s,
-            :class => 'add_button tool show_only_in_edit_mode'
+            :class => 'add_button tool show_only_in_edit_mode btn btn-success'
             )
   end
 
