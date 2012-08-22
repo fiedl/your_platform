7660c9e980193d7025e1054ef5b30943fce17e60
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-08-22 10:46:14 +0200

    deleting pending specs in your_platform for now

--- vendor/engines/your_platform/spec/helpers/attachments_helper_spec.rb
+++ vendor/engines/your_platform/spec/helpers/attachments_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe AttachmentsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#describe AttachmentsHelper do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
