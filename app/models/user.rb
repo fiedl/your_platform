b3ce8ec87e8389e4faf82401a209823808deaea7
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-09-02 13:03:40 +0200

    moving models user and profile_field to your_platform

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,118 +1,27 @@
 # -*- coding: utf-8 -*-
-class User < ActiveRecord::Base
 
-  attr_accessible           :first_name, :last_name, :name, :alias, :email, :create_account, :female, :add_to_group
+# This extends the your_platform User model.
+require_dependency YourPlatform::Engine.root.join( 'app/models/user' ).to_s
 
-  attr_accessor             :create_account, :name, :add_to_group
-                            # Boolean, der vormerkt, ob dem (neuen) Benutzer ein Account hinzugefügt werden soll.
+# This class represents a user of the platform. A user may or may not have an account.
+# While the most part of the user class is contained in the your_platform engine, 
+# this re-opened class contains all wingolf-specific additions to the user model.
+#
+class User
 
-  validates_presence_of     :first_name, :last_name
-  validates_uniqueness_of   :alias, :if => Proc.new { |user| ! user.alias.blank? }
-  validates_format_of       :email, :with => /^[a-z0-9_.-]+@[a-z0-9-]+\.[a-z.]+$/i, :if => Proc.new { |user| user.email }
-
-  has_profile_fields
-
-  has_one                   :account, class_name: "UserAccount", autosave: true, inverse_of: :user, dependent: :destroy
-  validates_associated      :account
-
-  is_structureable          ancestor_class_names: %w(Page Group), descendant_class_names: %w(Page)
-
-  has_dag_links             link_class_name: 'RelationshipDagLink', ancestor_class_names: %w(Relationship), descendant_class_names: %w(Relationship), prefix: 'relationships'
-
-  is_navable
-
-  before_save               :generate_alias_if_necessary, :capitalize_name, :write_alias_attribute
-  before_save               :build_account_if_requested, :add_to_group_if_requested
-
-  def name
-    first_name + " " + last_name
-  end
-  def name=( name )
-    name_components = name.split( " " )
-    if name_components.count > 1
-      self.first_name = name_components[ 0..-2 ].join( " " )
-      self.last_name = name_components[ -1 ]
-    end
-  end
-
-  # Diese Funktion gibt eine sinnvolle Beschriftung des Benutzers zurück, z.B. für die Beschriftung von Menüpunkten, 
-  # die diesen Benutzer repräsentieren. Damit ist der Aufruf der gleiche wie etwa beim Page-Modell. 
-  # <tt>@title = page.title</tt>, <tt>@title = user.title</tt>.
-  # Die Funktion gibt *nicht* den akademischen Titel oder die Anrede des Benutzers zurück.
+  # This method returns a kind of label for the user, e.g. for menu items representing the user.
+  # Use this rather than the name attribute itself, since the title method is likely to be overridden
+  # in the main application.
+  # Notice: This method does *not* return the academic title of the user.
+  # 
+  # Here, title returns the name and the aktivitaetszahl, e.g. "Max Mustermann E10 H12".
+  # 
   def title
     ( name + "  " + aktivitaetszahl ).strip
   end
-
-  def self.find_by_title( title )
-    User.all.select { |user| user.title == title }.first
-  end
-
-  def self.by_title( title )
-    User.find_by_title title
-  end
-
-  def alias
-    @alias = UserAlias.new( read_attribute( :alias ), :user => self ) unless @alias.kind_of? UserAlias
-    return @alias
-  end
-  def alias=( a )
-    @alias = a
-    write_alias_attribute
-  end
-
-  def capitalize_name
-    self.first_name.capitalize! unless first_name.include?( " " ) # zwei Vornamen
-    self.last_name.capitalize! unless last_name.include?( " " ) # "de Silva"
-    self.name
-  end
-
-  def has_account?
-    return true if self.account
-  end
   
-  def deactivate_account
-    self.account.destroy if self.account
-    self.account = nil
-  end
-
-  def relationships
-    relationships_parent_relationships + relationships_child_relationships
-  end
-
-  # Versucht, einen Benutzer anhand eines login_strings zu identifizieren, der beim Anmelden eingegeben wird.
-  # Das kann eine E-Mail-Adresse, ein Benutzername, Vor- und Zuname, etc. sein.
-  def self.identify( login_string )
-    UserIdentification.find_users login_string
-  end
-
-  def self.authenticate( login_string, password )
-    UserAccount.authenticate login_string, password 
-  end
-
-  def groups
-    self.ancestor_groups
-  end
-
-  def workflows
-    my_workflows = []
-    self.groups.each do |group|
-      my_workflows += group.child_workflows
-    end
-    return my_workflows
-  end
-
-  # Verbindungen (im Sinne des Wingolfs am Hochschulort), d.h. Bänder, die ein Mitglied trägt.
-  def corporations
-    my_corporations = []
-    if Group.wingolf_am_hochschulort
-      my_corporations += ( self.ancestor_groups & Group.wingolf_am_hochschulort.child_groups ).select do |wah|
-        ( wah.becomes( Wah ).aktivitas.descendant_users | wah.becomes( Wah ).philisterschaft.descendant_users ).include? self
-      end.collect { |group| group.becomes( Wah ) }
-    end
-    return my_corporations
-  end
-
-  # Der Bezirksverband, dem der Benutzer zugeordnet ist.
+  # This method returns the bv (Bezirksverband) the user is associated with.
+  #
   def bv
     if Bv.all and self.ancestor_groups
       bv_of_this_user = ( Bv.all & self.ancestor_groups ).first
@@ -120,72 +29,14 @@ class User < ActiveRecord::Base
     return bv_of_this_user.becomes Bv if bv_of_this_user
   end
 
+  # This method returns the aktivitaetszahl of the user, e.g. "E10 H12".
+  #
   def aktivitaetszahl
     self.corporations.collect do |corporation| 
       year_of_joining = ""
       year_of_joining = corporation.membership_of( self ).created_at.to_s[2, 2] if corporation.membership_of( self ).created_at
       corporation.token + "\u2009" + year_of_joining
     end.join( " " )
-  end
-
-  # Returns all UserGroupMemberships for this user. 
-  # If the option :with_deleted is set true, this includes all deleted UserGroupMemberships.
-  def memberships
-    UserGroupMembership.find_all_by_user self
-  end
-  
-  def inspect
-    "User: " + self.alias
-  end
-
-  def gender
-    return :female if female?
-    return :male
-  end
-  def gender=( new_gender )
-    if new_gender.to_s == "female"
-      self.female = true 
-    else
-      self.female = false
-    end
-  end
-
-  private
-
-  def write_alias_attribute
-    write_attribute :alias, @alias
-  end
-
-  def generate_alias_if_necessary
-    self.alias.generate! if self.alias.blank?
-  end
-
-  # If the attribute `create_account` is set to `true` or to `1`, e.g. by an html form,
-  # this code makes sure that the account association is build.
-  # This code is run on validation, as you can see above in this model.
-  # Note: A welcome email is automatically sent on save by the UserAccount model.
-  def build_account_if_requested
-
-    # If this value is set by an html form, it is "0" or "1". But "0" would 
-    # transform to true rather than to false.
-    # Thus, we have to make sure that "0" means false.
-    self.create_account = false if self.create_account == "0"
-
-    if self.create_account
-      self.account.destroy if self.has_account?
-      self.account = self.build_account
-      self.create_account = false # to make sure that this code is nut run twice.
-      return self.account
-    end
-
-  end
-
-  def add_to_group_if_requested
-    if self.add_to_group 
-      group = add_to_group if add_to_group.kind_of? Group
-      group = Group.find( add_to_group ) if add_to_group.to_i
-      UserGroupMembership.create( user: self, group: group ) if group
-    end
   end
 
 end
---
+++ vendor/engines/your_platform/app/models/profile_field.rb
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+class ProfileField < ActiveRecord::Base
+  
+  attr_accessible        :label, :type, :value
+  
+  belongs_to             :profileable, polymorphic: true
+
+  # Profilfelder können zusammengesetzt sein: 
+  acts_as_tree
+
+  def display_html
+    # Override this in the inheriting classes in ordner to modify the html output of the value.
+    self.value
+  end
+
+end
+
+class Custom < ProfileField
+  def self.model_name; ProfileField.model_name; end
+
+end
+
+class Organization < ProfileField
+  def self.model_name; ProfileField.model_name; end
+end
+
+class Email < ProfileField
+  def self.model_name; ProfileField.model_name; end
+
+  def display_html
+    ActionController::Base.helpers.mail_to self.value
+  end
+
+end
+
+class Address < ProfileField
+  def self.model_name; ProfileField.model_name; end
+
+  # Google Maps integration
+  # see: http://rubydoc.info/gems/gmaps4rails/
+  acts_as_gmappable 
+
+  def display_html
+    ActionController::Base.helpers.simple_format self.value
+  end
+
+  def gmaps4rails_address
+    self.value
+  end
+
+  def gmaps
+    true
+  end
+
+  # TODO: resolve redundancy with class AddressString
+
+
+  def latitude ;      geo_information :lat           end
+  def longitude ;     geo_information :lng           end
+  def country ;       geo_information :country       end
+  def country_code ;  geo_information :country_code  end
+  def city ;          geo_information :city          end
+  def postal_code ;   geo_information :postal_code   end
+
+  def plz
+    return postal_code if country_code == "DE"
+    return nil
+  end
+
+  def geo_information( key )
+    @geo_information = geo_information_from_gmaps unless @geo_information
+    @geo_information[ key ] if @geo_information
+  end
+
+  def bv
+    address= AddressString.new self.value
+    return Bv.by_address( address )
+  end
+
+  private
+
+  def geo_information_from_gmaps
+    begin
+      Gmaps4rails.geocode( self.gmaps4rails_address ).first
+    rescue
+      return nil
+      # Wenn keine Verbindung zu GoogleMaps besteht, wird hier ein Fehler auftreten,
+      # der die Anwendung jedoch nicht beenden sollte. 
+    end
+  end
+
+end
+
+class Study < ProfileField
+  def self.model_name; ProfileField.model_name; end
+
+end
+
+class BankAccount < ProfileField
+  def self.model_name; ProfileField.model_name; end
+
+end
+
+class Description < ProfileField
+  def self.model_name; ProfileField.model_name; end
+
+  def display_html
+    ActionController::Base.helpers.simple_format self.value
+  end
+
+end
+
+class Phone < ProfileField
+  def self.model_name; ProfileField.model_name; end
+
+end
+
+class Homepage < ProfileField
+  def self.model_name; ProfileField.model_name; end
+
+  def display_html
+    url = self.value
+    url = "http://#{url}" unless url.starts_with? 'http://'
+    ActionController::Base.helpers.link_to self.value, url
+  end
+
+end
