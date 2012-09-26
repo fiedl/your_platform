40ee6d611d6e20d20beacc3116742f786c101fa8
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-09-26 16:27:18 +0200

    fixing issue of the last commit: migration failed in travis

--- db/migrate/20120926140743_add_slim_menu_attribute_to_root_page.rb
+++ db/migrate/20120926140743_add_slim_menu_attribute_to_root_page.rb
@@ -2,16 +2,20 @@ class AddSlimMenuAttributeToRootPage < ActiveRecord::Migration
   def up
     
     # Add slim_menu attribute to the root page entry (wingolf.org).
-    nav_node = Page.find_root.nav_node
-    nav_node.slim_menu = true
-    nav_node.save
+    if Page.find_root
+      nav_node = Page.find_root.nav_node
+      nav_node.slim_menu = true
+      nav_node.save
+    end
 
   end
   def down
 
-    nav_node = Page.find_root.nav_node
-    nav_node.slim_menu = false
-    nav_node.save
+    if Page.find_root
+      nav_node = Page.find_root.nav_node
+      nav_node.slim_menu = false
+      nav_node.save
+    end
 
   end
 end
