0f9be2febd7db3af560543143763451d9b230d01
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-10-11 01:47:16 +0200

    completing specs for the erstbandphilister_parent_group group mixin

--- spec/models/group_mixins/special_groups_spec.rb
+++ spec/models/group_mixins/special_groups_spec.rb
@@ -10,6 +10,8 @@ describe GroupMixins::SpecialGroups do
       # He has joined corporation_a in 1960 and corporation_b in 1962.
       # The older membership defines in which corporation he is erstbandphilister.
 
+      @some_group = create( :group )
+
       @corporation_a = create( :wah_group )
       @philisterschaft_a = @corporation_a.philisterschaft
       @philister_a = @philisterschaft_a.child_groups.create( name: "Philister" )
@@ -34,6 +36,10 @@ describe GroupMixins::SpecialGroups do
 
     end
 
+
+    # Finder and Creator Methods
+    # ------------------------------------------------------------------------------------------
+
     describe "#is_erstbandphilister_parent_group?" do
       context "for an erstbandphilister_parent_group" do
         subject { @erstbandphilister_a.is_erstbandphilister_parent_group? }
@@ -42,26 +48,6 @@ describe GroupMixins::SpecialGroups do
       context "for a non-erstband_philister_parent_group" do
         subject { @philisterschaft_a.is_erstbandphilister_parent_group? }
         it { should == false }
-      end
-    end
-
-    describe "#users" do
-      it "should return the child users" do
-        @erstbandphilister_a.users.should include( @user )
-        @erstbandphilister_b.users.should_not include( @user )
-        # because @user is erstbandphilister of @corporation_a but not of @corporation_b
-      end
-    end
-
-    describe "#child_users" do
-      it "should be the same as #users" do
-        @erstbandphilister_a.child_users.should == @erstbandphilister_a.users
-      end
-    end
-
-    describe "#descendant_users" do
-      it "should be the same as #users" do
-        @erstbandphilister_a.descendant_users.should == @erstbandphilister_a.users
       end
     end
 
@@ -82,6 +68,94 @@ describe GroupMixins::SpecialGroups do
       end
     end
 
-  end
+    describe "#create_erstbandphilister_parent_group" do
+      context "for a philisterschaft group" do
+        context "if present" do
+          it "should raise an error" do
+            expect { @philisterschaft_a.create_erstbandphilister_parent_group }.to raise_error
+          end
+        end
+        context "if absent" do
+          subject { @philisterschaft_c.create_erstbandphilister_parent_group }
+          it "should return the new erstbandphilister_parent_group" do
+            subject.is_erstbandphilister_parent_group?.should be_true
+            subject.parent_groups.first.should == @philisterschaft_c
+          end
+        end
+      end
+      context "for a non-philisterschaft group" do
+        it "should raise an error" do
+          expect { @some_group.create_erstbandphilister_parent_group }.to raise_error
+        end
+      end
+    end
 
+    describe "#erstbandphilister" do
+      it "should be the same as #find_erstbandphilister_parent_group" do
+        @philisterschaft_a.erstbandphilister.should == 
+          @philisterschaft_a.find_erstbandphilister_parent_group
+      end
+    end
+
+    describe "#erstbandphilister!" do
+      context "if present" do
+        it "should return the present group" do
+          @philisterschaft_a.erstbandphilister!.should == 
+            @philisterschaft_a.find_erstbandphilister_parent_group
+        end
+      end
+      context "if absent" do
+        it "should create the group" do
+          @philisterschaft_c.erstbandphilister.should == nil
+          erstbandphilister_c = @philisterschaft_c.erstbandphilister!
+          erstbandphilister_c.is_erstbandphilister_parent_group?.should be_true
+          erstbandphilister_c.should == @philisterschaft_c.find_erstbandphilister_parent_group
+        end
+      end
+    end
+
+    describe ".create_erstbandphilister_parent_groups" do
+      subject { Group.create_erstbandphilister_parent_groups }
+      it "should not raise an error, e.g. when trying to overwrite a group" do
+        expect { subject }.to_not raise_error
+      end
+      it "should leave the present groups alone" do
+        subject
+        @philisterschaft_a.find_erstbandphilister_parent_group.should == @erstbandphilister_a
+      end
+      it "should create the absent groups" do
+        subject
+        @philisterschaft_c.find_erstbandphilister_parent_group.should_not == nil
+        @philisterschaft_c.find_erstbandphilister_parent_group.is_erstbandphilister_parent_group?.should be_true
+      end
+    end
+
+
+    # Redefined User Association Methods
+    # ------------------------------------------------------------------------------------------
+
+    describe "#users" do
+      it "should return the child users" do
+        UserGroupMembership.find_by_user_and_group( @user, @philisterschaft_a ).created_at.should <
+          UserGroupMembership.find_by_user_and_group( @user, @philisterschaft_b ).created_at
+        @erstbandphilister_a.users.should include( @user )
+        @erstbandphilister_b.users.should_not include( @user )
+        # because @user is erstbandphilister of @corporation_a but not of @corporation_b
+        # (see 'scenario' comment in the before block.)
+      end
+    end
+
+    describe "#child_users" do
+      it "should be the same as #users" do
+        @erstbandphilister_a.child_users.should == @erstbandphilister_a.users
+      end
+    end
+
+    describe "#descendant_users" do
+      it "should be the same as #users" do
+        @erstbandphilister_a.descendant_users.should == @erstbandphilister_a.users
+      end
+    end
+
+  end
 end
