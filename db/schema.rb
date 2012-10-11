78be1d8994b0e36b839d6eb0d1659c69e764d132
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2012-10-11 02:13:53 +0200

    adding migration to create erstbandphilister groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120928211931) do
+ActiveRecord::Schema.define(:version => 20121011001151) do
 
   create_table "attachments", :force => true do |t|
     t.string   "file"
