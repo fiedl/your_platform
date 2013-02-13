b289aa2c9f2972f4da99d61d475a75622cf96dc5
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-02-13 03:13:16 +0100

    removing obsolete test folder, since we're using rspec.

--- test/functional/profiles_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ProfilesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/workflows_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class WorkflowsHelperTest < ActionView::TestCase
-end
