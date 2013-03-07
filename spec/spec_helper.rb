d5fbd030b72a59a55cf08e39c908461b567b45d9
Jörg Reichardt <reichardt.joerg@gmail.com>
2013-03-07 21:11:40 +0100

    Move stub data to support.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -75,24 +75,9 @@ RSpec.configure do |config|
   # FactoryGirl syntax helper
   config.include FactoryGirl::Syntax::Methods
 
-  # set geocoder timeout to a small value in order to accelerate tests without 
+  # set geocoder to use stubs while in test
   # network connection.
   Geocoder.configure( lookup: :test )
-  Geocoder::Lookup::Test.add_stub(
-      "Pariser Platz 1\n 10117 Berlin", [
-      {
-          latitude: 52.5163,
-          longitude: 13.3778,
-          address: "Pariser Platz 1\n 10117 Berlin",
-          state: 'Berlin',
-          state_code: 'BER',
-          country: 'Germany',
-          country_code: 'DE',
-          city: 'Berlin',
-          postal_code: '10117'
-      }
-  ]
-  )
 end
 
 
