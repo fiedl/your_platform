1b123a608171896b7325660ba7c7d2194c3d13f7
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-03-24 03:46:51 +0100

    fixing migration that fails in test environment

--- db/migrate/20130320011252_add_redirections_for_special_pages.rb
+++ db/migrate/20130320011252_add_redirections_for_special_pages.rb
@@ -1,6 +1,8 @@
 class AddRedirectionsForSpecialPages < ActiveRecord::Migration
   def up
-    Page.find_root.update_attributes( :redirect_to => "http://wingolf.org" )
-    Page.find_intranet_root.update_attributes( :redirect_to => "root#index" )
+    if Page.find_root && Page.find_intranet_root
+      Page.find_root.update_attributes( :redirect_to => "http://wingolf.org" )
+      Page.find_intranet_root.update_attributes( :redirect_to => "root#index" )
+    end
   end
 end
