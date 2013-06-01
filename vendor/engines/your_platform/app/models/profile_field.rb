eaf6771d07f09802db60196a24d4aa3aef7595e2
Jörg Reichardt <reichardt.joerg@gmail.com>
2013-06-01 22:39:19 +0200

    Prevent a nil reference exception.

--- vendor/engines/your_platform/app/models/profile_field.rb
+++ vendor/engines/your_platform/app/models/profile_field.rb
@@ -335,7 +335,7 @@ module ProfileFieldTypes
     def self.model_name; ProfileField.model_name; end
 
     def display_html
-      url = self.value
+      url = self.value || ''
       url = "http://#{url}" unless url.starts_with? 'http://'
       ActionController::Base.helpers.link_to url, url
     end
