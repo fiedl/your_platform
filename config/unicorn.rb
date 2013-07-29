92e709d9bc0a65825625ab33b7f4026426963404
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-07-29 17:59:15 +0200

    deleting old configuration files

--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-root = "/var/wingolfsplattform/"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-listen "/tmp/unicorn.wingolfsplattform.sock"
-worker_processes 10
-timeout 30
