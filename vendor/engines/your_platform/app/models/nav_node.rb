4e46296f9dbd455a8d78a9f2e670c5f5f8ea184e
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-08-20 21:58:48 +0200

    refactoring and writing specs for NavNode

--- vendor/engines/your_platform/app/models/nav_node.rb
+++ vendor/engines/your_platform/app/models/nav_node.rb
@@ -1,44 +1,118 @@
 # -*- coding: utf-8 -*-
+#
+# Each Navable object has got an associated NavNode, i.e. an object representing the information
+# relevant to the position of the Navable object within the navigational structure.
+#
 class NavNode < ActiveRecord::Base
   attr_accessible :breadcrumb_item, :hidden_menu, :menu_item, :slim_breadcrumb, :slim_menu, :slim_url, :url_component
 
   belongs_to :navable, polymorphic: true
 
+  # The +url_component+ represents the part of the url, which is contributed by
+  # the Navable object. 
+  #
+  # If you have the following url 
+  #   http://example.com/products/phones/ ,
+  # and the current Navable is the Page @products_page, then its +url_component+ is
+  # 'products/'.
+  #
+  #     @products_page = Page.find_by_title("Products")
+  #     @products_page.nav_node.url_component  # => "products/"
+  #
+  # The default +url_component+ uses the Navable's title.
+  # But you can override the url_component of a Navable just by setting it.
+  # 
+  #     @nav_node = @products_page.nav_node
+  #     @nav_node.url_component = "our_products/"
+  #     @nav_node.save
+  # 
   def url_component
-    component = super
-    component = self.navable.title.parameterize + "/"  unless component
-    return component
+    super || "#{self.navable.title.parameterize}/"
   end
 
+  # The +breadcrumb_item+ is the string representing the Navable in a breadcrumb navigation.
+  # 
+  # For example:   example.com  >  Products  >  Phones
+  #                                --------
+  # The String "Products" is the +breadcrumb_item+ of the @products_page. 
+  # It defaults to the Navable's title and can be customized using the setter method
+  # +breadcrumb_item=+.
+  #
   def breadcrumb_item
-    title = super
-    title = self.navable.title unless title
-    return title
+    super || self.navable.title
   end
-
+  
+  # The +menu_item+ is the string representing the Navable in the vertical menu.
+  # It defaults to the Navable's title and can be customized using the setter method
+  # +menu_item=+.
+  #
   def menu_item
-    title = super
-    title = self.navable.title unless title
-    return title
+    super || self.navable.title
   end
-
+  
+  # The +hidden_menu+ attribute says if the Navable should be hidden from
+  # the vertical menu. 
+  #
+  # By default, 
+  #   * Pages are shown in the menu
+  #   * Groups are shown in the menu
+  #   *   exception: The :officers_parent groups are hidden in the menu.
+  #   * Users are hidden in the menu
+  #   * Events are hidden in the menu
+  #   * Workflows are hidden in the menu
+  #
+  # You can override the setting for a Navable by using the setter method 
+  # +hidden_menu=+ on the NavNode.
+  #
   def hidden_menu
     hidden = super
     hidden = true if self.navable.kind_of? User if hidden.nil?
     hidden = true if self.navable.kind_of? Event if hidden.nil?
     hidden = true if self.navable.title == "Amtsträger" if hidden.nil?
+    hidden = false if hidden.nil?
     return hidden
   end
+  
+  # +slim_breadcrumbs+ marks if the Navable should be hidden from the breadcrumb navigation
+  # in order to save space.
+  # 
+  # By default, no element is hidden from the breadcrumb navigation.
+  # To hide an element, just set 
+  #
+  #   @some_page.nav_node.update_attribute(:slim_breadcrumb, true)
+  # 
+  def slim_breadcrumb
+    super || false
+  end
 
-  # Gibt die vollständige Url des Navigationsknotens zurück, die sich aus den einzelnen url_components zusammensetzt.
+  # +url+ returns the joined url_components of this NavNode's Navable and its ancestors
+  # resulting in the generated url of the Navable.
+  # 
+  # Example:
+  #   Breadcrumb:  Example.com  >  Products  >  Phones
+  #   Url:         http://example.com/products/phones
+  #
+  # A possible trailing slash is removed from the +url+. Thus, the example's url does
+  # end on 'phones' rather than 'phones/'.
+  #
   def url
     url = ancestor_nodes_and_self.collect do |nav_node|
       nav_node.url_component
-    end.join.gsub( /(\/)$/, '' ) # Das gsub entfernt den abschließenden Slash, wenn einer da ist.
+    end.join.gsub( /(\/)$/, '' ) # The gsub call removes the trailing slash.
   end
-
-  # Gibt die Breadcrumb-Elemente zurück, die zu diesem Knoten führen, und zwar als assoziatives Array: <tt>{ title => object }</tt>, 
-  # wobei +title+ die Beschriftung des Breadcrumb-Elementes und +object+ das repräsentierte Objekt angibt.
+  
+  # +breadcrumbs+ returns an Array of breadcrumb Hashes representing the route to the
+  # Navable associated with this NavNode.
+  #
+  # Example:
+  #   Breadcrumb:  Example.com  >  Products  >  Phones
+  #   Url:         http://example.com/products/phones
+  #
+  #     @phones_page.nav_node.breadcrumbs  
+  #       # => [ {title: "Example.com", navable: @root_page, slim: false},
+  #              {title: "Products", navable: @products_page, slim: false},
+  #              {title: "Phones", navable: @phones_page, slim: false} ]
+  #
   def breadcrumbs
     breadcrumbs_to_return = []
     navables = self.ancestor_navables_and_own
@@ -50,40 +124,35 @@ class NavNode < ActiveRecord::Base
     return breadcrumbs_to_return
   end
 
-  # Gibt die übergeordneten navigationsfähigen Objekte (User, Page, ...) inkl. des eigenen Objektes als Array zurück,
-  # beginnend mit dem entferntesten (root) und endend mit dem eigenen Objekt.
-  def ancestor_navables_and_own
-    # Das Plugin 'acts-as-dag' unterstützt offenbar nicht, die Ancestor-Knoten nach ihrer Distanz zu sortieren.
-    # Daher muss, solange hier keine Lösung besteht, von Knoten zu Knoten vorgegangen werden, damit die Reihenfolge
-    # eingehalten wird.
-    # TODO: Sobald diese Funktionalität vorliegt, ist dies hier sicherlich performanter umzusetzen.
-    object = self.navable
-    navables = []
-    while object do
-      break if object == navables.last
-      navables << object if object.respond_to? :nav_node
-      for parent in object.parents do
-        if parent.respond_to? :nav_node
-          object = parent
-          break
-        end
-      end
-    end
-    navables.reverse!
-    return navables
-  end
-
-  # Gibt die übergeordneten navigationsfähigen Objekte zurück,
-  # beginnend mit dem entferntesten (root) und endend mit dem am nächsten liegenden.
+  # This returns the Navable ancestors of the Navable associated with this NavNode as an Array.
+  #
   def ancestor_navables
-    ancestor_navables_and_own.slice( 0..-2 )
+    self.navable.ancestors.select do |ancestor| 
+      ancestor.respond_to? :nav_node
+    end
   end
-
+  
+  # This returns the Navable ancestors of the Navable associated with this NavNode as an Array
+  # plus this NavNode's Navable as last element.
+  #
+  def ancestor_navables_and_own
+    ancestor_navables + [ self.navable ]
+  end
+  
+  # +ancestor_nodes+ returns an Array of the NavNodes of the ancestors of the Navable
+  # associated with this NavNode.
+  #
+  def ancestor_nodes
+    @ancestor_nodes ||= self.ancestor_navables.collect do |ancestor|
+      ancestor.nav_node
+    end
+  end
+  
+  # +ancestor_nodes_and_self+ returns an Array of the NavNodes of the ancestors of the
+  # Navable associated with this NavNode  plus  this NavNode as last element.
+  # 
   def ancestor_nodes_and_self
-    nodes = @ancestor_nodes_and_self 
-    nodes = self.navable.ancestors.collect { |ancestor| ancestor.nav_node } + [ self ] unless nodes
-    @ancestor_nodes_and_self = nodes unless @ancestor_nodes_and_self
-    return nodes
+    ancestor_nodes + [ self ]
   end
 
 end
---
+++ vendor/engines/your_platform/spec/models/nav_node_spec.rb
@@ -0,0 +1,258 @@
+require 'spec_helper'
+
+describe NavNode do
+  before do
+    # Since Users, Pages, Groups, etc. are Navables, i.e. have NavNodes associated,
+    # we here just pick the NavNode of a Page.
+    @page = create(:page, title: "My Page")
+    @nav_node = @page.nav_node
+    @nav_node.save
+  end
+  subject { @nav_node }
+  
+  describe "#url_component" do
+    subject { @nav_node.url_component }
+    describe "for no url_component set" do
+      it "should use the navable's title for generation" do
+        subject.should == "my-page/"
+      end
+    end
+    describe "for a url_component set" do
+      before do
+        @nav_node.url_component = "foo/"
+      end
+      it "should use the customized setting" do
+        subject.should == "foo/"
+      end
+    end
+  end
+  describe "#url_component=" do
+    subject { @nav_node.url_component = "foo/" }
+    it "should override the default setting" do
+      @nav_node.url_component.should_not == "foo/"
+      subject
+      @nav_node.url_component.should == "foo/"
+    end
+    it "should persist after saving" do
+      @nav_node.url_component.should_not == "foo/"
+      subject
+      @nav_node.save
+      @reloaded_nav_node = NavNode.find(@nav_node.id)
+      @reloaded_nav_node.url_component.should == "foo/"
+    end
+  end
+  
+  describe "#breadcrumb_item" do
+    subject { @nav_node.breadcrumb_item }
+    describe "for no breadcrumb_item set" do
+      it "should use the navable's title for generation" do
+        subject.should == "My Page"
+      end
+    end
+    describe "for a breadcrumb_item set" do
+      before do
+        @nav_node.breadcrumb_item = "Foo"
+      end
+      it "should use the customized setting" do
+        subject.should == "Foo"
+      end
+    end
+  end
+  describe "#breadcrumb_item=" do
+    subject { @nav_node.breadcrumb_item = "Foo" }
+    it "should override the default setting" do
+      @nav_node.breadcrumb_item.should_not == "Foo"
+      subject
+      @nav_node.breadcrumb_item.should == "Foo"
+    end
+    it "should persist after saving" do
+      @nav_node.breadcrumb_item.should_not == "Foo"
+      subject
+      @nav_node.save
+      @reloaded_nav_node = NavNode.find(@nav_node.id)
+      @reloaded_nav_node.breadcrumb_item.should == "Foo"
+    end
+  end
+  
+  describe "#menu_item" do
+    subject { @nav_node.menu_item }
+    describe "for no menu_item set" do
+      it "should use the navable's title for generation" do
+        subject.should == "My Page"
+      end
+    end
+    describe "for a menu_item set" do
+      before do
+        @nav_node.menu_item = "Foo"
+      end
+      it "should use the customized setting" do
+        subject.should == "Foo"
+      end
+    end
+  end
+  describe "#menu_item=" do
+    subject { @nav_node.menu_item = "Foo" }
+    it "should override the default setting" do
+      @nav_node.menu_item.should_not == "Foo"
+      subject
+      @nav_node.menu_item.should == "Foo"
+    end
+    it "should persist after saving" do
+      @nav_node.menu_item.should_not == "Foo"
+      subject
+      @nav_node.save
+      @reloaded_nav_node = NavNode.find(@nav_node.id)
+      @reloaded_nav_node.menu_item.should == "Foo"
+    end
+  end
+    
+  describe "#hidden_menu" do
+    subject { @nav_node.hidden_menu }
+    describe "for the setting being overridden" do
+      it "should return the forced setting" do
+        @nav_node.hidden_menu = true
+        @nav_node.hidden_menu.should == true
+        @nav_node.hidden_menu = false
+        @nav_node.hidden_menu.should == false
+      end
+    end
+    describe "for Pages, by default" do
+      before { @nav_node = create(:page).nav_node }
+      it { should == false }
+    end
+    describe "for Groups, by default" do
+      before { @nav_node = create(:group).nav_node }
+      it { should == false }
+    end
+    describe "for :officers_parent groups, by default" do
+      before { @nav_node = create(:group).find_or_create_officers_parent_group.nav_node }
+      it { should == true }
+    end
+    describe "for Users, by default" do
+      before { @nav_node = create(:user).nav_node }
+      it { should == true }
+    end
+    describe "for Events, by default" do
+      before { @nav_node = create(:event).nav_node }
+      it { should == true }
+    end
+    # 
+    # TODO: Later, when Workflows are Navables, use this:
+    #
+    # describe "for Workflows, by default" do
+    #   before { @nav_node = create(:workflow).nav_node }
+    #   it { should == true }
+    # end
+    # 
+    # For the moment:
+    #
+    describe "for Workflows, by default" do
+      specify "Workflows are not Navable at the moment, thus do not respond to #nav_node" do
+        create(:workflow).should_not respond_to :nav_node
+      end
+    end
+  end
+  
+  describe "#slim_breadcrumb" do
+    subject { @nav_node.slim_breadcrumb }
+    describe "for being set to true" do
+      before { @nav_node.slim_breadcrumb = true }
+      it { should == true }
+    end
+    describe "for being set to false" do
+      before { @nav_node.slim_breadcrumb = false }
+      it { should == false }
+    end
+    describe "for not being set" do
+      it { should == false }
+    end
+  end
+  
+  describe "(route methods)" do
+    before do
+      @root_page = create(:page, title: "Example.com")
+      @root_page.nav_node.update_attribute(:url_component, "http://example.com/")
+      @products_page = create(:page, title: "Products")
+      @products_page.parent_pages << @root_page
+      @phones_page = create(:page, title: "Phones")
+      @phones_page.parent_pages << @products_page
+      
+      @nav_node = @phones_page.nav_node
+    end
+  
+    describe "#url" do
+      subject { @nav_node.url }
+      it "should return the joined url" do
+        subject.should == "http://example.com/products/phones"
+      end
+      it "should remove the trailing slash (/)" do
+        subject.should_not end_with "/"
+      end
+    end
+  
+    describe "#breadcrumbs" do
+      subject { @nav_node.breadcrumbs }
+      it "should return an Array of Hashes" do
+        subject.should be_kind_of Array
+        subject.first.should be_kind_of Hash
+      end
+      specify "the Hash's attributes :title, :navable and :slim should be set" do
+        subject.first[:title].should_not == nil
+        subject.first[:navable].should_not == nil
+        subject.first[:slim].should_not == nil        
+      end
+      it { should == [ {title: "Example.com", navable: @root_page, slim: false},
+                       {title: "Products", navable: @products_page, slim: false},
+                       {title: "Phones", navable: @phones_page, slim: false} ] }
+    end
+  
+    describe "#ancestor_navables" do
+      subject { @nav_node.ancestor_navables }
+      it "should return the navable ancestors of the NavNode's Navable" do
+        subject.should == [ @root_page, @products_page ]
+      end
+      describe "for the ancestors' ids not being in an ascending order matching the hierarchy" do
+        before do
+          
+          # The @products_page is created before the @root_page on purpose.
+          #
+          @products_page = create(:page, title: "Products") 
+          @root_page = create(:page, title: "Example.com")
+          @root_page.nav_node.update_attribute(:url_component, "http://example.com/")
+          @products_page.parent_pages << @root_page
+          @phones_page = create(:page, title: "Phones")
+          @phones_page.parent_pages << @products_page
+      
+          @nav_node = @phones_page.nav_node
+        end
+        it "should return the navable ancestors of the NavNode's Navable" do
+          subject.should == [ @root_page, @products_page ]
+        end
+      end
+    end
+
+    describe "#ancestor_navables_and_own" do
+      it { should = [ @root_page, @products_page, @phone_page ] }
+    end 
+
+    describe "#ancestor_nodes" do
+      subject { @nav_node.ancestor_nodes }
+      it "should be an Array of NavNodes" do
+        subject.should be_kind_of Array
+        subject.first.should be_kind_of NavNode
+      end
+      it "should return the NavNodes of the Navable's ancestors" do
+        # use .to_s since these are, in fact, different objects in memory.
+        subject.to_s.should == [ @root_page.nav_node, @products_page.nav_node ].to_s
+      end
+    end
+    
+    describe "#ancestor_nodes_and_self" do
+      subject { @nav_node.ancestor_nodes_and_self }
+      it "should return an Array of the ancestor_nodes plus this NavNode as the last element" do
+        subject.to_s.should == [ @root_page.nav_node, @products_page.nav_node, @nav_node ].to_s
+      end
+    end
+    
+  end
+end
