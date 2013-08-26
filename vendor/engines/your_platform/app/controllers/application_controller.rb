11a4eebb4f74d039abc9f1a0aaeed2f0b33bf3ca
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-08-27 00:22:59 +0200

    i18n: determine locale in your_platform application_controller.
    https://trello.com/c/OxWCXisl/374-sprache-feststellen-i18n

--- vendor/engines/your_platform/app/controllers/application_controller.rb
+++ vendor/engines/your_platform/app/controllers/application_controller.rb
@@ -2,10 +2,31 @@ class ApplicationController < ActionController::Base
 
   layout "bootstrap"
 
+  # TODO: Change before_filter to before_action  (http://stackoverflow.com/questions/16519828)
+  #
+  before_filter :set_locale
   helper_method :current_user
   
   def current_user
     current_user_account.user if current_user_account
+  end
+  
+  # The locale of the application s set as follows:
+  #   1. Use the url parameter 'locale' if given.
+  #   2. Use the language of the web browser if supported by the app.
+  #   3. Use the default locale if no other could be determined.
+  #
+  def set_locale
+    I18n.locale = params[:locale] || browser_language_if_supported_by_app || I18n.default_locale
+  end
+  
+  private
+  def extract_locale_from_accept_language_header
+    # see: http://guides.rubyonrails.org/i18n.html
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first.to_sym if request.env['HTTP_ACCEPT_LANGUAGE']
+  end
+  def browser_language_if_supported_by_app
+    ([extract_locale_from_accept_language_header] & I18n.available_locales).first
   end
 
 end
