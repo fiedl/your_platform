be44587bdeaff3f2272047e5d7819b3028fd6f56
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-08-27 14:46:29 +0200

    i18n: adding basic locales spec

--- vendor/engines/your_platform/spec/features/locales_spec.rb
+++ vendor/engines/your_platform/spec/features/locales_spec.rb
@@ -9,10 +9,20 @@ feature 'Locales' do
   end
   
   scenario "providing the :locale parameter to display the page in different languages" do
+    
+    # providing the url parameter should change the locale.
+    #
     visit user_path(@user, :locale => :de)
     page.should have_text "Mein Profil"
     visit user_path(@user, :locale => :en)
     page.should have_text "My Profile"
+    
+    # the locale should be kept when visiting another page (using a cookie).
+    #
+    visit root_path
+    page.should have_text "My Profile"
+    page.should_not have_text "Mein Profil"
+
   end
 
 end
