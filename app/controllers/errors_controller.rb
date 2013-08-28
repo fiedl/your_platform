a4a3d3738117a5d67d7f5b78ac38a42dcfed2d3e
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-08-28 23:15:01 +0200

    unauthorized error: redirect to sign_in if the user is not signed in rather than just showing 'unauthorized'.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,5 +3,8 @@ class ErrorsController < ApplicationController
   skip_authorization_check
 
   def unauthorized
+    if not current_user
+      redirect_to sign_in_path, flash: { error: I18n.t(:unauthorized_please_sign_in) }
+    end
   end
 end
