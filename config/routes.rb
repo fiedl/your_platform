da6979e5b5907740bfeeefba305ffe18c8046444
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-08-29 01:15:18 +0200

    moving some routes to your_platform.

--- config/routes.rb
+++ config/routes.rb
@@ -11,39 +11,15 @@ Wingolfsplattform::Application.routes.draw do
   root :to => 'root#index'
 
   resources :posts
-
-  resources :groups do
-    get :my, on: :collection
-  end
-  resources :groups, as: :wah # to define wah_path
-
   resources :events  
-
   resources :bookmarks
 
   match "users/new/:alias" => "users#new"
 
   get :my_favorites, controller: "favorites", action: "index"
 
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
   resources :user_group_memberships
   resources :status_group_memberships
-
-  resources :users do
-    get :autocomplete_title, on: :collection
-    put :forgot_password, on: :member # the path method appears to be 'forgot_password_user_path'
-  end
 
   resources :workflows
 
@@ -52,50 +28,8 @@ Wingolfsplattform::Application.routes.draw do
 
   mount WorkflowKit::Engine => "/workflow_kit", as: 'workflow_kit'
 
-
-
   match 'profile/:alias' => 'users#show', :as => :profile
 
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
--- vendor/engines/your_platform/config/routes.rb
+++ vendor/engines/your_platform/config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   
   match "search" => "search#index", as: "search"
 
+  resources :users do
+    get :autocomplete_title, on: :collection
+    put :forgot_password, on: :member
+  end
+
+  resources :groups do
+    get :my, on: :collection
+  end
+
   resources :user_accounts
   resources :pages 
   resources :attachments
