13996e6952121597d75556065075d92044c3ba81
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-08-30 16:03:47 +0200

    moving the ToolButtonsHelper to your_platform.

--- vendor/engines/your_platform/app/helpers/tool_buttons_helper.rb
+++ vendor/engines/your_platform/app/helpers/tool_buttons_helper.rb
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 module ToolButtonsHelper
   
   def remove_button( object )
-    title = t( :remove )
+    title = t(:remove)
     title += ": " + object.title if object.respond_to?(:title) && object.title.present?
     link_to( tool_icon( "icon-trash icon-white" ),
              object,
@@ -13,7 +12,7 @@ module ToolButtonsHelper
            )
   end
 
-  def add_button( url, options = {} ) # TODO: Was braucht er für minimale Informationen?
+  def add_button( url, options = {} )
 
     # label for the button = icon + "add"
     label = tool_icon( "icon-plus icon-black" ) + " " + t( :add )
@@ -29,6 +28,22 @@ module ToolButtonsHelper
 
   end
 
+  def edit_button( options = {} )
+    tool_button :edit, "icon-edit icon-black", t(:edit), options
+  end
+
+  def save_button( options = {} )
+    tool_button( :save, "icon-ok icon-white", "", 
+                 :class => "save_button button btn btn-primary", :title => t(:save) )
+  end    
+
+  def cancel_button( options = {} )
+    tool_button( :cancel, "icon-remove icon-black", "", 
+                 :title => t(:cancel) )
+  end
+  
+  private
+  
   def tool_button( type, icon, text, options = {} )
     css_class = "button #{type}_button btn #{options[ :class ]}"; options.delete( :class )
     options = { :class => css_class }.merge( options )
@@ -38,21 +53,7 @@ module ToolButtonsHelper
     link_to( tool_icon( icon ) + " " + text,
              href,
              options )
-  end
-
-  def edit_button( options = {} )
-    tool_button :edit, "icon-edit icon-black", t( :edit ), options
-  end
-
-  def save_button( options = {} )
-    tool_button( :save, "icon-ok icon-white", "", 
-                 :class => "save_button button btn btn-primary", :title => t( :save ) )
-  end    
-
-  def cancel_button( options = {} )
-    tool_button( :cancel, "icon-remove icon-black", "", 
-                 :title => t( :cancel ) )
-  end
+  end  
 
   def tool_icon( type )
     content_tag( :i, "", :class => "#{type}" )
