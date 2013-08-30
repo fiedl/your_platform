eee3b33477368f78eeb3ee8f5920094fb553da97
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-08-30 16:56:42 +0200

    make title shown in html//head//title use the application name.

---
+++ vendor/engines/your_platform/app/helpers/title_helper.rb
@@ -0,0 +1,24 @@
+module TitleHelper
+  
+  def website_title_set_by_controller
+    @title
+    # alternative?:   content_for?(:title) ? yield(:title) : "YourPlatform"
+  end
+  
+  def website_title_with_app_name
+    if website_title_set_by_controller.present? 
+      if website_title_set_by_controller.include?(application_name)
+        return website_title_set_by_controller
+      else
+        return "#{website_title_set_by_controller} - #{application_name}"
+      end
+    else
+      return application_name
+    end
+  end
+  
+  def application_name 
+    Rails.application.class.parent_name
+  end
+  
+end
