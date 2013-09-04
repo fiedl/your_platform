f2ce54d7c269582d3f996a12189ac3ea717525b0
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-09-04 15:29:21 +0200

    do not use non-www flash message, since cross-subdomain cookies could be a security risk.

--- vendor/engines/your_platform/app/controllers/application_controller.rb
+++ vendor/engines/your_platform/app/controllers/application_controller.rb
@@ -16,7 +16,11 @@ class ApplicationController < ActionController::Base
   #
   def redirect_www_subdomain
     if request.host.split('.')[0] == 'www'
-      flash[:notice] = I18n.t(:you_may_leave_out_www)
+      # A flash cross-domain flash message would only work if the cookies are shared between the subdomains.
+      # But we won't make this decision for the main app, since this can be a security risk on shared domains.
+      # More info on sharing the cookie: http://stackoverflow.com/questions/12359522/
+      #
+      flash[:notice] = I18n.t(:you_may_leave_out_www)  
       redirect_to "http://" + request.host.gsub('www.','')
     end
   end
