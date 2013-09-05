a7c15e7a94dd6d2a1a8a329e9a330042e2ea6f8e
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-09-06 00:44:57 +0200

    introducing the set_title mechanism in the TitleHelper in order to replace the old mechanism, where the @title instance variable is set in the controller.

--- vendor/engines/your_platform/app/helpers/title_helper.rb
+++ vendor/engines/your_platform/app/helpers/title_helper.rb
@@ -1,8 +1,40 @@
+#
+# Each displayed html page has got a header title tag, i.e. <html><head><title>...</title></head>...</html>.
+# For a typical page, the title should be something like this: "User 'doe' - YourAppName", where the 
+# first part depends on the currently shown page, the second part just contains your app's name. 
+# If no first part exists, just display the app's name.
+# 
+# The first part of the title, i.e. the part specific to the currently shown page, 
+# is set in the html view of the currently shown page, since it is only required for html responses.
+# 
+#    <!-- In the view, e.g. app/views/users/show.html.erb: -->
+#    <% set_title("User '#{@user.alias}'") %>
+#
+# In the layout, the full title can be refered to as `website_title_with_app_name`.
+# 
+#    <!-- In the layout, e.g. app/views/layouts/application.html.erb: -->
+#    <head><title><%= website_title_with_app_name %></title>...</head>
+#
+# This behaviour is mostly inspired by Micheal Hartl's rails tutorial:
+#
+#   https://github.com/mhartl/sample_app/tree/master/app/views
+#   http://ruby.railstutorial.org/ruby-on-rails-tutorial-book
+#
 module TitleHelper
   
+  def set_title( title )
+    set_website_title(title)
+  end
+  def set_website_title( title )
+    provide(:title, title)
+  end
+  
   def website_title_set_by_controller
-    @title
-    # alternative?:   content_for?(:title) ? yield(:title) : "YourPlatform"
+    # yield(:title) uses the new mechanism.
+    # @title uses the old mechanism.
+    # TODO: Remove @title when it is not used in the controllers anymore.
+    #
+    content_for(:title) || @title
   end
   
   def website_title_with_app_name
