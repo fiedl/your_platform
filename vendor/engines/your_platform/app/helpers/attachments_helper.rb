77564a1a8ee2d63be5bb388641f76422eb24fbaa
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-09-10 16:22:30 +0200

    attachment fix: do not mix up attachments in blog view.

--- vendor/engines/your_platform/app/helpers/attachments_helper.rb
+++ vendor/engines/your_platform/app/helpers/attachments_helper.rb
@@ -1,9 +1,2 @@
 module AttachmentsHelper
-  
-  # This returns the proper attachment url, which routes the download through
-  # a controller.
-  #
-  def attachment_download_url(attachment, version = nil)
-    attachment_path(id: attachment.id, basename: File.basename(attachment.filename), extension: File.extension(attachment.filename) )
-  end
 end
