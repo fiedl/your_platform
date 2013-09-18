913cc4ef80a3243923acd4fd092637972b1b1f64
Manuel Zerpies <wp@mzerpies.de>
2013-09-18 23:01:13 +0200

    specs for changing date_of_birth begun

--- vendor/engines/your_platform/spec/features/user_change_date_of_birth_spec.rb
+++ vendor/engines/your_platform/spec/features/user_change_date_of_birth_spec.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
 
-feature "Change date of birth" do
+feature "Change date of birth", :focus => true do
   include SessionSteps
 
   background do
@@ -10,10 +10,18 @@ feature "Change date of birth" do
   end
   scenario "visit user page and change date of birth" do
     visit user_path(@user)
-    within("dd.date_of_birth") do
-      find(".best_in_place").set(I18n.localize("1980-01-01".to_date))
-      @user.date_of_birth.to_date.should have_content(I18n.localize("1980-01-01".to_date))
-    end
+    first("dd.date_of_birth").click
+    save_and_open_page
+
+#    within() do
+#      find(".best_in_place").click
+#      sleep 7
+#      #page.should have_selector("input")
+#      save_and_open_page
+#      fill_in :date_of_birth, :with => I18n.localize("1980-01-01".to_date)
+#      page.should have_text(I18n.localize("1980-01-01".to_date))
+#      @user.date_of_birth.should have_content(I18n.localize("1980-01-01".to_date))
+#    end
 
   end
 end
