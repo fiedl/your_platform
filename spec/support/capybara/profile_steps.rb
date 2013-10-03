252033511c3cdfa98e7c2e99e5d8a9c0c752d78e
Jörg Reichardt <reichardt.joerg@gmail.com>
2013-10-03 14:06:31 +0200

    Improve tests for single type sections.

--- spec/support/capybara/profile_steps.rb
+++ spec/support/capybara/profile_steps.rb
@@ -2,7 +2,7 @@ module ProfileSteps
   def section_should_be_editable(section, field_types = nil)
     within(".box.section.#{section}") do
       click_on I18n.t(:edit)
-      sleep 0.1
+
       page.should have_selector('a.add_button', visible: true)
 
       if field_types.kind_of? Array
@@ -10,18 +10,20 @@ module ProfileSteps
           test_field_type type
         end
       else
-        #puts section.to_s + ' with ' + @user.profile_fields.count.to_s
+        expect{
+          click_on I18n.t(:add)
+          page.should have_selector('.profile_field', visible: true)
+        }.to change{ all('.profile_field').count }
 
-        #expect {click_on I18n.t(:add)}.to change(UserAccount, :count).by 1
-        click_on I18n.t(:add)
-
-        page.should have_selector('.profile_field')
         within first '.profile_field' do
           page.should have_selector('input[type=text]')
         end
 
-        find('.remove_button').click
-        page.should_not have_selector('.profile_field')
+        expect{
+          find('.remove_button').click
+          page.should have_no_selector('.profile_field')
+        }.to change{ all('.profile_field').count }
+
       end
 
       page.should have_selector('a.save_button', visible: true)
@@ -33,7 +35,7 @@ module ProfileSteps
     #puts type.to_s + ' with ' + @user.profile_fields.count.to_s
     click_on I18n.t(:add)
 
-    puts all('.profile_field').count.to_s
+    puts all('.profile_field').count.to_s + ' profile fields'
     expect {
       field_name = type.name.demodulize.underscore
       page.should have_selector("a#add_#{field_name}_field", visible: true)
@@ -41,17 +43,18 @@ module ProfileSteps
       page.should have_content(I18n.t(field_name))
       click_on I18n.t(field_name)
       page.should have_no_selector("a#add_#{field_name}_field", visible: true)
-      puts all('.profile_field').count.to_s
+      puts all('.profile_field').count.to_s + ' profile fields'
     }.to change{ all('.profile_field').count }
 
     page.should have_selector('.profile_field')
 
     page.should have_selector('.remove_button')
 
-    expect {
+    #expect {
       all('.remove_button').last.click
-      page.should have_no_selector('this_is_just_to_trigger_capybaras_ajax_awaiting_which_is_not_triggered_by_click', visible: true)
-      puts all('.profile_field').count.to_s
-    }.to change{ all('.profile_field').count }
+      puts all('.remove_button').count.to_s + ' remove buttons'
+      page.should have_no_selector('this_is_just_to_trigger_capybaras_ajax_awaiting_which_is_not_triggered_by_click')
+      puts all('.profile_field').count.to_s + ' profile fields'
+    #}.to change{ all('.profile_field').count }
   end
 end
