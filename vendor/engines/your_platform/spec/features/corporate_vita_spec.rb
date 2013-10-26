d19e58491a2c5d71e30f5fb453c7c262bcfa9100
Jörg Reichardt <reichardt.joerg@gmail.com>
2013-10-26 21:30:44 +0200

    Add non-admin spec to corporate vita feature.

--- vendor/engines/your_platform/spec/features/corporate_vita_spec.rb
+++ vendor/engines/your_platform/spec/features/corporate_vita_spec.rb
@@ -10,7 +10,7 @@ feature 'Corporate Vita', js: true do
     @status_groups = @corporation.child_groups
   end
 
-  describe "promotion workflows:" do
+  describe 'as admin:' do
 
     background do
       @status_groups.first.assign_user @user
@@ -33,7 +33,7 @@ feature 'Corporate Vita', js: true do
       subject { page } # user profile page
       it 'should list the status group the user is a member of' do
         page.should have_content @status_groups.first.name
-        page.should_not have_content @status_groups.last.name
+        page.should have_no_content @status_groups.last.name
       end
     end
 
@@ -49,7 +49,7 @@ feature 'Corporate Vita', js: true do
         within '#corporate_vita' do
           page.should have_content @status_groups.first.name
           page.should have_content @status_groups.second.name
-          page.should_not have_content @status_groups.last.name
+          page.should have_no_content @status_groups.last.name
 
           # check this to avoid the double listing bug (sf 2013-01-24)
           page.should have_selector( 'a', count: 2 )
@@ -72,7 +72,7 @@ feature 'Corporate Vita', js: true do
 
     end
 
-    describe 'change the date of promation afterwards' do
+    describe 'change the date of promotion afterwards' do
       before do
         @first_promotion_workflow.execute( user_id: @user.id )
         @membership = UserGroupMembership.now_and_in_the_past.find_by_user_and_group( @user, @status_groups.first )
@@ -90,7 +90,7 @@ feature 'Corporate Vita', js: true do
           first('.best_in_place.status_group_date_of_joining').click
 
           within first '.best_in_place.status_group_date_of_joining' do
-            page.should have_field "created_at_date_formatted", with: @created_at_formatted
+            page.should have_field 'created_at_date_formatted', with: @created_at_formatted
           end
 
           # TODO: TEST THIS!
@@ -110,5 +110,63 @@ feature 'Corporate Vita', js: true do
     end
   end
 
+  describe 'as different user:' do
+
+    background do
+      @status_groups.first.assign_user @user
+
+      @first_promotion_workflow = create( :promotion_workflow, name: 'First Promotion',
+                                          :remove_from_group_id => @status_groups.first.id,
+                                          :add_to_group_id => @status_groups.second.id )
+      @first_promotion_workflow.parent_groups << @status_groups.first
+
+      @second_promotion_workflow = create( :promotion_workflow, name: 'Second Promotion',
+                                           :remove_from_group_id => @status_groups.second.id,
+                                           :add_to_group_id => @status_groups.last.id )
+      @second_promotion_workflow.parent_groups << @status_groups.second
+
+      login(:user)
+      visit user_path( @user )
+    end
+
+    describe 'viewing the user page' do
+      subject { page } # user profile page
+      it 'should list the status group the user is a member of' do
+        page.should have_content @status_groups.first.name
+        page.should have_no_content @status_groups.last.name
+      end
+    end
+
+    describe 'promoting users (i.e. change their status)' do
+      it 'should not be possible to promote users' do
+
+        # run the first workflow
+        within '.box.first' do
+          page.should have_no_content I18n.t(:change_status)
+        end
+      end
+
+    end
+
+    describe 'change the date of promotion afterwards' do
+      before do
+        @first_promotion_workflow.execute( user_id: @user.id )
+        @membership = UserGroupMembership.now_and_in_the_past.find_by_user_and_group( @user, @status_groups.first )
+        visit user_path( @user )
+      end
+
+      it 'should not be possible to change the date' do
+        within('#corporate_vita') do
+
+          @created_at_formatted = I18n.localize @membership.created_at.to_date
+
+          #page.should have_content @created_at_formatted #why does this fail?
+
+          # activate inplace editing of the date_field
+          page.should have_no_selector('.best_in_place.status_group_date_of_joining')
+        end
+      end
+    end
+  end
 end
 
