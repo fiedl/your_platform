d26db57b806824ff2d0262bf9b4ed479d4873c45
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-12-28 15:26:57 +0100

    fixing red '-1' spec.
    https://trello.com/c/0RDP04wo/454-fix-userpagespec-1-but-was-changed-by-0

--- vendor/engines/your_platform/app/helpers/sublime_helper.rb
+++ vendor/engines/your_platform/app/helpers/sublime_helper.rb
@@ -5,7 +5,7 @@ module SublimeHelper
   end
   
   def sublime_script_tag
-    javascript_include_tag "http://cdn.sublimevideo.net/js/#{sublime_token}.js" if sublime_token
+    javascript_include_tag "http://cdn.sublimevideo.net/js/#{sublime_token}.js" if sublime_token.present?
   end
   
 end
