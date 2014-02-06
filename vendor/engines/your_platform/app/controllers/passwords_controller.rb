11b65718c99c538615a2ddc12001daf904890f8f
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-02-07 00:10:46 +0100

    devise reset password: trying to override the devise controller.
    Sorry, I have to use the master stage to test this out. There might be a few additional commits.

--- vendor/engines/your_platform/app/controllers/passwords_controller.rb
+++ vendor/engines/your_platform/app/controllers/passwords_controller.rb
@@ -1,5 +1,9 @@
 class PasswordsController < Devise::PasswordsController
   
+  def new
+    super
+  end
+  
   # This method overrides the original one by devise:
   # https://github.com/plataformatec/devise/blob/master/app/controllers/devise/passwords_controller.rb
   #
@@ -7,6 +11,14 @@ class PasswordsController < Devise::PasswordsController
   # So, this method redirects to the action that generates a new password
   # and sends this new password via email.
   #
+  def create
+    redirect_to sign_in_path, notice: 'This is a test. This action does nothing at the moment.'
+  end
+  
+  def edit
+    super
+  end
+
   def update
     redirect_to sign_in_path, notice: 'This is a test. This action does nothing at the moment.'
   end
