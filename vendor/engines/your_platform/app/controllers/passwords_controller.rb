8c1827f591cdd435393b00411ba9c3ccbada3d54
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-02-06 23:41:30 +0100

    devise: beginning to override the password reset mechanism in order not to allow custom passwords but send generated passwords via email.

---
+++ vendor/engines/your_platform/app/controllers/passwords_controller.rb
@@ -0,0 +1,14 @@
+class PasswordsController < Devise::PasswordsController
+  
+  # This method overrides the original one by devise:
+  # https://github.com/plataformatec/devise/blob/master/app/controllers/devise/passwords_controller.rb
+  #
+  # We do not allow custom passwords at the moment. 
+  # So, this method redirects to the action that generates a new password
+  # and sends this new password via email.
+  #
+  def update
+    redirect_to sign_in_path, notice: 'This is a test. This action does nothing at the moment.'
+  end
+  
+end
