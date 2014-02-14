d54f4fd85590d8f8e85e33a8fb8e8e895ac8aa2b
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-02-14 01:30:46 +0100

    new localized and longer welcome email.

--- app/mailers/user_account_mailer.rb
+++ app/mailers/user_account_mailer.rb
@@ -2,7 +2,23 @@ class UserAccountMailer < ActionMailer::Base
   def welcome_email( user, password ) 
     @user = user
     @password = password
+    @bundesbruder_or_philister = @user.philister? ? "Philister" : "Bundesbruder"
+    
+    # Bundesbrüder, die nur in Estland aktiv sind, bekommen diese E-Mail auf englisch,
+    # alle anderen auf deutsch.
+    #
+    if @user.corporations.collect { |corporation| corporation.token } == ["Dp"]
+      locale = :en
+    else
+      locale = :de
+    end
+    
+    
     to = "#{@user.name} <#{@user.email}>"
-    mail to: to, subject: t( :welcome_to_wingolfsplattform )
+    
+    I18n.with_locale(locale) do
+      mail to: to, subject: t( :welcome_to_wingolfsplattform )
+    end
+    
   end
 end
