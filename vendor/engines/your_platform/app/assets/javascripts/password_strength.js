ac58b91de9c265236d1247029e6234ef0263aa00
Jörg Reichardt <reichardt.joerg@gmail.com>
2014-04-19 14:46:15 +0200

    Make password strength configurable

--- vendor/engines/your_platform/app/assets/javascripts/password_strength.js
+++ vendor/engines/your_platform/app/assets/javascripts/password_strength.js
@@ -4,8 +4,7 @@ var PasswordStrength = {
         var confirmation_input = $(password_confirmation_input_id)
         var submit = $(":submit");
 
-        var container = $('<div class="password_strength_container" />');
-        input.parent().append(container);
+        var container = $('.password_strength_container');
 
         var bg_elm = $('<div class="password_strength_bg" />');
         container.append(bg_elm);
@@ -20,12 +19,15 @@ var PasswordStrength = {
             container.append($('<div class="password_strength_separator" />').css(style));
         }
 
-        var advice = "A good password is easy to remember but hard for a stranger to guess. Uncommon words work well, but only if you use several. Also helpful: non-standard uPPercasing, creative spelllling, personal neologisms, and non-obvious numbers and symbols (using $ for s or 0 for o is too obvious!)";
-        var info_button = $("<span class='password_strength_icon'/>");
+        //var advice = "A good password is easy to remember but hard for a stranger to guess. Uncommon words work well, but only if you use several. Also helpful: non-standard uPPercasing, creative spelllling, personal neologisms, and non-obvious numbers and symbols (using $ for s or 0 for o is too obvious!)";
+        var advice = container.data('password-strength-advice');
+
+        //shoutout to the nice guys from http://glyphicons.com/
+        var info_button = $("<i class='icon-info-sign'/>");
 
         //var info_button = $("<a href='#' tabindex='-1' class='password_strength_icon' />");
         //info_button.val(Sprite.make('web', 'information'));
-        info_button.text('?');
+        //info_button.text('?');
         info_button.css("display", 'none');
         info_button.tooltip({trigger: "hover"});
         info_button.prop("title", advice);
@@ -37,22 +39,7 @@ var PasswordStrength = {
         //password_desc.text('&nbsp;');
         container.append(password_desc);
 
-        var color_map = [
-            "",
-            "#c81818",
-            "#ffac1d",
-            "#a6c060",
-            "#27b30f"
-        ];
-
-        var word_map = [
-            // TRANSLATORS the following strings refer to a password strength meter on the registration page
-            ["", "Very weak"],
-            ["#c81818", "Weak"],
-            ["#e28f00", "So-so"],
-            ["#8aa050", "Good"],
-            ["#27b30f", "Great!"]
-        ];
+        var score_description_map = container.data('score-descriptions')
 
         var last_pwd = '';
         var last_confirmation_pwd = '';
@@ -82,20 +69,18 @@ var PasswordStrength = {
                 }
             } else {
                 score = PasswordStrength.score(pwd);
-                word = word_map[score];
                 info_button.prop("title", advice);
             }
 
-            password_desc.css("color", word[0]);
-            password_desc.text(pwd.length ? word[1] : "");
+            container.removeClass()
+            container.addClass('password_strength_container')
+            container.addClass('password_strength_score' + score)
+            password_desc.text(pwd.length ? score_description_map[score] : "");
 
             if (pwd.length && score < 3) {
                 info_button.show();
             } else {
                 info_button.hide();
-                if (info_button.tooltip) {
-                    //info_button.tooltip.hide();
-                }
             }
 
             var password_mismatch = pwd != confirmation_input.val();
@@ -111,8 +96,6 @@ var PasswordStrength = {
                 submit.removeProp("disabled");
             }
 
-            var color_ind = score;
-            elm.css("backgroundColor", color_map[color_ind]);
             elm.cur_score = score;
             if (score == 0) {
                 elm.css("width", "0%");
