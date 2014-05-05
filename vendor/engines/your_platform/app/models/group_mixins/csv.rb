dbc2bc842e2979f8888ca672a1583acf5f4dfec1
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-05-05 02:36:36 +0200

    csv: adding list for member development of a group, i.e. when did status changes occur.

--- vendor/engines/your_platform/app/models/group_mixins/csv.rb
+++ vendor/engines/your_platform/app/models/group_mixins/csv.rb
@@ -29,6 +29,7 @@ module GroupMixins::Csv
     case column_configuration
       when 'birthday_list' then members_birthdays_to_csv
       when 'address_list' then members_addresses_to_csv
+      when 'member_development' then member_development_to_csv
       else members_names_to_csv
     end
   end
@@ -116,6 +117,44 @@ module GroupMixins::Csv
           member.name_prefix,
           member.name_suffix
         ]
+      end
+    end
+  end
+  
+  # Diese Methode stellt Informationen zur Mitgliederbewegung in einem CSV-Format
+  # zusammen. Hierfür werden die Gruppenmitglieder mit ihren wichtigsten Daten
+  # sowie ihren jeweiligen Eintrittsdaten der Statusgruppen aufgeführt.
+  # 
+  def member_development_to_csv
+    status_groups = if self.corporation?
+      self.becomes(Corporation).status_groups
+    end || []
+    status_group_names = status_groups.collect { |group| group.name }
+    
+    CSV.generate(csv_options) do |csv|
+      csv << [
+        I18n.t(:last_name),
+        I18n.t(:first_name),
+        '',
+        I18n.t(:date_of_birth),
+        I18n.t(:date_of_death)
+      ] + status_group_names
+      
+      self.members.each do |member|
+        status_group_membership_dates = status_groups.collect do |status_group|
+          datetime = UserGroupMembership.find_by_user_and_group(member, status_group).try(:valid_from)
+          date = datetime.try(:to_date)
+          localized_date = I18n.localize(date) if date
+          localized_date || ''
+        end
+        
+        csv << [
+          member.last_name,
+          member.first_name,
+          member.title.gsub(member.name, '').strip,
+          member.date_of_birth.nil? ? '' : I18n.localize(member.date_of_birth),
+          member.date_of_death || ''
+        ] + status_group_membership_dates
       end
     end
   end
