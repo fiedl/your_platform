478bc83fb9b60d70e23fd18613e7cc92da9f0857
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-07-13 15:21:28 +0200

    groups#show: dirty-fixing the new-user gui mechanism.

--- vendor/engines/your_platform/spec/features/groups_page_spec.rb
+++ vendor/engines/your_platform/spec/features/groups_page_spec.rb
@@ -25,7 +25,7 @@ feature "Groups Page" do
       within('.box.section.members') do
         click_on I18n.t(:add)
 
-        page.should have_selector '#group_members ul.child_users li', count: 1
+        page.should have_selector '#group_members .new_child_users li', count: 1
         page.should have_text @user.title
         find('.user-select-input').value.should == ""
       end
@@ -65,13 +65,13 @@ feature "Groups Page" do
 
     scenario 'should not render list entries for hidden members' do
       visit group_path(@group)
-      page.should have_selector '#group_members ul.child_users li', count: 11
+      page.should have_selector '#group_members tr', count: 12
       page.should have_no_text 'Hidden'
     end
     
     scenario 'should render list entries for dead members' do
       visit group_path(@group)
-      page.should have_selector '#group_members ul.child_users li', count: 11
+      page.should have_selector '#group_members tr', count: 12
       page.should have_text 'Dead'
     end
   end
