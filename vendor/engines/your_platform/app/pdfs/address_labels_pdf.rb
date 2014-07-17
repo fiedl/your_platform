a6bec23282dfbe039afc54fd4185db9c37e7164e
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-07-18 00:12:10 +0200

    pdf address label export layout correction.

--- vendor/engines/your_platform/app/pdfs/address_labels_pdf.rb
+++ vendor/engines/your_platform/app/pdfs/address_labels_pdf.rb
@@ -3,14 +3,14 @@ require 'prawn/measurement_extensions'
 class AddressLabelsPdf < Prawn::Document
   
   def initialize(addresses, options = {title: '', updated_at: Time.zone.now, sender: ''})
-    super(page_size: 'A4', top_margin: 5.mm, bottom_margin: 5.mm, left_margin: 10.mm, right_margin: 10.mm)
+    super(page_size: 'A4', top_margin: 8.mm, bottom_margin: 8.mm, left_margin: 10.mm, right_margin: 10.mm)
     
     @document_title = options[:title]
     @document_updated_at = options[:updated_at]
     @required_page_count = (addresses.count / 24).round + 1
     @sender = options[:sender]
         
-    define_grid columns: 3, rows: 8, gutter: 5.mm
+    define_grid columns: 3, rows: 8, gutter: 10.mm
     
     for p in (0..(@required_page_count - 1))
       page_header
@@ -61,10 +61,10 @@ class AddressLabelsPdf < Prawn::Document
   end
     
   def page_header_text
-    "Druck auf Zweckform 3475, 70x36 mm², 24 Stk pro Blatt, DIN-A4"
+    "Druck auf Zweckform 3475, 70x36 mm², 24 Stk pro Blatt, DIN-A4. Skalierung auf 100% stellen!"
   end
   def page_header
-    bounding_box([0, 29.15.cm], width: 18.5.cm, height: 0.5.cm) do
+    bounding_box([0, 28.85.cm], width: 18.5.cm, height: 0.5.cm) do
       text page_header_text, size: 8.pt, align: :center
     end
   end
@@ -72,7 +72,7 @@ class AddressLabelsPdf < Prawn::Document
     "#{Rails.application.class.parent_name}: #{@document_title}, Datenstand: #{I18n.localize(@document_updated_at)}, Seite #{page_number} von #{@required_page_count}"
   end
   def page_footer
-    bounding_box([0, -0.2.cm], width: 18.5.cm, height: 0.5.cm) do
+    bounding_box([0, -0.5.cm], width: 18.5.cm, height: 0.5.cm) do
       text page_footer_text, size: 8.pt, align: :center
     end
   end
