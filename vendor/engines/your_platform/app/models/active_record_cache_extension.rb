bbc400b399b2d9386d09a1e2b745a5c43f31db48
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-08-12 13:07:17 +0200

    model caching: fixing wrong use of .table_name.

--- vendor/engines/your_platform/app/models/active_record_cache_extension.rb
+++ vendor/engines/your_platform/app/models/active_record_cache_extension.rb
@@ -2,20 +2,22 @@ module ActiveRecordCacheExtension
   extend ActiveSupport::Concern
     
   def cached(method_name, args)
-    Rails.cache.fetch([table_name, id, method_name, **args], expire_in: 1.week) { send(method_name(**args)) }
+    p [self.class.table_name, id, method_name, **args]
+    Rails.cache.fetch([self.class.table_name, id, method_name, **args], expire_in: 1.week) { send(method_name(**args)) }
   end
   
   def delete_cached(method_name)
-    Rails.cache.delete_matched "#{table_name}*#{id}*#{method_name}*"
+    p "DELETE #{self.class.table_name}*#{id}*#{method_name}*"
+    Rails.cache.delete_matched "#{self.class.table_name}*#{id}*#{method_name}*"
   end
   
   def delete_cache
-    Rails.cache.delete_matched "#{table_name}*#{id}*"
+    Rails.cache.delete_matched "#{self.class.table_name}*#{id}*"
   end
   
   def cache_created_at(method_name, args)
     CacheAdditions
-    Rails.cache.created_at [table_name, id, method_name, **args]
+    Rails.cache.created_at [self.class.table_name, id, method_name, **args]
   end
     
   module ClassMethods    
