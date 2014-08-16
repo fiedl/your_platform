541bd767dc880ccb81bb09072ac6463742f8c16a
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-08-16 18:40:45 +0200

    model caching: circumvent marshaling bug.
    The problems we had, actually did not occur when converting an arel, but were due to a bug
    described here:
    https://github.com/mperham/dalli/issues/250
    
    This circumvents this bug, but writing a cache takes a little longer.

--- vendor/engines/your_platform/app/models/active_record_cache_extension.rb
+++ vendor/engines/your_platform/app/models/active_record_cache_extension.rb
@@ -2,9 +2,15 @@ module ActiveRecordCacheExtension
   extend ActiveSupport::Concern
   
   def cached(method_name)
-    convert_ids_of_cached_relation(Rails.cache.fetch([self.class.table_name, id, method_name], expire_in: 1.week) do
-      convert_ids_of_uncached_relation(send(method_name))
-    end)
+    #require 'pry'; binding.pry
+    Rails.cache.fetch([self.class.table_name, id, method_name], expire_in: 1.week) do 
+      result = send(method_name)
+      Marshal.dump(result) # This circumvents a bug: https://github.com/mperham/dalli/issues/250
+      result
+    end
+    #convert_ids_of_cached_relation(Rails.cache.fetch([self.class.table_name, id, method_name], expire_in: 1.week) do
+    #  convert_ids_of_uncached_relation(send(method_name))
+    #end)
   end
   
   def delete_cached(method_name)
@@ -22,20 +28,30 @@ module ActiveRecordCacheExtension
   
   private
 
-  def convert_ids_of_cached_relation(relation)
-    if relation.kind_of?(Array) && relation.first.kind_of?(Integer)
-      self.class.unscoped.find relation
-    else
-      relation
-    end
-  end
-  def convert_ids_of_uncached_relation(relation)
-    if relation.kind_of? ActiveRecord::Relation
-      relation.pluck(:id)
-    else
-      relation
-    end
-  end
+  # THIS CONVERSION IS CRAP:
+  # Noone can know that the ids belong to this class (self)!
+
+  #def convert_ids_of_cached_relation(relation)
+  #  p "CONVERT 1"
+  #  if relation.kind_of?(Array) && relation.first.kind_of?(Integer)
+  #    self.class.unscoped.find relation
+  #  else
+  #    relation
+  #  end
+  #end
+  #def convert_ids_of_uncached_relation(relation)
+  #  p "CONVERT 2"
+  #  p relation.class.name
+  #  p relation.kind_of? ActiveRecord::Relation
+  #  p relation if relation.kind_of?(Array)
+  #  
+  #  if relation.kind_of? ActiveRecord::Relation
+  #    p relation.pluck(:id)
+  #    relation.pluck(:id)
+  #  else
+  #    relation
+  #  end
+  #end
   
   module ClassMethods
   end
