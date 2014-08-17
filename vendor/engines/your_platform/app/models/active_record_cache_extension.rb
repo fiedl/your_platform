f87420ef5ae8580c06911be74247bba7347c897a
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-08-17 22:39:13 +0200

    model caching: experiment with cache invalidation rather than cache deletion.
    invalidation is much faster than deletion with wild cards (factor 8).

--- vendor/engines/your_platform/app/models/active_record_cache_extension.rb
+++ vendor/engines/your_platform/app/models/active_record_cache_extension.rb
@@ -2,24 +2,30 @@ module ActiveRecordCacheExtension
   extend ActiveSupport::Concern
   
   def cached(method_name)
-    Rails.cache.fetch([self.class.table_name, id, method_name], expire_in: 1.week) do 
+    Rails.cache.fetch([self, method_name], expires_in: 1.week) do 
       result = send(method_name)
       Marshal.dump(result) # This circumvents a bug: https://github.com/mperham/dalli/issues/250
       result
     end
   end
   
+  def invalidate_cache
+    self.touch
+  end
+  
   def delete_cached(method_name)
-    Rails.cache.delete_matched "#{self.class.table_name}/#{id}/#{method_name}"
+    invalidate_cache
+    #Rails.cache.delete_matched "#{self.class.table_name}/#{id}/#{method_name}"
   end
   
   def delete_cache
-    Rails.cache.delete_matched "#{self.class.table_name}/#{id}/*"
+    invalidate_cache
+    #Rails.cache.delete_matched "#{self.class.table_name}/#{id}/*"
   end
   
   def cache_created_at(method_name)
-    CacheAdditions
-    Rails.cache.created_at [self.class.table_name, id, method_name]
+    #CacheAdditions
+    Rails.cache.created_at [self, method_name]
   end
   
   module ClassMethods
