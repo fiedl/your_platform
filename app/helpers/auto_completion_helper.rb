0f11647bfef86de7a73e25f883916c7f91f8447a
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-10-24 20:57:34 +0200

    events#show: adding activator to the contact_people field.
    Otherwise, the field can’t be edited, since the box has no ‚edit‘
    button.
    
    trello: https://trello.com/c/cOa9EpLf/677-veranstaltungen

--- app/helpers/auto_completion_helper.rb
+++ app/helpers/auto_completion_helper.rb
@@ -1,15 +1,27 @@
 module AutoCompletionHelper
 
-  def multiple_users_best_in_place( object, attribute )
+  # options: 
+  #   - activator: places a pen next to the field that activates it (for boxes without edit button)
+  #
+  def multiple_users_best_in_place(object, attribute, options = {})
     # autocomplete_field_tag :user_title, '', autocomplete_title_users_path %>
-    best_in_place( object, attribute, 
+    
+    activator_id = options[:activator] ? "#{object.cache_key} #{attribute} multi user selector activator".parameterize : ''
+    
+    html = best_in_place( object, attribute, 
                    html_attrs: { 
                      'data-autocomplete-url' => autocomplete_title_users_path,
                      :class => 'multiple-users-select-input'
                    },
                    display_with: lambda { |str|
                      str.split(",").collect { |name| link_to(name.strip, search_guess_path(query: name.strip)) }.join(", ").html_safe
-                   } )
+                   },
+                   activator: (activator_id.present? ? ('#' + activator_id) : '')
+                   )
+    if options[:activator]
+      html += link_to(content_tag(:i, '', class: 'icon-edit'), '#', id: activator_id, class: 'multi_user_select_activator', title: I18n.t(:edit)).html_safe
+    end
+    return html.html_safe
   end
 
   def user_best_in_place( object, attribute )
