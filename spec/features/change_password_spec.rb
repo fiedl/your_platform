8b65f4087144e93273c5b34a40873356b13f1364
Jörg Reichardt <reichardt.joerg@gmail.com>
2014-11-12 18:40:20 +0100

    Ensure jqery is loaded before PasswordStrength.
    Also, adapt tests to the now working client side validation.
    Unfortunately, the positive case is still defunct.

--- spec/features/change_password_spec.rb
+++ spec/features/change_password_spec.rb
@@ -12,7 +12,6 @@ feature 'Change Password', :js => true do
       login(@user)
       visit user_path(@user)
       within('.box.section.access') do
-
         click_on I18n.t(:edit)
       end
     end
@@ -28,11 +27,11 @@ feature 'Change Password', :js => true do
       it { should have_field('user_account_password_confirmation') }
       it { should have_field('user_account_current_password') }
       it { should have_field(I18n.t(:i_agree_i_do_not_use_the_same_password_on_other_services), :checked => false) }
-      it { should have_button(I18n.t(:submit_changed_password)) }
+      it { should have_button(I18n.t(:submit_changed_password), disabled: true) }
 
-      describe 'and matching password and confirmation' do
+      describe 'and matching complex password and confirmation' do
         before do
-          @password = 'Password123'
+          @password = 'fordprefecthasanawesometowel!'
           fill_in 'user_account_password', with: @password
           fill_in 'user_account_password_confirmation', with: @password
         end
@@ -44,34 +43,40 @@ feature 'Change Password', :js => true do
 
           describe 'and having checked the agreement, after clicking submit' do
             before do
-              find('label', :text => I18n.t(:i_agree_i_do_not_use_the_same_password_on_other_services)).set(true)
+              check(I18n.t(:i_agree_i_do_not_use_the_same_password_on_other_services))
+              page.save_screenshot("~/blubb.jpg")
+              sleep(2.0)
               click_button I18n.t(:submit_changed_password)
             end
             it { should have_notice(I18n.t('devise.registrations.updated')) }
           end
 
           describe 'but not having checked the agreement' do
-            before do
-              #page.save_and_open_page('~/blubb')
-              page.save_screenshot('~/blubb')
-              #click_button I18n.t(:submit_changed_password)
-            end
-
-            it { should_not have_notice(I18n.t('devise.registrations.updated')) }
             it { should have_button(I18n.t('submit_changed_password'), disabled: true) }
           end
-
         end
 
         describe 'but wrong current password' do
           before do
             fill_in 'user_account_current_password', with: 'invalid'
-            click_button I18n.t(:submit_changed_password)
           end
 
-          it { should have_content(I18n.t('errors.messages.not_saved.one', resource:'user account')) }
-
+          it { should have_button(I18n.t('submit_changed_password'), disabled: true) }
         end
+
+      end
+
+      describe 'and matching simple password and confirmation' do
+        before do
+          @password = 'Password123'
+          fill_in 'user_account_password', with: @password
+          fill_in 'user_account_password_confirmation', with: @password
+          fill_in 'user_account_current_password', with: @current_password
+          check(I18n.t(:i_agree_i_do_not_use_the_same_password_on_other_services))
+        end
+
+        it { should_not have_notice(I18n.t('devise.registrations.updated')) }
+        it { should have_button(I18n.t('submit_changed_password'), disabled: true) }
 
       end
 
@@ -81,11 +86,10 @@ feature 'Change Password', :js => true do
           fill_in 'user_account_password', with: @password
           fill_in 'user_account_password_confirmation', with: 'invalid'
           fill_in 'user_account_current_password', with: @current_password
-          click_button I18n.t(:submit_changed_password)
+          #click_button I18n.t(:submit_changed_password)
         end
 
-        it { should have_validation_errors }
-        it { should have_content(I18n.t('errors.messages.not_saved.one', resource:'user account')) }
+        it { should have_button(I18n.t('submit_changed_password'), disabled: true) }
       end
 
     end
