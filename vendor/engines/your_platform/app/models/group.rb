fiedl/your_platform

commit 9d3729e431d6ae1e28316a7edca5f2a6ccf4aa95

events: to attend an event does not count as full corporation membership.
* modify `Role#current_member`
* add `Role#full_member`
* modify `User.current_corporations`
* fixing specs

trello: https://trello.com/c/cOa9EpLf/677-veranstaltungen

vendor/engines/your_platform/app/models/group.rb
@@ -63,6 +63,18 @@ class Group < ActiveRecord::Base
     I18n.t( super.to_sym, default: super ) if super
   end
   
+  def extensive_name
+    if has_flag? :attendees
+      name + (parent_events.first ? ": " + parent_events.first.name : '')
+    elsif has_flag? :contact_people
+      name + (parent_events.first ? ": " + parent_events.first.name : '')
+    elsif has_flag? :admins_parent
+      name + ": " + parent_groups.first.parent_groups.first.name
+    else
+      name
+    end
+  end
+  
   # This sets the format of the Group urls to be
   # 
   #     example.com/groups/24-planeswalkers
@@ -122,9 +134,13 @@ class Group < ActiveRecord::Base
 
   # Events
   # ------------------------------------------------------------------------------------------
+  
+  def events
+    self.descendant_events
+  end
 
   def upcoming_events
-    self.descendant_events.upcoming
+    self.events.upcoming
   end
   
   

lib/tasks/patch_groups.rake
@@ -1,7 +1,8 @@
 namespace :patch do
 
   task :groups => [
-    'groups:part1'
+    'groups:part1',
+    'groups:part2'
   ]
   
   namespace :groups do
@@ -22,6 +23,10 @@ namespace :patch do
       'print_info',
       'add_wingolf_super_groups',
       'recalculate_membership_validity_ranges_for_super_groups'
+    ]
+    
+    task :part2 => [
+      'full_members_flags'
     ]
     
     task :add_wingolf_super_groups => [:environment, :requirements, :print_info] do
@@ -109,6 +114,25 @@ namespace :patch do
       
       log.info ""
       log.success "#{counter} UserGroupMemberships aktualisiert."
+    end
+    
+    task :full_members_flags => [:environment, :requirements, :print_info] do
+      log.section "Aktivitas und Philisterschaft mit :full_members markieren"
+      log.info "Damit wird ermittelt, ob eine Person ordentliches Mitglied einer"
+      log.info "Verbindung ist."
+      
+      groups = 
+        Group.find_by_flag(:alle_aktiven).child_groups +
+        Group.find_by_flag(:alle_philister).child_groups -
+        Group.find_all_by_flag(:officers_parent)
+      
+      groups.each do |group|
+        group.add_flag :full_members
+        print ".".green
+      end
+      
+      log.info ""
+      log.info "Fertig."
     end
   end
   

spec/factories/wingolf_corporation.rb
@@ -15,6 +15,8 @@ FactoryGirl.define do
       corporation.import_default_group_structure "default_group_sub_structures/wingolf_am_hochschulort_children.yml"
       corporation.reload
       
+      corporation.child_groups.where(name: ['Aktivitas', 'Philisterschaft']).each { |g| g.add_flag :full_members }
+      
       Group.alle_aktiven << corporation.child_groups.where(name: 'Aktivitas').first
       Group.alle_philister << corporation.child_groups.where(name: 'Philisterschaft').first
     end

spec/features/erstbandphilister_spec.rb
@@ -4,14 +4,18 @@ feature "Erstbandphilister" do
   include SessionSteps
 
   before do
-    @corporation = create(:corporation)
-    @philisterschaft_group = @corporation.child_groups.create(name: "Philisterschaft")
-    @regular_philister_group = @philisterschaft_group.child_groups.create(name: "Philister")
+    @corporation = create :wingolf_corporation
+    @philisterschaft_group = @corporation.descendant_groups.where(name: "Philisterschaft").first
+    @regular_philister_group = @corporation.descendant_groups.where(name: "Philister").first
     @philisterschaft_group.create_erstbandphilister_parent_group
     @philister_user = create(:user_with_account)
     @regular_philister_group.assign_user @philister_user, at: 10.minutes.ago
 
     login(@philister_user)
+  end
+  
+  specify 'requirements' do
+    @corporation.descendant_groups.where(name: "Erstbandphilister").first.members.should include @philister_user
   end
 
   scenario "visit corporation site and navigate to the erstbandphilister site" do

spec/models/horizontal_nav_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe HorizontalNav do
   
   before do
-    @user = create(:user)
-    @corporation_a = create(:wah_group)
-    @corporation_b = create(:wah_group)
-    @bv = create(:bv_group)
+    @user = create :user
+    @corporation_a = create :wingolf_corporation
+    @corporation_b = create :wingolf_corporation
+    @bv = create :bv_group
     
     @horizontal_nav = HorizontalNav.new(user: @user, current_navable: Page.find_intranet_root)
   end
@@ -37,6 +37,8 @@ describe HorizontalNav do
         @corporation_a.status_group("Philister") << @user
         @membership = @corporation_b.status_group("Philister").assign_user @user, at: 1.hour.ago
         @membership.move_to @corporation_b.status_group("Schlicht Ausgetretene")
+        
+        @horizontal_nav = HorizontalNav.new(user: @user, current_navable: Page.find_intranet_root)
       end
       it "should include the corporations the user is still member in" do
         subject.should include @corporation_a.becomes(Group)

spec/models/user_spec.rb
@@ -105,6 +105,15 @@ describe User do
     it "should not use the wrong order (bug fix)" do
       subject.should_not == "H08 E06"
     end
+    context "when joining an event of a corporation" do
+      before do
+        @corporationZ = create :wingolf_corporation, token: 'Z'
+        @event = @corporationZ.child_events.create
+        @event.attendees << @user
+        @user.reload
+      end
+      it { should_not include @corporationZ.token }
+    end
   end
 
   describe "#cached(:aktivitaetszahl)" do

vendor/engines/your_platform/app/helpers/groups_helper.rb
@@ -33,14 +33,14 @@ module GroupsHelper
   end
   
   def cached_groups_of_user_table(user)
-    Rails.cache.fetch(['groups_of_user_table', user.membership_ids]) { groups_of_user_table(user) }
+    Rails.cache.fetch([user, 'groups_of_user_table'], expires_in: 1.week) { groups_of_user_table(user) }
   end
   
   private
 
   def membership_li( user, group )
     content_tag :li do
-      c = link_to group.title, group
+      c = link_to group.extensive_name, group
       membership = UserGroupMembership.with_invalid.find_by_user_and_group( user, group )
       c += remove_button( membership ) if membership.destroyable? and can?(:destroy, membership)
       c

vendor/engines/your_platform/app/models/group_mixins/corporations.rb
@@ -121,11 +121,18 @@ module GroupMixins::Corporations
     # This is used, for example, in the my-groups view, where the corporations groups
     # are displayed separately.
     #
+    # Some special groups are excluded manually, since they are not expected to show up
+    # in the groups list, e.g. attendee groups of corporation events.
+    #
     def find_non_corporations_branch_groups_of( user )
       ancestor_groups = user.groups
       corporations_branch = self.find_corporations_branch_groups
       corporations_branch = [] unless corporations_branch
-      return ancestor_groups - corporations_branch
+      return ancestor_groups - 
+        corporations_branch - 
+        Group.find_all_by_flag(:attendees) -
+        Group.find_all_by_flag(:contact_people) -
+        Group.find_all_by_flag(:officers_parent)
     end
   end
 

commit 96ec11ad4be57a22138fda0a3f07d1f77e372fa2

fixing Group#extensive_name getter.
was broken in https://github.com/fiedl/wingolfsplattform/commit/9d3729e431d6ae1e28316a7edca5f2a6ccf4aa95.

vendor/engines/your_platform/app/models/group.rb
@@ -73,7 +73,7 @@ class Group < ActiveRecord::Base
     elsif has_flag? :admins_parent
       name + ": " + parent_groups.first.parent_groups.first.name
     else
-      name
+      super
     end
   end
   