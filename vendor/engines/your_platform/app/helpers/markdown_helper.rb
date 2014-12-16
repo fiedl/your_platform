4c6925f1ff858589fb5143daada9b48a644fa54b
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-12-16 18:12:53 +0100

    markdown: do not filter html. admins and editors are allowed to use html.

--- vendor/engines/your_platform/app/helpers/markdown_helper.rb
+++ vendor/engines/your_platform/app/helpers/markdown_helper.rb
@@ -6,8 +6,8 @@ module MarkdownHelper
   #   * http://daringfireball.net/projects/markdown/syntax
   #
   def markdown(text, options = nil)
-    options ||= { hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true, fenced_code: true, gh_blockcode: true }
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render(sanitize(text) || "").html_safe
+    options ||= { hard_wrap: true, filter_html: false, autolink: true, no_intraemphasis: true, fenced_code: true, gh_blockcode: true }
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render(text || "").html_safe
   end
 end
 
