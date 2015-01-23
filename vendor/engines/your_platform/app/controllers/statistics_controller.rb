73a425b2e9101e0f989f43e0c3e60c99e803c0d1
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-01-23 11:58:53 +0100

    statistics: Let the statistics_controller handle the csv export as well.
    This way, the authorization is handled correctly. Also, this is a
    preparation for the next step, where several statistics are served by
    the statistics_controller via ajax.

--- vendor/engines/your_platform/app/controllers/statistics_controller.rb
+++ vendor/engines/your_platform/app/controllers/statistics_controller.rb
@@ -4,6 +4,16 @@ class StatisticsController < ApplicationController
   
   def index
     authorize! :index, :statistics
+    
+    @corporation_joining_statistics_export = ListExport.new(Group.corporations_parent, 'join_statistics')
+    
+    respond_to do |format|
+      format.html  # render view
+      format.csv do
+        bom = "\xEF\xBB\xBF".force_encoding('utf-8') # UTF-8
+        send_data (bom + @corporation_joining_statistics_export.to_csv), filename: ("statistics #{Time.zone.now}".parameterize + ".csv")
+      end
+    end
   end
   
 end
