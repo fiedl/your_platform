31db91e6f64ba4b37ae510d6e61e7ee1b41c6822
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-02-06 02:41:27 +0100

    bootstrap 3: trying to add missing glyphicons from bootstrap 2.
    https://github.com/marcoceppi/bootstrap-glyphicons

--- vendor/engines/your_platform/app/helpers/glyphicon_helper.rb
+++ vendor/engines/your_platform/app/helpers/glyphicon_helper.rb
@@ -1,11 +1,23 @@
 module GlyphiconHelper
   
   def icon(icon_key)
-    glyphicon(icon_key)
+    if icon_key.to_s.in? ['beer', 'coffee']
+      icon_key = "icon-#{icon_key}"
+    end
+    if icon_key.to_s.start_with? 'icon-'
+      bootstrap2_icon(icon_key)
+    else
+      glyphicon(icon_key)
+    end
   end
 
   def glyphicon(icon_key)
-    content_tag :span, '', class: "glyphicon glyphicon-#{icon_key}", 'aria-hidden' => true
+      content_tag :span, '', class: "glyphicon glyphicon-#{icon_key}", 'aria-hidden' => true
+  end
+  
+  def bootstrap2_icon(icon_key)
+    icon_key = "icon-#{icon_key}" unless icon_key.to_s.start_with? 'icon-'
+    content_tag :i, '', class: "icon-large #{icon_key}"
   end
   
 end
