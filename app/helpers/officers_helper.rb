a5cdad1076560122f9223902684afcbeafce7381
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-02-18 21:52:37 +0100

    Amtsträger-Sortierung: Erst die Chargen, dann Nebenämter, dann Admins.
    (Eingabe Jona Kornder).

--- app/helpers/officers_helper.rb
+++ app/helpers/officers_helper.rb
@@ -5,13 +5,32 @@ module OfficersHelper
   
   def sort_officer_groups(officer_groups)
     officer_groups.sort_by do |group|
-      [:senior, :fuxmajor, :kneipwart, :phil_x, :schriftwart, 
-        :kassenwart, 
-        :alle_seniores, :alle_fuxmajores, :alle_kneipwarte,
-        :main_admins, :main_admins_parent, 
-        :admins, :admins_parent, :chargen]
-        .index(group.flags.first.try(:to_sym)) || (100 + group.id)
+      officer_group_sort_score(group)
     end
+  end
+  
+  # This group determines the sort order of officer groups
+  # in the officers box.
+  #
+  def officer_group_sort_score(group)
+    flag = group.flags.first.try(:to_sym)
+    #
+    # Ganz oben stehen die Chargen, Philister-x, Schriftwart und 
+    # Kassenwart.
+    # 
+    [:senior, :fuxmajor, :kneipwart, :phil_x, :schriftwart, :kassenwart, 
+    :alle_seniores, :alle_fuxmajores, :alle_kneipwarte]
+    .index(flag) ||
+    #
+    # Ganz unten stehen die Administratoren und die Chargen-Definition.
+    # (Letztere ist die Chargen-Gruppe, die die Einzel-Chargen enthält.)
+    #
+    [:main_admins, :main_admins_parent, :admins, :admins_parent, :chargen]
+    .index(flag).try(:+, 2e6) ||
+    #
+    # Dazwischen kommen alle übrigen Ämter.
+    #
+    (group.id + 1e6)
   end
   
 end
