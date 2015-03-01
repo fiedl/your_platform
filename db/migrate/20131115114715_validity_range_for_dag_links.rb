73dd171bfc6718d2fe678e78f0e55162a00cf314
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-03-01 07:50:24 +0100

    removing old data migration from database migration file

--- db/migrate/20131115114715_validity_range_for_dag_links.rb
+++ db/migrate/20131115114715_validity_range_for_dag_links.rb
@@ -3,13 +3,13 @@ class ValidityRangeForDagLinks < ActiveRecord::Migration
     change_table :dag_links do |t|
       t.rename :deleted_at, :valid_to
       t.datetime :valid_from
-      DagLink.connection.execute "UPDATE `dag_links` SET `valid_from`=`created_at` WHERE 1"
+      #DagLink.connection.execute "UPDATE `dag_links` SET `valid_from`=`created_at` WHERE 1"
     end
   end
   def down
     change_table :dag_links do |t|
       t.rename :valid_to, :deleted_at
-      DagLink.connection.execute "UPDATE `dag_links` SET `created_at`=`valid_from` WHERE `valid_from` IS NOT NULL"   
+      #DagLink.connection.execute "UPDATE `dag_links` SET `created_at`=`valid_from` WHERE `valid_from` IS NOT NULL"   
       t.remove_column :valid_from
     end
   end
