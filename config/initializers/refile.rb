62a71cfe5ae06fb046f5826cc5f77cb7bf93ec2a
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-03-14 19:55:54 +0100

    using asset host as refile host. this is required when using https.

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,6 +4,8 @@ Refile.automount = false
 Refile.store = Refile::Backend::FileSystem.new(Rails.root.join("uploads/refile/store").to_s)
 Refile.cache = Refile::Backend::FileSystem.new(Rails.root.join("uploads/refile/cache").to_s)
 
+Refile.host = Rails.configuration.asset_host if Rails.configuration.asset_host.present?
+
 # Add in missing methods for file uploads in Rails < 4
 ActionDispatch::Http::UploadedFile.class_eval do
   unless instance_methods.include?(:eof?)
