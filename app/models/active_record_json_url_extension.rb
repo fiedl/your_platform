ebfd7d90fb6506ed03f3190957ccddbd1b7bf055
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-03-29 15:34:56 +0200

    rails 4: fixing model url helper
    * Code refactoring.
    * Including the necessary url options in the dummy app configuration.

--- app/models/active_record_json_url_extension.rb
+++ app/models/active_record_json_url_extension.rb
@@ -1,16 +1,16 @@
 module ActiveRecordJsonUrlExtension
   extend ActiveSupport::Concern
 
-  def serializable_hash( options = {} )
-    options[ :methods ] = [ :url ] if not options[ :methods ]
-    options[ :methods ] = [ options[ :methods ] ] if options[ :methods ].kind_of? Symbol
-    options[ :methods ] << :url if options[ :methods ]
-    raise 'options[ :methods ] should be an array' unless options[ :methods ].kind_of? Array
+  def serializable_hash(options = {})
+    options[:methods] = [:url] if not options[:methods]
+    options[:methods] = [options[:methods]] if options[:methods].kind_of? Symbol
+    options[:methods] << :url if options[:methods]
+    raise 'options[:methods] should be an array' unless options[:methods].kind_of? Array
     super options
   end
 
   def url
-    UrlHelper.new( self ).url
+    UrlHelper.new(self).url
   end
   
   # The following class generates a scope that prevents the url helpers
@@ -22,30 +22,27 @@ module ActiveRecordJsonUrlExtension
   #
   #   my_instance = MyModel.create()
   #
-  # You may call <tt>my_instance.url()</tt> to get the same result as for
-  # <tt>url_for(my_instance)</tt>.
+  # You may call `my_instance.url()` to get the same result as for
+  # `url_for(my_instance)`.
   # But you should not be able to call, for example, 
-  # <tt>my_instance.my_model_path( ... )</tt>.
+  # `my_instance.my_model_path( ... )`.
   #
   class UrlHelper
-
-    include Rails.application.routes.url_helpers if Rails.version.starts_with?("3")
+    include Rails.application.routes.url_helpers
     include ActionDispatch::Routing::UrlFor
 
-    def initialize( obj )
+    def initialize(obj)
       @obj = obj
     end
     
     def url_options
-      Rails.application.config.action_mailer.default_url_options
+      Rails.application.config.action_mailer.default_url_options || raise("Please set 'config.action_mailer.default_url_options = {host: ...}' in the application config.")
     end
-
-    def url
-      url_for @obj
-    end
-
-  end
   
+    def url
+      url_for(@obj)
+    end
+  end
 end
 
-ActiveRecord::Base.send( :include, ActiveRecordJsonUrlExtension )
+ActiveRecord::Base.send(:include, ActiveRecordJsonUrlExtension)
--- demo_app/my_platform.rails4/config/environments/development.rb
+++ demo_app/my_platform.rails4/config/environments/development.rb
@@ -40,4 +40,5 @@ Rails.application.configure do
   
   # Mailer Settings
   config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000, protocol: 'http'}
 end
