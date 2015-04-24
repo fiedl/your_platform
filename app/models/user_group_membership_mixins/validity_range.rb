3522c859c5d8ec855804a86facd1624ed5a533fe
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-04-24 20:49:10 +0200

    validity range: use scopes rather than methods after rails-4 update.
    separating validity perspective and time perspective.
    
    trello: https://trello.com/c/Cdpfg3hM/812-validityrange-nach-rails-4-update-refaktorieren

--- app/models/user_group_membership_mixins/validity_range.rb
+++ app/models/user_group_membership_mixins/validity_range.rb
@@ -3,37 +3,68 @@
 # They can begin at some time and end at some time. This is expressed by the
 # ValidityRange of a membership.
 #
-# Examples:
+#
+# ## Examples
 #
 #     membership.valid_from  # =>  time
 #     membership.valid_to    # =>  time
 #     membership.invalidate
-# 
-# Scopes:
 #
-#     UserGroupMembership.with_invalid
-#     UserGroupMembership.only_valid
-#     UserGroupMembership.only_invalid
-#     UserGroupMembership.at_time(time)
 # 
-# Default Scope: 
+# ## Scopes
+# 
+#   The same functionality can be described from tho different perspectives:
+# 
+#   From the "validity perspective", a membership can be currently valid or
+#   invalid. One can filter memberships by their validity status.
+#   
+#   From the "time perspective", there are current memberships and past
+#   memberships. 
+#   
+#   These perspectives are linked by the fact that "past memberships" are just
+#   memberships that are currently invalid but have been valid in the past.
+#   
+#   Validity Perspective:    
+#   
+#       UserGroupMembership.valid
+#       UserGroupMembership.invalid
+#       UserGroupMembership.with_invalid
+#   
+#   Time Perspective: 
+# 
+#       UserGroupMembership.now
+#       UserGroupMembership.past
+#       UserGroupMembership.now_and_past
+#       UserGroupMembership.now_and_in_the_past
+#       UserGroupMembership.at_time(time)
+#   
+#   Default Scope: 
 #
-# By default, the `only_valid` scope is applied, i.e. only memberships are 
-# found that are valid at present time. To override this scope, use either
-# `with_invalid` or `unscoped`.
+#   By default, the `valid` scope is applied, i.e. only memberships are 
+#   found that are valid at present time. To override this scope, use the
+#   either `with_invalid` scope.
 #
-# Caveats:
-# * There is only one valid_from and one valid_to time per object. 
-#   Therefore, you can't keep track of first archiving an object and later 
-#   un-archiving it. Un-archiving an object loses the information of first archiving it.
-# * Currently, the future is not handled (Article.future and article.archive at: 1.hour.from.now 
-#   do not work.) But this is planned to be implemented in the future.
 # 
-# Some functionality has been extracted out into the temporal_scopes gem in order to 
-# test the scopes easily. But, this code has been abandoned since the Rails-4 migration
-# took more time.
-#   => https://github.com/fiedl/temporal_scopes/blob/master/lib/temporal_scopes/has_temporal_scopes.rb
+# ## Caveats
 # 
+#   * There is only one `valid_from` and one `valid_to` time per object. 
+#     Therefore, you can't keep track of first invalidating an object and 
+#     later re-validating it. Re-validating an object loses the information
+#     of first invalidating it.
+# 
+#   * Currently, the future is not handled (`Article.future` and 
+#     `article.invalidate at: 1.hour.from.now` do not work.) But this is 
+#     planned to be implemented in the future.
+# 
+#   * Some functionality has been extracted out into the temporal_scopes gem 
+#     in order to test the scopes easily. But, this code has been abandoned 
+#     since the Rails-4 migration took more time.
+#     => https://github.com/fiedl/temporal_scopes/blob/master/lib/temporal_scopes/has_temporal_scopes.rb
+# 
+#   * Rails 5 supports an `.or(...)` syntax: 
+#     https://github.com/rails/rails/pull/16052
+#     TODO: Refactor the queries in the scopes when migrating to Rails 5.
+#
 module UserGroupMembershipMixins::ValidityRange
   
   extend ActiveSupport::Concern
@@ -42,36 +73,45 @@ module UserGroupMembershipMixins::ValidityRange
     attr_accessible :valid_from, :valid_to, :valid_from_localized_date, :valid_to_localized_date
     before_validation :set_valid_from_to_now
     
-    default_scope { only_valid }
+    default_scope { valid }
+    
+    # Validity Perspective
+    # TODO: Allow :valid to include memberships that BECOME valid in the future.
+    scope :valid, -> { where("valid_from IS NULL OR valid_from <= ?", Time.zone.now).where("valid_to IS NULL OR valid_to >= ?", Time.zone.now) }
+    scope :invalid, -> { with_invalid.where("valid_to < ?", Time.zone.now) }
+    # scope :with_invalid  # This is defined as method below due to some issues.
+    scope :only_valid, -> { valid }
+    scope :only_invalid, -> { invalid }
+    
+    # Time Perspective
+    scope :now, -> { valid }
+    scope :past, -> { invalid }
+    scope :in_the_past, -> { invalid }
+    scope :with_past, -> { with_invalid }
+    scope :now_and_past, -> { with_invalid }
+    scope :now_and_in_the_past, -> { with_invalid }
+    scope :at_time, -> (time) { with_past.where("valid_from IS NULL OR valid_from <= ?", time).where("valid_to IS NULL OR valid_to >= ?", time) }
+    scope :this_year, -> { with_invalid.where("valid_from >= ?", "#{Time.zone.now.year}-01-01 00:00:00") }
+    scope :started_after, -> (time) { where('NOT valid_from IS NULL').where("valid_from >= ?", time) }
   end
   
-  
-  # Attributes in the database
-  # ====================================================================================================
-  
-  def valid_from_localized_date
-    self.valid_from ? I18n.localize(self.valid_from.try(:to_date)) : ""
-  end
-  def valid_from_localized_date=(new_date)
-    self.valid_from = new_date.to_datetime
-    valid_from_will_change!
-  end
-
-  def set_valid_from_to_now(force = false)
-    self.valid_from ||= Time.zone.now if self.new_record? or force
-    return self
-  end
-  
-  def valid_to_localized_date
-    self.valid_to ? I18n.localize(self.valid_to.try(:to_date)) : ""
-  end
-  def valid_to_localized_date=(new_date)
-    if new_date == "-"
-      self.valid_to = nil
-    else
-      self.valid_to = new_date.to_datetime
+  module ClassMethods
+    # This scope widens the query such that also memberships that are not valid 
+    # at the present time are returned.
+    # 
+    # Have a look at `rewhere`:
+    # https://github.com/rails/rails/commit/f950b2699f97749ef706c6939a84dfc85f0b05f2#diff-bf6dd6226db3aab589916f09236881c7R562
+    #
+    # But `rewhere` is not enough. We need more filtering:
+    # https://github.com/fiedl/temporal_scopes/blob/master/lib/temporal_scopes/has_temporal_scopes.rb
+    # 
+    # TODO: Check if this still needs the extra filter when migrating to Rails 5.
+    # 
+    def with_invalid
+      relation = unscope(where: [:valid_from, :valid_to])
+      relation.where_values.delete_if { |query| query.to_s.include?("valid_from") || query.to_s.include?("valid_to") } 
+      relation
     end
-    valid_to_will_change!
   end
   
   
@@ -126,103 +166,42 @@ module UserGroupMembershipMixins::ValidityRange
   def valid_at?(time)
     (self.valid_from == nil || self.valid_from <= time) && (self.valid_to == nil || self.valid_to >= time)
   end
-
+  
   # This method checks whether the present time lies within the validity range
   # of the membership.
   #
   def currently_valid?
     valid_at?(Time.zone.now)
   end
-
-
-  # Temporal scopes
+  
+  
+  # Attributes in the database
   # ====================================================================================================
   
-  module ClassMethods
-  
-    # This scope returns limits the query to memberships whose validity ranges match the
-    # given time.
-    #
-    # Example:
-    #
-    #     UserGroupMembership.find_all_by_user( u ).at_time( 1.hour.ago ).count
-    #
-    def at_time( time )
-      with_invalid
-        .where("valid_from IS NULL OR valid_from <= ?", time)
-        .where("valid_to IS NULL OR valid_to >= ?", time)
-    end
-    
-    # This scope limits the query to memberships that are valid at the present time.
-    # This is the default bahaviour. 
-    #
-    # Rails 5 will support `.or(...)`: https://github.com/rails/rails/pull/16052
-    # TODO: Refactor when migrating to Rails 5.
-    #
-    def only_valid
-      where("valid_from IS NULL OR valid_from <= ?", Time.zone.now)
-      .where("valid_to IS NULL OR valid_to >= ?", Time.zone.now)
-    end
-    
-    # This scope widens the query such that also memberships that are not valid at the
-    # present time are returned.
-    # 
-    # Have a look at `rewhere`.
-    # https://github.com/rails/rails/commit/f950b2699f97749ef706c6939a84dfc85f0b05f2#diff-bf6dd6226db3aab589916f09236881c7R562
-    #
-    # But `rewhere` is not enough. We need more filtering:
-    # https://github.com/fiedl/temporal_scopes/blob/master/lib/temporal_scopes/has_temporal_scopes.rb
-    # 
-    # TODO: Check if this still needs the extra filter when migrating to Rails 5.
-    # 
-    def with_invalid
-      relation = unscope(where: [:valid_from, :valid_to])
-      relation.where_values.delete_if { |query| query.to_s.include?("valid_from") || query.to_s.include?("valid_to") } 
-      relation
-    end
-    
-    # This scope limits the query to memberships that are invalid at the present time.
-    # 
-    def only_invalid
-      with_invalid.where("valid_to < ?", Time.zone.now)
-    end
-    
-    # This scope limits the query to memberships that are valid at the present time.
-    # This is the default bahaviour. 
-    #
-    def now
-      only_valid
-    end
+  def valid_from_localized_date
+    self.valid_from ? I18n.localize(self.valid_from.try(:to_date)) : ""
+  end
+  def valid_from_localized_date=(new_date)
+    self.valid_from = new_date.to_datetime
+    valid_from_will_change!
+  end
 
-    # This scope limits the query to memberships that are invalid at the present time.
-    # 
-    def in_the_past
-      only_invalid
-    end
-    def past
-      in_the_past
-    end
-
-    # This scope widens the query such that also memberships that are not valid at the
-    # present time are returned.
-    # 
-    def now_and_in_the_past
-      with_invalid
-    end
-    def with_past
-      now_and_in_the_past
-    end
-    
-    def this_year
-      with_invalid.where("valid_from >= ?", "#{Time.zone.now.year}-01-01 00:00:00")
-    end
-    
-    def started_after(time)
-      where('NOT valid_from IS NULL').where("valid_from >= ?", time)
-    end
-  
+  def set_valid_from_to_now(force = false)
+    self.valid_from ||= Time.zone.now if self.new_record? or force
+    return self
   end
   
+  def valid_to_localized_date
+    self.valid_to ? I18n.localize(self.valid_to.try(:to_date)) : ""
+  end
+  def valid_to_localized_date=(new_date)
+    if new_date == "-"
+      self.valid_to = nil
+    else
+      self.valid_to = new_date.to_datetime
+    end
+    valid_to_will_change!
+  end
 end
 
 class Array
