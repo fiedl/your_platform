9a3e4b02814766799428a4af57638f322cb9cf0d
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-04-28 17:51:03 +0200

    set mini magick timeout to 60 seconds.
    trello: https://trello.com/c/uiM1SaEi/831-grosse-uploads

---
+++ config/initializers/mini_magick.rb
@@ -0,0 +1,8 @@
+# Image Manipulation using MiniMagick
+#
+# https://github.com/minimagick/minimagick
+# http://www.rubydoc.info/github/minimagick/minimagick/MiniMagick/Configuration
+#
+MiniMagick.configure do |config|
+  config.timeout = 60  # seconds
+end
