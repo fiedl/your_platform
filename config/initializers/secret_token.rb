7d2001c6bac90eba5f062bf7c6cacccf54e4f626
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-04-30 20:12:59 +0200

    reverting secret_token configuration to the old syntax in order to fix devise secret_key setting.
    http://stackoverflow.com/a/22584303/2066546

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,1 @@
 Rails.application.config.secret_key_base ||= Rails.application.secrets.secret_key_base
-Rails.application.config.secret_key ||= Rails.application.config.secret_key_base
--- demo_app/my_platform.rails4/config/initializers/secret_token.rb
+++ demo_app/my_platform.rails4/config/initializers/secret_token.rb
@@ -1,1 +1,2 @@
-Rails.application.config.secret_key_base = Rails.application.secrets.secret_key_base
+# Updating this configuration line to the newer syntax might break devise secret_key.
+MyPlatform::Application.config.secret_key_base = Rails.application.secrets.secret_key_base
