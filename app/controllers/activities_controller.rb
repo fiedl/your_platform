88439ae0d50d78ca9b0d0047448ba1f433558ae8
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-05-12 08:41:48 +0200

    activities#index: reduce number of entries to reduce browser memory need.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,7 +6,7 @@ class ActivitiesController < ApplicationController
     
     @activities = PublicActivity::Activity
     @activities = @activities.where(owner: current_user.administrated_objects.select { |o| o.kind_of?(User) }) unless Role.of(current_user).global_admin?
-    @activities = @activities.order('created_at desc').limit(1000)
+    @activities = @activities.order('created_at desc').limit(100)
     
     @title = t(:activity_log)
   end
