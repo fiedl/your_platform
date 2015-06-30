4ac02fbd526c39fab0d8a2f3f843a320fa761dca
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-06-30 02:45:54 +0200

    attachments: adding spec to make sure that attachment download raises unauthorized if the user has no right to download the attachment.
    See also: Discussion at: https://github.com/fiedl/your_platform/commit/4a13e2dd40fe3374c0152a42e72510bca779bdb6

---
+++ spec/features/attachments_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+feature "Attachments" do
+  include SessionSteps
+
+  before do
+    @user = create :user_with_account
+    @group = create :group
+    @page = @group.child_pages.create
+    @attachment = create :attachment, parent_id: @page.id, parent_type: 'Page'
+  end
+
+  context "when the user has the right to download the attachment" do
+    before do
+      @group << @user
+      login @user
+    end
+    scenario "download the attachment" do
+      visit @attachment.file.url
+      # Checking the pdf content will raise an error, "invalid byte sequence in UTF-8",
+      # because this test tool can't read the pdf. But the error means, the pdf has
+      # been opened successfully, i.e. no "access denied".
+      #
+      expect { page.has_no_content? I18n.t(:access_denied) }.to raise_error(ArgumentError)
+    end
+  end
+
+  context "when the user has no right to download the attachment" do
+    before do
+      login @user
+    end
+    scenario "download the attachment", :js do
+      visit @attachment.file.url
+      page.should have_content I18n.t(:access_denied)
+    end
+  end
+
+end
