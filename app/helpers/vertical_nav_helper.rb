3b6898cf07b8f6a99e875ecd106f6a778c65a69f
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-07-01 14:54:10 +0200

    fixing issue where vertical nav has mistakenly not be shown.

--- app/helpers/vertical_nav_helper.rb
+++ app/helpers/vertical_nav_helper.rb
@@ -1,12 +1,12 @@
 module VerticalNavHelper
-  
+
   # Returns the html code for the vertical navigation, i.e. the vertical menu.
   #
   def vertical_nav
     render partial: 'layouts/vertical_nav'
   end
 
-  # Generates the HTML code for a vertical menu, where the given navable object, 
+  # Generates the HTML code for a vertical menu, where the given navable object,
   # e.g. a Page or a User, is the currently active element.
   #
   def vertical_menu_for(navable)
@@ -16,29 +16,29 @@ module VerticalNavHelper
         @active_navable = navable
         @active_navables = [@active_navable]
         @child_navables = cached_child_navables(navable)
-        
+
         menu_elements_html(@ancestor_navables, :ancestor) +
         menu_element(@active_navable, :active) +
         menu_elements_html(@child_navables, :child)
       end
     end
   end
-  
+
   def show_vertical_nav?
-    not @hide_vertical_nav && Rails.cache.fetch([@navable, "show_vertical_nav?"]) do
+    (not @hide_vertical_nav) && @navable && Rails.cache.fetch([@navable, "show_vertical_nav?"]) do
       @navable.present? && (@navable != Page.find_root) && (@navable.children.count + @navable.ancestors.count > 1)
     end
   end
 
   def child_navables(navable)
-    non_hidden_navables(navable.navable_children, :child).sort_by do |object| 
+    non_hidden_navables(navable.navable_children, :child).sort_by do |object|
       [ ['Page', 'Group'].index(object.class.base_class.name),  # pages above groups
       object.created_at]
     end
   end
 
   private
-  
+
   def cached_ancestor_navables(navable)
     NavNode
     Rails.cache.fetch([navable, 'ancestor_navables', navable.ancestors_cache_key], expires_in: 1.week) { ancestor_navables(navable) }
@@ -46,13 +46,13 @@ module VerticalNavHelper
   def ancestor_navables(navable)
     non_hidden_navables(navable.nav_node.ancestor_navables, :ancestor)
   end
-  
+
   def cached_child_navables(navable)
     #NavNode
     #Rails.cache.fetch([navable, 'child_navables', navable.children_cache_key], expires_in: 1.week) { child_navables(navable) }
     child_navables(navable)
   end
-  
+
   def non_hidden_navables(navables, element_class)
     navables.select do |navable|
       not(navable.nav_node.hidden_menu and element_class == :child) and
@@ -67,13 +67,13 @@ module VerticalNavHelper
   end
 
   def menu_element( object, element_class )
-    content_tag( :li, :class => "#{element_class} #{object.class.name.downcase}" ) do 
+    content_tag( :li, :class => "#{element_class} #{object.class.name.downcase}" ) do
       link_to(menu_element_title(object), current_tab_path(object), data: {
         vertical_nav_path: vertical_nav_path(navable_type: object.class.base_class.name, navable_id: object.id)
       })
     end
   end
-  
+
   def menu_element_title(object)
     if append_corporation_to_menu_element_title?(object)
       "#{object.title} (#{object.corporation.title})"
@@ -81,7 +81,7 @@ module VerticalNavHelper
       object.title
     end
   end
-  
+
   def append_corporation_to_menu_element_title?(object)
     object.kind_of?(Group) &&
       object.corporation &&
