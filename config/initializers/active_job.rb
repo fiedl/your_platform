50d2e9c2e6faa7de3426d24de77c72f30780b0d0
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-07-10 16:03:10 +0200

    configure sidekiq queues in initializer.

--- config/initializers/active_job.rb
+++ config/initializers/active_job.rb
@@ -1,12 +1,4 @@
 Rails.application.config.active_job.queue_adapter = :sidekiq
 
 # redis is already namspaced
-Rails.application.config.active_job.queue_name_prefix = "" 
-
-# Please do not forget to defined the queues in the main app:
-#
-#     ➜ cat config/sidekiq.yml
-#     :queues:
-#       - default
-#       - notification
-#
+Rails.application.config.active_job.queue_name_prefix = ""
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,14 @@
 Sidekiq.default_worker_options = { 'backtrace' => true, retry: false }
 
+Sidekiq.options[:queues] = ['default', 'mailgate']
+
+# https://github.com/brainopia/sidekiq-limit_fetch
+#
+Sidekiq::Queue['default'].limit = 25
+Sidekiq::Queue['mailgate'].limit = 1
+
 # http://stackoverflow.com/questions/14825565/sidekiq-deploy-to-multiple-environments
 # 
-
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost:6379/0', namespace: "#{::STAGE}_sidekiq" }
 end
