88d6f60c0be9f6938c1c8adc6fa54d40ec81221e
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-07-14 22:06:32 +0200

    comments: fixing issue where ui does not respond to comment submission.
    1. it's `comment.author`, not `comment.user`.
    2. Comment out badge rules for the moment. This will come in gamification (https://trello.com/c/Sl2cZspV/886-gamification).
    
    trello: https://trello.com/c/S0dJ3toY/895-dringende-korrekturen-an-e-mail-verteilern

--- app/models/merit/badge_rules.rb
+++ app/models/merit/badge_rules.rb
@@ -26,10 +26,10 @@ module Merit
       # Find badge by badge_id, badge_id takes presidence over badge
       # grant_on 'users#create', badge_id: 7, badge: 'just-registered', to: :itself
 
-      # If it has 10 comments, grant commenter-10 badge
-      grant_on 'comments#create', badge: 'commentator', level: 10 do |comment|
-        comment.user.comments.count == 10
-      end
+      # # If it has 10 comments, grant commenter-10 badge
+      # grant_on 'comments#create', badge: 'commentator', level: 10 do |comment|
+      #   comment.author.comments.count == 10
+      # end
 
       # If it has 5 votes, grant relevant-commenter badge
       # grant_on 'comments#vote', badge: 'relevant-commenter',
