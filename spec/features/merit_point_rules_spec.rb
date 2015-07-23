ae5af590222465c296f345b1ed5bb43e7b80988c
Joachim Back <joachim.back@web.de>
2015-07-23 23:10:34 +0200

    login after adding to group

--- spec/features/merit_point_rules_spec.rb
+++ spec/features/merit_point_rules_spec.rb
@@ -5,17 +5,18 @@ feature "Merit Point Rules" do
   
   background do
     @user = create :user_with_account
-    @points = @user.points
-    @group = create(:group)
-    @event1 = @group.child_events.create name: "Gamification Hacking Workshop", start_at: 1.day.from_now
-    @event2 = @group.child_events.create name: "Another meeting", start_at: 10.day.from_now
-    @group.assign_user @user, at: 1.year.ago
    end
   
   scenario "joining event earns 1 point" do
-    login @user
+    # setting up scenario
+    @points = @user.points
+    @group = create(:group)
+    @group.assign_user @user, at: 1.year.ago
+    @event1 = @group.child_events.create name: "Gamification Hacking Workshop", start_at: 1.day.from_now
+    @event2 = @group.child_events.create name: "Another meeting", start_at: 10.day.from_now
 
     # Just looking at events#index does nothing.
+    login @user
     visit root_path
     @user.reload.points.should == @points
 
@@ -25,6 +26,8 @@ feature "Merit Point Rules" do
     page.should have_selector '#join_event', visible: true
     find('#join_event').click
     @user.reload.points.should == @points+1
+
+    visit root_path
     within('.box.upcoming_events') { click_on @event2.name }
     page.should have_selector '#join_event', visible: true
     find('#join_event').click
