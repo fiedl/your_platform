5e354b827d49e37bb6d1c511d4e2a84dd97e1397
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-10-20 21:27:21 +0200

    projects: migrating dummy app table

---
+++ demo_app/my_platform/db/migrate/20151020181604_create_projects.your_platform.rb
@@ -0,0 +1,11 @@
+# This migration comes from your_platform (originally 20151011125623)
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
--- demo_app/my_platform/db/schema.rb
+++ demo_app/my_platform/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930184703) do
+ActiveRecord::Schema.define(version: 20151020181604) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -296,6 +296,13 @@ ActiveRecord::Schema.define(version: 20150930184703) do
   add_index "profile_fields", ["profileable_id", "profileable_type", "type"], name: "profileable_type", using: :btree
   add_index "profile_fields", ["profileable_id", "profileable_type"], name: "profileable", using: :btree
   add_index "profile_fields", ["type"], name: "type", using: :btree
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.string   "name",       limit: 255
