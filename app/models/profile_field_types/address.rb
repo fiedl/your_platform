6ed4c9c0f02df995607362a6eaad144bd27d7776
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2015-11-16 08:40:16 +0100

    separate address fields: In `ProfileFieldTypes::Address`,
    have the address information be stored in separate child profile fields.
    
    Trello:
    https://trello.com/c/TH9fpsrK/928-anpassung-des-adress-systems-nach-neue
    n-problemen-mit-den-wingolfblattern-2015-zie8ephi

--- app/models/profile_field_types/address.rb
+++ app/models/profile_field_types/address.rb
@@ -4,90 +4,147 @@ module ProfileFieldTypes
   # 
   class Address < ProfileField
     def self.model_name; ProfileField.model_name; end
+    
+    # def display_html
+    #   ActionController::Base.helpers.simple_format self.value
+    # end
+    
+    
+    concerning :SubFields do
+      included do
+        has_child_profile_fields :first_address_line, :second_address_line, :postal_code, :city, :region, :country_code
 
-    attr_accessible :postal_address if defined? attr_accessible
+        def street_with_number
+          self.get_field(:first_address_line) || geo_information(:street)
+        end
+        
+        def country
+          #self.get_field(:country) || geo_information(:country)
+        end
+        
+        def country_code
+          self.get_field(:country_code) || geo_information(:country_code) || default_country_code
+        end
+        
+        def default_country_code
+          'de'
+        end
+        
+        def city
+          self.get_field(:city) || geo_information(:city)
+        end
+        
+        def postal_code
+          self.get_field(:postal_code) || geo_information(:postal_code)
+        end
+        
+        def state # state/province/region
+          geo_information(:state)
+        end
+        
+        def composed_address
+          Biggs::Formatter.new(blank_country_on: default_country_code).format(
+            get_field(:country_code),
+            street: get_field(:first_address_line),
+            city: get_field(:city),
+            zip: get_field(:postal_code),
+            state: get_field(:region)
+          ).strip
+        end
+        
+        def value
+          if first_address_line.present?
+            composed_address
+          else
+            read_attribute :value
+          end
+        end
+
+
+    end
 
     # Google Maps integration
     # see: http://rubydoc.info/gems/gmaps4rails/
+    #
     acts_as_gmappable
+    concerning :GoogleMapsIntegration do
+      def gmaps4rails_address
+        self.value
+      end
+
+      def gmaps
+        true
+      end
+    end
     
-    def geo_location
-      find_or_create_geo_location
+    concerning :GeoCoding do
+      def geo_location
+        find_or_create_geo_location
+      end
+      
+      def find_geo_location
+        @geo_location ||= GeoLocation.find_by_address(value)
+      end
+      
+      def find_or_create_geo_location
+        @geo_location ||= GeoLocation.find_or_create_by address: value if self.value && self.value != "—"
+      end
+
+      def geo_information( key )
+        return nil if self.value == "—"
+        return geo_location.send(key).strip if self.value && geo_location.send(key).try(:strip).present?
+      end
+
+      def geocoded?
+        (find_geo_location && @geo_location.geocoded?).to_b
+      end
+      def geocode
+        return @geo_location.geocode if @geo_location
+        return @geo_location.geocode if find_geo_location
+        return find_or_create_geo_location
+      end
+      
+      def latitude
+        geo_information :latitude
+      end
+      def longitude
+        geo_information :longitude
+      end
     end
 
-    def find_geo_location
-      @geo_location ||= GeoLocation.find_by_address(value)
-    end
-
-    def find_or_create_geo_location
-      @geo_location ||= GeoLocation.find_or_create_by address: value if self.value && self.value != "—"
-    end
-
-    def display_html
-      ActionController::Base.helpers.simple_format self.value
-    end
-
-    # This is needed to display the map later.
-    def gmaps4rails_address
-      self.value
-    end
-
-    def gmaps
-      true
-    end
-
-    def latitude ;      geo_information :latitude      end
-    def longitude ;     geo_information :longitude     end
-    def country ;       geo_information :country       end
-    def country_code ;  geo_information :country_code  end
-    def city ;          geo_information :city          end
-    def postal_code ;   geo_information :postal_code   end
-    def plz ;           geo_information :plz           end
-
-    def geo_information( key )
-      return nil if self.value == "—"
-      geo_location.send( key ) if self.value
-    end
-
-    def geocoded?
-      (find_geo_location && @geo_location.geocoded?).to_b
-    end
-    def geocode
-      return @geo_location.geocode if @geo_location
-      return @geo_location.geocode if find_geo_location
-      return find_or_create_geo_location
-    end
 
     # Allow to mark one address as primary postal address.
     # 
-    def postal_address
-      self.has_flag? :postal_address
-    end 
-    def postal_address=(new_postal_address)
-      if new_postal_address != self.postal_address
-        if new_postal_address
-          self.clear_postal_address
-          self.add_flag :postal_address
-        else
-          self.remove_flag :postal_address
-        end
-        self.delete_cache
-      end
-    end
-    def postal_address?
-      self.postal_address
-    end
-    def clear_postal_address
-      if self.profileable
-        self.profileable.profile_fields.where(type: "ProfileFieldTypes::Address").each do |address_field|
-          address_field.remove_flag :postal_address
+    attr_accessible :postal_address if defined? attr_accessible
+    concerning :PostalAddressFlag do
+      def postal_address
+        self.has_flag? :postal_address
+      end 
+      def postal_address=(new_postal_address)
+        if new_postal_address != self.postal_address
+          if new_postal_address
+            self.clear_postal_address
+            self.add_flag :postal_address
+          else
+            self.remove_flag :postal_address
+          end
+          self.delete_cache
         end
       end
-    end
-    def postal_or_first_address?
-      postal_address? or (self.profileable && self.profileable.profile_fields.where(type: "ProfileFieldTypes::Address").order(:id).limit(1).pluck(:id).first == self.id)
-    end
-    
+      def postal_address?
+        self.postal_address
+      end
+      def clear_postal_address
+        if self.profileable
+          self.profileable.profile_fields.where(type: "ProfileFieldTypes::Address").each do |address_field|
+            address_field.remove_flag :postal_address
+          end
+        end
+      end
+      def postal_or_first_address?
+        postal_address? or (self.profileable && self.profileable.profile_fields.where(type: "ProfileFieldTypes::Address").order(:id).limit(1).pluck(:id).first == self.id)
+      end
+    end    
     
     def self.fix_one_liner_addresses
       self.all.each do |address_field|
@@ -99,5 +156,4 @@ module ProfileFieldTypes
     end
 
   end
-  
 end
