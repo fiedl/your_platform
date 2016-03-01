f02ac034e3606130b45d7737e9e553c21dba45b9
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-03-01 22:46:47 +0100

    redis analytics: only consider authenticated (signed in) users, since we do not want to track bots.
    Trello: https://trello.com/c/PXhUOGxA/965-metrics-redisanalytics
    https://trello.com/c/YMfaFWa5/180-mumble-sitzung-am-01-03-2016

--- config/initializers/redis_analytics.rb
+++ config/initializers/redis_analytics.rb
@@ -3,4 +3,19 @@ require 'redis_analytics'
 RedisAnalytics.configure do |configuration|
   configuration.redis_connection = Redis.new(:host => 'localhost', :port => '6379')
   configuration.redis_namespace = "#{::STAGE}_redis_analytics"
+  
+  # We only want to track signed in users, not bots et cetera.
+  #
+  # At 2016-02, before adding this filter, we had about 1800 visits 
+  # per week, each visit with ca. 6 page hits and 7 minutes time
+  # spent.
+  #
+  configuration.add_filter do |request, response|
+    ## user.path == ...
+    ## user = request.env['warden'].user.user if request.env['warden'].authenticate?
+    #
+    # Skip non-authenticated visitors:
+    not request.env['warden'].authenticate?
+  end
+  
 end
