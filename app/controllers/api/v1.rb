9a6ab56fceada80f023b32d1400617b359cbd4f6
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-03-31 00:19:59 +0200

    api: adding entry point for documentation
    Trello: https://trello.com/c/CM90aomm/234-api

---
+++ app/controllers/api/v1.rb
@@ -0,0 +1,8 @@
+# This is the application programming interface (API), version 1,
+# for YourPlatform.
+#
+# The documentation can be accessed here:
+# http://www.rubydoc.info/github/fiedl/your_platform/master/Api/V1
+#
+module Api::V1
+end
