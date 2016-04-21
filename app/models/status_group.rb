08efe34f8455830258b3b219180849f3f0988384
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-04-21 14:14:08 +0200

    More helpful error message for status group selection algorithm.

--- app/models/status_group.rb
+++ app/models/status_group.rb
@@ -1,28 +1,28 @@
-# 
-# A StatusGroup is a sub-Group of a Corporation that memberships model the 
+#
+# A StatusGroup is a sub-Group of a Corporation that memberships model the
 # current status of members within the corporation.
 #
 # At present time, the status of a user within a corporation is unique.
 #
 class StatusGroup < Group
-  
+
   def self.find_all_by_corporation(corporation)
     corporation.leaf_groups.select do |group|
       group.ancestor_events.count == 0
     end
   end
-  
+
   def self.find_all_by_user(user, options = {})
     user_groups = options[:with_invalid] ? user.parent_groups : user.direct_groups
     user.corporations.collect do |corporation|
       StatusGroup.find_all_by_corporation(corporation)
     end.flatten & user_groups
   end
-  
+
   def self.find_by_user_and_corporation(user, corporation)
     status_groups = (StatusGroup.find_all_by_corporation(corporation) & StatusGroup.find_all_by_user(user))
-    raise "Slection algorithm not unique, yet. Please correct this. Found possible status groups: #{status_groups.map(&:name).join(', ')}." if status_groups.count > 1
+    raise "Selection algorithm not unique, yet, for user #{user.id}. Please correct this. Found possible status groups: #{status_groups.map(&:name).join(', ')}." if status_groups.count > 1
     status_groups.last
   end
-  
+
 end
