d67234ebba5a40cc8de2a786ea5417ac86b83853
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-05-25 08:51:52 +0200

    drop "public root element"-flagged pages (2016-reiGha5u)
    Drop the concept of „public root elements“.
    
    These have been child page of the public root page, which have been
    displayed on the public_root#index but not in the horizontal navigation.
    
    The concept has been replaced with the concept of teaser boxes: Each
    child pages is also shown as teaser box, which makes the horizontal nav
    dispensable, especially on mobile devices.
    
    Whether to show the page in the horizontal navigation will be
    determined by the nav node setting (`NavNode#hidden_menu`) associated
    with the page.
    
    2016-reiGha5u
    
    Trello: https://trello.com/c/u5Fl2VmC/991

--- app/controllers/public_root_controller.rb
+++ app/controllers/public_root_controller.rb
@@ -1,20 +1,15 @@
 class PublicRootController < ApplicationController
-  
+
   def index
     @page = Page.public_root
     authorize! :read, @page
-    
-    @public_root_element_pages = Page.flagged(:public_root_element)
 
-    @child_pages = @page.child_pages - @public_root_element_pages - Page.flagged(:imprint) - Page.flagged(:intranet_root)
-    @child_pages.select! { |p| not p.title.in? @public_root_element_pages.pluck(:title) }
-
-    @teaser_box_pages = @public_root_element_pages + @child_pages
+    @teaser_box_pages = @page.child_pages - Page.flagged(:imprint) - Page.flagged(:intranet_root)
 
     @events = Event.where(publish_on_global_website: true)
       .upcoming.order('events.start_at, events.created_at')
       .limit(5)
-      
+
     @hide_attachment_drop_fields = false
     
     set_current_navable @page
@@ -22,5 +17,5 @@ class PublicRootController < ApplicationController
     set_current_access :public
     set_current_access_text :this_is_the_public_website_and_can_be_read_by_all_internet_users
   end
-  
+
 end
