1df9665d5061a9e77d950c572cb874dc50303af2
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-05-31 18:32:53 +0200

    fixing asset path issue

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,5 +1,5 @@
 module IconHelper
-  
+
   def icon(icon_key)
     if icon_key.to_s.in? ['beer', 'coffee', 'key', 'lock', 'unlock', 'unlock-alt', 'archive', 'undo', 'history', 'folder-open', 'sort-alpha-asc', 'sort-numeric-asc', 'sort-numeric-desc']
       awesome_icon(icon_key)
@@ -7,35 +7,35 @@ module IconHelper
       glyphicon(icon_key.to_s.gsub('glyphicon-', ''))
     end
   end
-  
+
   # This includes an icon from Twitter-Bootstrap's Glyphicon icon set:
   # http://getbootstrap.com/components/#glyphicons
   #
   def glyphicon(icon_key)
     content_tag :span, '', class: "glyphicon glyphicon-#{icon_key}", 'aria-hidden' => true
   end
-  
+
   # This includes an icon from the Font-Awesome icon set:
   # http://fortawesome.github.io/Font-Awesome/icons/
-  # 
+  #
   # This inserts something like
   #     <i class="fa fa-beer fa-2x"></i>
   #
   def awesome_icon(icon_key)
     # This helper is defined in:
     # https://github.com/bokmann/font-awesome-rails
-    
+
     fa_icon icon_key if defined?(fa_icon)  # it's not defined in mailers.
   end
-  
+
   def large_awesome_icon(icon_key)
     fa_icon "#{icon_key} 2x"
   end
-  
+
   def excel_icon
-    image_tag image_path('img/excel2013_file_bw_semitransparent_107.png'), height: 16
+    image_tag 'img/excel2013_file_bw_semitransparent_107.png', height: 16
   end
-  
+
   def csv_icon
     icon 'list-alt'
   end
