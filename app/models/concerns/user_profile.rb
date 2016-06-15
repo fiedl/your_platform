d0ec41ab9a0d81bb4bfbfbf949c66e16d3fd33eb
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-06-15 18:10:34 +0200

    removing whitespace [ci-skip]

--- app/models/concerns/user_profile.rb
+++ app/models/concerns/user_profile.rb
@@ -1,7 +1,7 @@
 # This module contains all the profile-related methods of a User.
-# 
+#
 concern :UserProfile do
-  
+
   included do
     has_profile_fields profile_sections: [:contact_information, :about_myself, :study_information, :career_information,
        :organizations, :bank_account_information]
@@ -15,7 +15,7 @@ concern :UserProfile do
       field.label.downcase.include?('mobil') or field.label.downcase.include?('handy')
     end
   end
-  
+
   def phone
     phone_field.try(:value)
   end
@@ -25,35 +25,34 @@ concern :UserProfile do
   def phone_field
     landline_profile_fields.first || phone_profile_fields.first
   end
-  
+
   def mobile
     (mobile_phone_profile_fields + phone_profile_fields).first.try(:value)
   end
   def mobile=(new_number)
     (mobile_phone_profile_fields.first || profile_fields.create(label: I18n.t(:mobile), type: 'ProfileFieldTypes::Phone')).update_attributes(value: new_number)
   end
-  
+
   def profile_field_by_label(label)
     profile_fields.where(label: label).first
   end
   def profile_field_value(label)
     profile_field_by_label(label).try(:value).try(:strip)
   end
-  
+
   def personal_title_field
     profile_field_by_label 'personal_title'
   end
   def personal_title
     cached { personal_title_field.try(:value).try(:strip) }
   end
-  
   def academic_degree_field
     profile_field_by_label 'academic_degree'
   end
   def academic_degree
     cached { academic_degree_field.try(:value).try(:strip) }
   end
-  
+
   def study_fields
     profile_fields.where(type: 'ProfileFieldTypes::Study')
   end
@@ -76,8 +75,8 @@ concern :UserProfile do
   def text_after_name
     name_surrounding_profile_field.try(:name_suffix).try(:strip)
   end
-  
-      
+
+
   def fill_in_template_profile_information
     self.profile_fields.create(label: :personal_title, type: "ProfileFieldTypes::General")
     self.profile_fields.create(label: :academic_degree, type: "ProfileFieldTypes::AcademicDegree")
@@ -96,5 +95,5 @@ concern :UserProfile do
     pf.text_above_name = ""; pf.name_prefix = "Herrn"; pf.name_suffix = ""; pf.text_below_name = ""
     pf.save
   end
-  
+
 end
