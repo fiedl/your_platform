df933b3d1928270509305168568ab9ddd41037e1
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-06-16 15:02:29 +0200

    adding exception notifier for ticket system, which allows to use the current user's email address as sender address.
    Trello: https://trello.com/c/IvuNA8lo/1001

---
+++ app/models/exception_notifier/ticket_system_notifier.rb
@@ -0,0 +1,19 @@
+module ExceptionNotifier
+  class TicketSystemNotifier < EmailNotifier
+
+    def initialize(options)
+      @fallback_sender_address = options[:fallback_sender_address]
+      options[:sender_address] ||= options[:fallback_sender_address]
+      super(options)
+    end
+
+    def call(exception, options = {})
+      options[:env]['exception_notifier.exception_data'][:backtrace] = exception.backtrace
+      if current_user = options[:env]['exception_notifier.exception_data'][:current_user]
+        options[:sender_address] = "#{current_user.title} <#{current_user.email}>"
+      end
+      super(exception, options)
+    end
+
+  end
+end
