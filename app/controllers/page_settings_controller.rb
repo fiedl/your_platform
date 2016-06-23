a0c87bbdf572e7bc7be5a535ad23cdfd4b064f79
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-06-23 15:48:05 +0200

    page settings: i18n (2016-poo8AePh)
    2016-poo8AePh
    
    Trello: https://trello.com/c/u5Fl2VmC/991

--- app/controllers/page_settings_controller.rb
+++ app/controllers/page_settings_controller.rb
@@ -4,7 +4,7 @@ class PageSettingsController < ApplicationController
     @page = Page.find params[:page_id]
     authorize! :manage, @page
 
-    set_current_title @page.title
+    set_current_title "#{I18n.t(:page_settings)}: #{@page.title}"
     set_current_navable @page
     set_current_access :admin
     set_current_access_text :only_page_admins_can_view_or_change_these_settings
