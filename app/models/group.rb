dfa7fb155199b046e5575dcfe64f59f9e7c72a37
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-08-17 16:01:32 +0200

    fixing "missing superclass" issue

--- app/models/group.rb
+++ app/models/group.rb
@@ -80,7 +80,7 @@ class Group < ActiveRecord::Base
   # 'admins', use the translation.
   #
   def name
-    I18n.t( super.to_sym, default: super ) if super.present?
+    I18n.t( super.to_sym, default: super ) if defined?(super) && super.present?
   end
 
   def name_with_surrounding
