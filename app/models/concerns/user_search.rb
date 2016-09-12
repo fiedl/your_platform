fb2013d88c28ec1260b96b869b3f4c352ee2abd5
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-09-12 16:46:13 +0200

    User#search: include geo search
    If the search query has the form „London 50km“ users within a radius of
    50km of the geo location of London will be searched.
    
    Trello:
    https://trello.com/c/V3gMKMdK/1029-gruppen-definition-nach-umkreis-2016-
    ca6iigei

--- app/models/concerns/user_search.rb
+++ app/models/concerns/user_search.rb
@@ -2,6 +2,23 @@ concern :UserSearch do
   class_methods do
 
     def search(query)
+      search_by_geo_location(query) || search_by_name_and_title(query)
+    end
+
+    private
+
+    def search_by_geo_location(query)
+      if match_data = query.match(/(.*) ([0-9]*)km/)
+        address = match_data[1]
+        radius = match_data[2]
+        users = User.within radius_in_km: radius, around: address
+        users.select do |user|
+          user.alive? && user.wingolfit?
+        end
+      end
+    end
+
+    def search_by_name_and_title(query)
       q = "%" + query.gsub(' ', '%') + "%"
       users = self
         .where("CONCAT(first_name, ' ', last_name) LIKE ?", q)
