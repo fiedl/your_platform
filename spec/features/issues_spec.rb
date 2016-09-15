ba52b20da57bbbcef6ebe2d05340e50e7e290e71
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-09-15 22:51:10 +0200

    adding specs to simulate an admin fixing an administrative issue
    Scenario: A postal address has too many lines. The admins sees the
    issue and fixes it by formatting the address in a more compact manor.
    
    Trello: https://trello.com/c/vVrTElX7/1037-wingolfsblatter-issues

---
+++ spec/features/issues_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+feature "Issues", :js do
+  include SessionSteps
+
+  scenario "Fixing an issue where a postal address has too many lines" do
+    @person = create :user
+    @name_surrounding_field = @person.profile_fields.create type: 'ProfileFieldTypes::NameSurrounding'
+    @name_surrounding_field.text_below_name = "Student"
+    @name_surrounding_field.save
+    @address_field = @person.profile_fields.create type: 'ProfileFieldTypes::Address', label: 'Study address'
+    @address_field.first_address_line = "King's Parade"
+    @address_field.second_address_line = "King's College"
+    @address_field.postal_code = "CB2 1ST"
+    @address_field.city = "Cambridge"
+    @address_field.region = "Cambridgeshire"
+    @address_field.country_code = 'GB'
+    @address_field.save
+
+    Issue.count.should == 0
+    Issue.scan(@address_field)
+    Issue.count.should == 1
+
+    login :admin
+    visit issues_path
+
+    page.should have_text "Student"
+    page.should have_text "Cambridgeshire"
+    page.should have_text "King's College"
+
+    # The admin tries to save some space here: The address is allowed to take up
+    # up to four lines:
+
+    click_on :edit
+    enter_in_edit_mode 'li.region', ""
+    enter_in_edit_mode 'li.first_address_line', "King's College, King's Parade"
+    enter_in_edit_mode 'li.second_address_line', ""
+    click_on :save
+
+    page.should have_text t(:scanning_issue)
+    page.should have_no_text t(:scanning_issue)
+    page.should have_text t(:thanks)
+
+    @address_field.reload.region.should be_empty
+
+    Issue.scan(@address_field)
+    Issue.count.should == 0
+  end
+
+end
