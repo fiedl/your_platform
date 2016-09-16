bc1e4b86175810f56ea1284f0396665a397a1b38
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-09-16 17:11:56 +0200

    NavableVisit: fixing readonly mode

--- app/models/concerns/user_recommendations.rb
+++ app/models/concerns/user_recommendations.rb
@@ -1,22 +1,24 @@
 concern :UserRecommendations do
-  
+
   def track_visit(navable)
-    self.group_ids.each do |group_id|
-      NavableVisit.create group_id: group_id, navable: navable
+    unless readonly?
+      self.group_ids.each do |group_id|
+        NavableVisit.create group_id: group_id, navable: navable
+      end
     end
   end
-  
+
   def recommended_navables
     NavableVisit
       .where(group_id: self.group_ids)
       .group(:navable_type, :navable_id)
       .order('count_id desc').count('id')
       .take(10)
-      .collect { |type_and_id_and_count| # [[type, id], count] 
+      .collect { |type_and_id_and_count| # [[type, id], count]
         type_and_id_and_count.first.first.constantize.find(type_and_id_and_count.first.last)
-      }.select { |navable| 
+      }.select { |navable|
         self.can? :read, navable
       }
   end
-  
+
 end
