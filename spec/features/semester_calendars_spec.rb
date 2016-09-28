773e90c27ecd3254bf6880eeaec4b72f33671adf
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-09-28 16:00:18 +0200

    semester calendars: fixing specs after switching to datetime_select

--- spec/features/semester_calendars_spec.rb
+++ spec/features/semester_calendars_spec.rb
@@ -17,7 +17,13 @@ feature "Semester Calendars", :js do
     visit edit_semester_calendar_path(@semester_calendar)
 
     click_on :add_event_to_semester_calendar
-    find('.event_starts_at input').set I18n.localize(Time.zone.now.change(month: 7).to_time)
+    #find('.event_starts_at input').set I18n.localize(Time.zone.now.change(month: 7).to_time)
+    page.all('.event_starts_at select')[0].find(:option, "01").select_option
+    page.all('.event_starts_at select')[1].find(:option, "07").select_option
+    page.all('.event_starts_at select')[2].find(:option, Time.zone.now.year.to_s).select_option
+    page.all('.event_starts_at select')[3].find(:option, "20").select_option
+    page.all('.event_starts_at select')[4].find(:option, "15").select_option
+
     find('.event_name input').set "My new event"
     find('.event_location input').set "adH"
 
