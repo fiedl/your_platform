7bc81c1d843af52e02bacaecfe9b8797736b6993
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-10-19 18:25:20 +0200

    pages: edit teaser text
    https://trello.com/c/4QB4aNDl/1050-blog-modul

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,6 @@
 class Page < ActiveRecord::Base
 
-  attr_accessible        :content, :title, :redirect_to, :author, :tag_list if defined? attr_accessible
+  attr_accessible        :content, :title, :teaser_text, :redirect_to, :author, :tag_list if defined? attr_accessible
 
   is_structureable       ancestor_class_names: %w(Page User Group Event), descendant_class_names: %w(Page User Group Event)
   is_navable
