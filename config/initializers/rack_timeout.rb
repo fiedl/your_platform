ff30005d08ba8b2ce318fce502184e43a56a8295
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-10-23 12:32:38 +0200

    rack timeout: fix logging issues

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -6,3 +6,9 @@
 #     Rack::Timeout.timeout = 120
 #
 Rack::Timeout.timeout = 120
+
+# Prevent info logging in the stderr stream.
+# https://github.com/heroku/rack-timeout/issues/63#issuecomment-170416025
+#
+Rack::Timeout::Logger.update($stderr, ::Logger::ERROR)
+
