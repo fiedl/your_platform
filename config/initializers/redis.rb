074e26b582a845fd413d4940b21bc848e11769ac
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-11-18 16:30:05 +0100

    namespacing the redis cache
    The base redis connection configuration is extracted out into
    `RedisConnectionConfiguration`.
    
    The redis store does support Proc namespaces.
    https://trello.com/c/6xYBRb09/1075#comment-582de8644a6f8d6c10650127
    https://github.com/redis-store/redis-store/blob/master/lib/redis/store/namespace.rb#L88
    
    See also my stackoverflow question:
    http://stackoverflow.com/q/40638628/2066546
    
    https://trello.com/c/6xYBRb09/1075-redis-bottleneck
    https://trello.com/c/cypH3vmW/896-multitenancy

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -10,3 +10,54 @@
 
 ENV['REDIS_HOST'] ||= 'redis' if (Resolv.getaddress "redis" rescue false)
 ENV['REDIS_HOST'] ||= 'localhost'
+
+# Use this class to provide a configuration for a `:redis_store`:
+#
+# Where you normally write
+#
+#     Rails.application.config.cache_store = :redis_store,
+#       {host: ..., expires_in: 1.day, namespace: 'cache'}
+#
+# you may write
+#
+#     Rails.application.config.cache_store = :redis_store,
+#       RedisConnectionConfiguration.new(:cache, {expires_in: 1.day}).to_hash
+#
+# This way, we can collect the common configuration options in the
+# class below.
+#
+class RedisConnectionConfiguration
+
+  # Arguments:
+  # - namespace_key, e.g. "cache" or "sidekiq".
+  # - options, e.g. {port: '6379', expires_in: 1.week}
+  #
+  def initialize(namespace_key, options = {})
+    @namespace_key = namespace_key
+    @options = options
+    return self
+  end
+
+  def default_options
+    {
+      host: ENV['REDIS_HOST'],
+      port: '6379',
+      expires_in: 1.week,
+      namespace: namespace,
+      timeout: 15.0
+    }
+  end
+
+  def namespace
+    if Gem.loaded_specs.has_key? 'apartment'
+      Proc.new { "#{::STAGE}_#{Apartment::Tenant.current}_#{@namespace_key}" }
+    else
+      "#{::STAGE}_#{@namespace_key}"
+    end
+  end
+
+  def to_hash
+    default_options.merge(@options)
+  end
+end
+
