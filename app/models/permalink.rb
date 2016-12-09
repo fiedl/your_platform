337add7c2d077108278adcf5dc9cbc4a3dbafc4d
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-12-09 14:23:43 +0100

    permalinks: validations for global and host-specific paths
    https://trello.com/c/539ozGCD/1051-pfade-und-permalinks-2016-biu5mate

--- app/models/permalink.rb
+++ app/models/permalink.rb
@@ -1,6 +1,10 @@
 class Permalink < ActiveRecord::Base
   belongs_to :reference, polymorphic: true
-  validates :path, presence: true, uniqueness: {scope: :host}
+  validates :path, presence: true
+
+  validates :path, uniqueness: true, if: Proc.new { |permalink| not permalink.host.present? }
+  validates :path, uniqueness: true, if: Proc.new { |permalink| Permalink.where(host: nil, path: permalink.path).any? }
+  validates :path, uniqueness: {scope: :host}
 
   scope :for_host, -> (host) { where(host: [nil, host]) }
 
