7f20ec6832192da1ad04d15ea57c81881b9120da
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-12-28 15:11:49 +0100

    removing whitespace [ci-skip]

--- app/helpers/you_tube_helper.rb
+++ app/helpers/you_tube_helper.rb
@@ -1,5 +1,5 @@
 module YouTubeHelper
-  
+
   # Convert YouTube links to embedding iframes.
   # Using auto html: https://github.com/dejan/auto_html
   #
@@ -8,14 +8,14 @@ module YouTubeHelper
       youtube(autoplay: false)
     end.html_safe
   end
-  
+
   def you_tube(content)
     youtubify content
   end
-  
+
 end
 
-# In order to use the helper method with best_in_place's :display_with argument, 
+# In order to use the helper method with best_in_place's :display_with argument,
 # the ActionView::Base has to include the method.
 #
 module ActionView
