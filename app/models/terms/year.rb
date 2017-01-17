8feab183ffb5381b0726140c19a0a3386dff34e0
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-01-17 12:10:49 +0100

    TermInfos::Year: we need to specify also hour and minute
    Otherwise, we ignore the memberships starting at
    `„yyyy-01-01“.to_date`, because they are most likely before the current
    time of the day.
    
    https://trello.com/c/0o1V9xaf/1092-term-infos-2017-cee9ievu

--- app/models/terms/year.rb
+++ app/models/terms/year.rb
@@ -1,10 +1,10 @@
 class Terms::Year < Term
 
   def start_at
-    Time.zone.now.change(month: 1, day: 11, year: year)
+    Time.zone.now.change(month: 1, day: 1, year: year, hour: 0, minute: 0)
   end
   def end_at
-    Time.zone.now.change(month: 12, day: 31, year: year)
+    Time.zone.now.change(month: 12, day: 31, year: year, hour: 23, minute: 59)
   end
 
   def title
