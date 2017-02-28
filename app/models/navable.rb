f6c95f3e2db06e953576204af581af15108efcf3
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-02-28 13:18:55 +0100

    removing whitespace [ci skip]

--- app/models/navable.rb
+++ app/models/navable.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This module provides the +is_navable+ method for ActiveRecord::Base.
-# Calling this method marks the model (User, Page, ...) as navable, i.e. has menu, breadcrumbs, etc. 
+# Calling this method marks the model (User, Page, ...) as navable, i.e. has menu, breadcrumbs, etc.
 #
 # The inclusion in ActiveRecord::Base is done in
 # config/initializers/active_record_navable_extension.rb.
@@ -10,14 +10,14 @@
 module Navable
   def is_navable
     has_one                :nav_node, as: :navable, dependent: :destroy, autosave: true
-    
+
     include InstanceMethodsForNavables
   end
   module InstanceMethodsForNavables
-    def is_navable? 
+    def is_navable?
       true
     end
-    
+
     def navable?
       is_navable?
     end
@@ -35,7 +35,7 @@ module Navable
     def nav
       nav_node
     end
-    
+
     # We do not show all kinds of objects in the menu.
     # Therefore select the appropriate items.
     #
