22cd32901f0f6590affb19e5e5671b312cd3553b
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-03-08 18:07:39 +0100

    Groups::Everyone: adapting the model name
    This way, we need no separate route. And the cache key stays the same.

--- app/models/groups/everyone.rb
+++ app/models/groups/everyone.rb
@@ -14,4 +14,10 @@ class Groups::Everyone < Group
   def self.find_or_create
     self.first || self.create
   end
+
+  # This is used to determine the routes for this resource.
+  # http://stackoverflow.com/a/9463495/2066546
+  def self.model_name
+    Group.model_name
+  end
 end
