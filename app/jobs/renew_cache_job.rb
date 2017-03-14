ad02dfd69a64428b324b91617de982657277b8b5
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-03-14 16:15:42 +0100

    active job does not support the `Time` type. Thus, patch `perform_later`.
    https://trello.com/c/6dNTE3FL/1-renew-cache

--- app/jobs/renew_cache_job.rb
+++ app/jobs/renew_cache_job.rb
@@ -4,11 +4,18 @@ class RenewCacheJob < ApplicationJob
   # This job renews the cache of the given `record`.
   # Only caches that are older than `time` are renewed.
   #
-  def perform(record_or_records, time = Time.zone.now)
+  def perform(record_or_records, time_i)
+    time = Time.at(time_i)
     if record_or_records.respond_to? :each
       record_or_records.each { |record| record.try(:renew_cache, time) }
     else
       record_or_records.try(:renew_cache, time)
     end
   end
+
+  def self.perform_later(record_or_records, time = Time.zone.now)
+    super(record_or_records, time.to_i)
+  end
+
+
 end
