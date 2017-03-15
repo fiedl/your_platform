a353d87dcab5bc78ac9e3edbd0823c681a195655
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-03-15 17:29:38 +0100

    travis: do not test caching in NO_CACHING=true environment

--- spec/models/concerns/corporation_caching_spec.rb
+++ spec/models/concerns/corporation_caching_spec.rb
@@ -2,16 +2,20 @@ require 'spec_helper'
 
 describe CorporationCaching do
 
-  describe ".cached_methods" do
-    subject { Corporation.cached_methods }
+  unless ENV['NO_CACHING']
 
-    it "should include methods that are Corporation-specific" do
-      subject.should include :status_group_ids
+    describe ".cached_methods" do
+      subject { Corporation.cached_methods }
+
+      it "should include methods that are Corporation-specific" do
+        subject.should include :status_group_ids
+      end
+
+      it "should inherit methods from Group" do
+        subject.should include :member_table_rows
+      end
     end
 
-    it "should inherit methods from Group" do
-      subject.should include :member_table_rows
-    end
   end
 
 end
