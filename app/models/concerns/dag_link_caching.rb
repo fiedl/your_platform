6ae61d6c805012e06b6e73b3172bb70d6d46c0b4
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-03-17 11:40:23 +0100

    dag link caching: `needs_review` should not trigger a renew_cache
    It’s not needed and would take way too long.

--- app/models/concerns/dag_link_caching.rb
+++ app/models/concerns/dag_link_caching.rb
@@ -1,7 +1,7 @@
 concern :DagLinkCaching do
 
   included do
-    after_save { RenewCacheJob.perform_later(self, time: Time.zone.now) }
+    after_save { RenewCacheJob.perform_later(self, time: Time.zone.now) if self.changes.except(:needs_review, :updated_at).any? }
     after_commit :delay_renew_cache_of_ancestor_and_descendant, on: :destroy
   end
 
