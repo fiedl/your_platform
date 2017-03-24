f1a042504d42f2c28623164950e94d691aca882c
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-03-24 10:07:07 +0100

    fixing group export spec

--- spec/features/group_export_spec.rb
+++ spec/features/group_export_spec.rb
@@ -5,15 +5,15 @@ require 'spec_helper'
 #
 feature 'Group Member List Export' do
   include SessionSteps
-  
+
   before do
     @group = create :group, :with_members
 
     login :admin
   end
-  
+
   scenario 'exporting a birthday list as csv' do
-    visit group_path @group, list: 'birthday_list', format: 'csv'
+    visit group_list_export_path group_id: @group.id, list: 'birthday_list', format: 'csv'
     page.should have_text @group.members.first.last_name
-  end 
+  end
 end
