70e4032858c4aa96a91d978f74bf0fe65339549e
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-03-29 17:48:46 +0200

    sidekiq attempts: In rails 4, `deserialize` is a class method.
    Fixes „undefined method < for nil“.
    
    https://trello.com/c/cQMbH6X4/8-re-configure-sidekiq

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,14 +1,16 @@
 class ApplicationJob < ActiveJob::Base
   self.queue_adapter = :sidekiq
+  attr_accessor :attempt_number
 
   def serialize
     # http://stackoverflow.com/a/38592564/2066546
     # http://edgeapi.rubyonrails.org/classes/ActiveJob/Core.html
-    super.merge('attempt_number' => (@attempt_number || 0) + 1)
+    super.merge('attempt_number' => (attempt_number || 0) + 1)
   end
 
+  # This method is for rails 5.
   def deserialize(job_data)
-    @attempt_number = job_data['attempt_number']
+    attempt_number = job_data['attempt_number']
     super
   end
 
@@ -16,7 +18,18 @@ class ApplicationJob < ActiveJob::Base
     # There are cases where sidekiq is too fast, i.e. the background worker
     # begins to process before the record is accessible through the database.
     # Just retry in a couple of seconds.
-    retry_job(wait: 30, queue: :retry) if @attempt_number < 5
+    retry_job(wait: 30, queue: :retry) if attempt_number < 5
+  end
+
+end
+
+class ActiveJob::Base
+
+  # This method is for rails 4.
+  def self.deserialize(job_data)
+    job = super
+    job.attempt_number = job_data['attempt_number']
+    job
   end
 
 end
