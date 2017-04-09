341fef62cecd41d147024d746e49cb5c69f89d1b
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-04-09 20:20:11 +0200

    Patching `ApplicationJob#serialize` trying to fix "(ArgumentError) "wrong number of arguments (given 0, expected 2)"", https://github.com/fiedl/your_platform/issues/23, http://support.wingolfsplattform.org/tickets/6930.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -8,4 +8,23 @@ class ApplicationJob < ActiveJob::Base
       retry_job(wait: 30, queue: :retry)
     end
   end
+
+  # This patch tries to fix "(ArgumentError) "wrong number of arguments (given 0, expected 2)".
+  #
+  # @see https://github.com/rails/rails/issues/22044
+  # @see https://github.com/fiedl/your_platform/issues/23
+  #
+  # TODO: When migrating to rails 5, check if https://github.com/rails/rails/pull/26221 is
+  # pulled already.
+  #
+  def serialize
+    serialized = super
+
+    # @see https://github.com/rails/rails/blob/v5.0.0.1/activejob/lib/active_job/core.rb#L116
+    if defined?(@serialized_arguments) && @serialized_arguments.present?
+      serialized["arguments"] = @serialized_arguments
+    end
+
+    serialized
+  end
 end
