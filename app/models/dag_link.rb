4f080543aca31e9a11b650d9eb1fede5e8668a27
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-04-20 10:31:32 +0200

    adding `DagLink#title` as required by the background cache logging.

--- app/models/dag_link.rb
+++ app/models/dag_link.rb
@@ -2,6 +2,10 @@ class DagLink < ApplicationRecord
 
   acts_as_dag_links polymorphic: true
 
+  def title
+    "Link #{ancestor_type} #{ancestor_id} --> #{descendant_type} #{descendant_id}"
+  end
+
   include DagLinkTypes
   include DagLinkRepair
   include DagLinkCaching if use_caching?
