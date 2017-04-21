96f9d6d9450811e491e70938789e4c8ccdbac588
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-04-21 11:26:58 +0200

    api: adding more specs for public events
    https://trello.com/c/hEqaZpL1/1124-api-fur-events

--- spec/features/api_public_events_spec.rb
+++ spec/features/api_public_events_spec.rb
@@ -28,4 +28,44 @@ feature "Public api for events" do
     page.should have_no_text "Private event"
   end
 
+  scenario "GET /api/v1/public/events?limit=1" do
+    @past_event = create :event, start_at: 2.days.ago, end_at: nil, publish_on_global_website: true
+    @upcoming_events = [
+      create(:event, start_at: 1.day.from_now, end_at: nil, publish_on_global_website: true),
+      create(:event, start_at: 2.days.from_now, end_at: nil, publish_on_global_website: true)
+    ]
+
+    visit "/api/v1/public/events?limit=1"
+
+    page.should have_no_text @past_event.name
+    page.should have_text @upcoming_events.first.name
+    page.should have_no_text @upcoming_events.last.name
+  end
+
+  scenario "GET /api/v1/public/events?year=2006" do
+    @event_2006 = create :event, start_at: Time.zone.now.change(year: 2006), publish_on_global_website: true
+    @other_event = create :event, start_at: 2.days.ago, publish_on_global_website: true
+
+    visit "/api/v1/public/events?year=2006"
+
+    page.should have_no_text @other_event.name
+    page.should have_text @event_2006.name
+  end
+
+  scenario "GET /api/v1/public/events?year=2006&term=winter_term" do
+    @event_summer_2006 = create :event, start_at: Time.zone.now.change(year: 2006, month: 7), publish_on_global_website: true
+    @event_winter_2006 = create :event, start_at: Time.zone.now.change(year: 2006, month: 12), publish_on_global_website: true
+    @event_winter_2006_2007 = create :event, start_at: Time.zone.now.change(year: 2007, month: 1), publish_on_global_website: true
+    @event_winter_2010 = create :event, start_at: Time.zone.now.change(year: 2010, month: 12), publish_on_global_website: true
+
+    visit "/api/v1/public/events?year=2006&term=winter_term"
+
+    page.should have_text @event_winter_2006.name
+    page.should have_text @event_winter_2006_2007.name
+    page.should have_no_text @event_summer_2006.name
+    page.should have_no_text @event_winter_2010.name
+  end
+
+
+
 end
