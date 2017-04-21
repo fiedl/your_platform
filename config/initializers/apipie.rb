861fef75ac47971ea09565f8eb428c8efee33060
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-04-21 10:53:59 +0200

    make api documentation render markdown
    https://trello.com/c/YIvoFV5d/11-api-documentation

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,3 +1,19 @@
+class ApipieCustomRender
+  def initialize
+
+  end
+  def renderer
+    @renderer ||= begin
+      markdown_options = {autolink: true, no_intra_emphasis: true, fenced_code: true, gh_blockcode: true}
+      renderer_options = {hard_wrap: true}
+      Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(renderer_options), markdown_options)
+    end
+  end
+  def to_html(text)
+    renderer.render(text)
+  end
+end
+
 Apipie.configure do |config|
   config.app_name                = "#{Rails.application.class.parent_name}: API"
   config.app_info                = "YourPlatform: Administrative and social network platform for closed user groups. See: https://github.com/fiedl/your_platform."
@@ -5,6 +21,7 @@ Apipie.configure do |config|
   config.doc_base_url            = "/apipie"
   #config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
   config.api_controllers_matcher = "#{YourPlatform::Engine.root}/app/controllers/api/**/*.rb"
+  config.markup                  = ApipieCustomRender.new
   config.reload_controllers      = Rails.env.development?
 end
 
