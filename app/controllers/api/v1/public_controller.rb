f203fe5bf3522161910183855061361156d78fc7
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2017-04-21 11:42:03 +0200

    allow requests from other hosts for public api.
    https://trello.com/c/hEqaZpL1/1124-api-fur-events

--- app/controllers/api/v1/public_controller.rb
+++ app/controllers/api/v1/public_controller.rb
@@ -4,4 +4,13 @@
 # For api documentation, see /api.
 #
 class Api::V1::PublicController < ApplicationController
+
+  before_action :allow_cross_origin_requests
+
+  # Add the Cross-origin resource sharing header for public requests.
+  #
+  def allow_cross_origin_requests
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
 end
