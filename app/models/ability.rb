d4d9e9557dc889f6c5078c5087ee24d9dc886316
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-07-05 19:38:12 +0200

    Feature switch: Allow permalinks in the tests.
    We needed to introduce a feature switch for the permalinks editing ui.
    But we still need to test the ui. Thus, allow it in the test
    environment.
    
    And, allow it for developers.
    
    https://trello.com/c/CE7AKScY/1154-merge-des-homepage-moduls-wingolforg

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -138,6 +138,7 @@ class Ability
 
     can :use, :term_reports
     can :use, :requests_index
+    can :use, :permalinks
 
     can :use, :masquerade do
       # Only global admins that are developers are allowed
@@ -499,6 +500,7 @@ class Ability
     can :use, :semester_calendars
     cannot :use, :pdf_search  # It's not ready, yet. https://trello.com/c/aYtvpSij/1057
     can :use, :wysihtml if Rails.env.test? # it's not ready, yet, but the tests use it already.
+    can :use, :permalinks if Rails.env.test? # it's not ready, yet, but the tests use it already.
 
     # Use the tabs view in users#show. This has been a beta feature previously.
     # TODO: Remove the feature switch whenever you feel we won't switch back.
