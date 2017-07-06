2f3a8351a34678d213823455873624b7c144a68b
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-07-06 21:59:20 +0200

    fixing draggable boxes to work with pow.
    Use the path rather than the url to submit the new box configuration.
    
    Otherwise, this might cause cross-site issues with pow.
    
    https://trello.com/c/CE7AKScY/1154-merge-des-homepage-moduls-wingolforg

--- app/helpers/draggable_boxes_helper.rb
+++ app/helpers/draggable_boxes_helper.rb
@@ -2,7 +2,7 @@ module DraggableBoxesHelper
 
   def draggable_boxes_row(page)
     if can? :update, page
-      content_tag(:div, class: 'row row-eq-height draggable_boxes box_configuration', data: {box_configuration: page.box_configuration.to_json, page_url: page.url}) do
+      content_tag(:div, class: 'row row-eq-height draggable_boxes box_configuration', data: {box_configuration: page.box_configuration.to_json, page_url: page_path(page)}) do
         yield
       end
     else
