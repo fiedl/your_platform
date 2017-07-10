c1aa32be46026d0d364db19a65e5a038b484ef22
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-07-10 23:50:21 +0200

    fixing "no such key :Enter"
    Selenium::WebDriver::Error::UnsupportedOperationError:
           no such key :Enter
    
    https://trello.com/c/gmYwiOkn/1158-rspec-chrome-headless-statt-phantomjs

--- spec/support/press_enter.rb
+++ spec/support/press_enter.rb
@@ -1,15 +1,15 @@
 module PressEnter
-  
+
   def press_enter(receiver)
-    send_key(receiver, :Enter)
+    send_key(receiver, :enter)
   end
-  
+
   def send_key(receiver, key)
     selector = "##{receiver}" if receiver.kind_of?(String) or receiver.kind_of?(Symbol)
     selector ||= "##{receiver[:in]}" if receiver.kind_of?(Hash)
     raise 'no selector given' unless selector
-    
+
     find(:css, selector).native.send_key(key)
-  end    
-  
+  end
+
 end
