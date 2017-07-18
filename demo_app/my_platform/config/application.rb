6cbf70bd33b9d281ff96da51baee2c3b3842cf52
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-07-18 11:15:05 +0200

    `STAGE` needs to be defined earlier
    https://trello.com/c/oIddSLU0/1161-rails-5

--- demo_app/my_platform/config/application.rb
+++ demo_app/my_platform/config/application.rb
@@ -1,12 +1,12 @@
 require_relative 'boot'
 
+::STAGE = 'your_platform_test' if ENV['RAILS_ENV'] == 'test'
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-::STAGE = 'your_platform_test' if ENV['RAILS_ENV'] == 'test'
 
 module MyPlatform
   class Application < Rails::Application
