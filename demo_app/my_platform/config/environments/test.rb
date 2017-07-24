56e2b72462fa4c763da87160df2121e112d2f18f
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-07-24 19:57:16 +0200

    disable asset compiling in testing for debugging js errors
    https://trello.com/c/oIddSLU0/1161-rails-5

--- demo_app/my_platform/config/environments/test.rb
+++ demo_app/my_platform/config/environments/test.rb
@@ -38,6 +38,11 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
