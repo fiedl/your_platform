b4f0b3dcbb313a59f5ff950a0dca3bb40e2e86af
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-07-28 16:52:11 +0200

    travis: trying to fix datetime comparison issue
    Comparison has worked locally, but has failed on travis.
    
    https://travis-ci.org/fiedl/your_platform/jobs/258492258
    
    Further info:
    
    -
    https://community.codeship.com/t/how-to-solve-time-comparison-issues-wit
    h-your-rails-test/669
    -
    https://relishapp.com/rspec/rspec-expectations/v/2-8/docs/built-in-match
    ers/be-within-matcher
    - https://apidock.com/ruby/Time/round
    
    But I don’t want to change all matcher occurrences. Thus, I’m patching
    time comparison to `round(6)` in the spec_helper.
    
    https://trello.com/c/oIddSLU0/1161-rails-5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -238,6 +238,27 @@ RSpec.configure do |config|
   config.include HomePageSpecHelper, type: :feature
   config.include TabSpecHelper, type: :feature
 
+  # Modify datetime precision.
+  # To avoid this:
+  #
+  #     Failure/Error:
+  #
+  #      expected: 2016-04-26 10:10:04.573763343 +0000
+  #           got: 2016-04-26 10:10:04.573763000 +0000
+  #
+  # > This is actually a common issue that occurs with rails app tests.
+  # > The ruby Time object is very precise in comparison to the time your DB captures.
+  #
+  # https://community.codeship.com/t/how-to-solve-time-comparison-issues-with-your-rails-test/669
+  #
+  module TimeEqlPrecisionCorrection
+    def eql?(other)
+      self.round(6).eql?(other.round(6))
+    end
+  end
+  class Time
+    prepend TimeEqlPrecisionCorrection
+  end
 
   # Database Wiping Policy
   # ......................................................................................
