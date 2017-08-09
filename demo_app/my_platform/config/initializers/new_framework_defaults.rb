2d3c2491ba42803219f1e3b9046cd8f2e66f7031
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-08-10 00:27:47 +0200

    Switching `ActiveSupport.to_time_preserves_timezone` to new rails-5 default.
    https://trello.com/c/oIddSLU0/1161-rails-5

--- demo_app/my_platform/config/initializers/new_framework_defaults.rb
+++ demo_app/my_platform/config/initializers/new_framework_defaults.rb
@@ -6,10 +6,6 @@
 #
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
-# Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
-# Previous versions had false.
-ActiveSupport.to_time_preserves_timezone = false
-
 # Require `belongs_to` associations by default. Previous versions had false.
 Rails.application.config.active_record.belongs_to_required_by_default = false
 
