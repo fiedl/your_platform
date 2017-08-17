ed6bfb8bea5291508b33fbd31f1c40e05311e809
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-08-17 20:13:56 +0200

    fixing migration to resolve an issue
    of existing dag links where the valid_to is 0 rather than nil.
    
    https://trello.com/c/oIddSLU0/1161-rails-5

--- db/migrate/20170727152732_increase_validity_range_precision.rb
+++ db/migrate/20170727152732_increase_validity_range_precision.rb
@@ -1,5 +1,6 @@
 class IncreaseValidityRangePrecision < ActiveRecord::Migration[5.0]
   def change
+    DagLink.where(valid_to: 0).update_all valid_to: nil
     change_column :dag_links, :valid_from, :datetime, limit: 6
     change_column :dag_links, :valid_to, :datetime, limit: 6
   end
