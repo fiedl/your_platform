b64d02a4ed6ee71d63b23c52a01dfe1d13c0bfc5
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-08-31 01:52:30 +0200

    adding webpack-dev-server script
    which keeps exporting the packs when the source files are changed.
    
    https://trello.com/c/fY3vpcgm/1192-vuejs

--- app/javascripts/VueApp.js
+++ app/javascripts/VueApp.js
@@ -1,4 +1,5 @@
 // Make sure to run `bin/pack` after changing this file.
+// Or keep `bin/webpack-dev-server` running.
 // Otherwise, the changes are not picked up by the asset pipeline.
 
 import Vue from 'vue'
