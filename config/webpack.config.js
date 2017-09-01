22e70928e42d7ede3bdcc7f4a1cf83d8cdd96a09
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-09-01 15:10:02 +0200

    fixing webpack syntax for multiple entry points
    https://trello.com/c/fY3vpcgm/1192-vuejs
    https://trello.com/c/6Dtv77X0/1191-fix-passwörter-werden-als-zu-schwach-erkannt

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,9 +2,7 @@ var path = require('path');
 
 module.exports = {
   entry: {
-    your_platform_node_modules: path.resolve(__dirname, '..', 'app', 'javascripts', 'your_platform_node_modules.js')
-  },
-  entry: {
+    your_platform_node_modules: path.resolve(__dirname, '..', 'app', 'javascripts', 'your_platform_node_modules.js'),
     vue_app: path.resolve(__dirname, '..', 'app', 'javascripts', 'VueApp.js')
   },
   output: {
