3f8170edf7a12705aea20205f86a5578b338528e
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-09-03 15:50:09 +0200

    fixing "WARN Selenium [DEPRECATION] :timeout= is deprecated. Use #read_timeout= and #open_timeout= instead."

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,7 +97,8 @@ Geocoder.configure( lookup: :test )
 if ENV['SELENIUM']
   Capybara.register_driver :selenium_with_long_timeout do |app|
     client = Selenium::WebDriver::Remote::Http::Default.new
-    client.timeout = 120
+    client.read_timeout = 120
+    client.open_timeout = 120
     Capybara::Selenium::Driver.new(app, http_client: client)
   end
   Capybara.javascript_driver = :selenium_with_long_timeout
