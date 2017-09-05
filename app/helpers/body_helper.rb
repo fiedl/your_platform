388289ce45b525831d0b551a77cb535e4b286246
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-09-06 00:51:02 +0200

    cosmetic refactoring `body_tag` helper.
    I’m experimenting with how to init the vuejs app.
    
    https://trello.com/c/i7obt7JX/1194-störung-server-sehr-träge

--- app/helpers/body_helper.rb
+++ app/helpers/body_helper.rb
@@ -1,26 +1,24 @@
 module BodyHelper
 
-  def body_tag(options = {})
-    content_tag :body,
-      class: [
-        controller.controller_name,
-        "#{current_layout}-layout",
-        "#{current_layout}_layout",
-        @navable.try(:class).try(:name).try(:parameterize),
-        ("demo_mode" if demo_mode?),
-        ("ios" if params[:app] == "ios"),
-        options[:class]
-      ].join(" "),
-      data: {
-        locale: I18n.locale,
-        env: Rails.env.to_s,
-        layout: current_layout,
-        navable: @navable.try(:to_global_id).try(:to_s),
-        tab: current_tab
-      } do
-        content_tag :div, id: 'vue-app' do
-        yield
-      end
+  def body_tag(options = {}, &block)
+    css_classes = [
+      controller.controller_name,
+      "#{current_layout}-layout",
+      "#{current_layout}_layout",
+      @navable.try(:class).try(:name).try(:parameterize),
+      ("demo_mode" if demo_mode?),
+      ("ios" if params[:app] == "ios"),
+      options[:class]
+    ]
+    data = {
+      locale: I18n.locale,
+      env: Rails.env.to_s,
+      layout: current_layout,
+      navable: @navable.try(:to_global_id).try(:to_s),
+      tab: current_tab
+    }
+    content_tag :body, class: css_classes.join(" "), data: data do
+      (content_tag(:div, id: 'vue-app') { yield })
     end
   end
 
