8ac229b1bf44b934638c9e0ef4452b88e7ffbfbe
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-09-08 13:08:45 +0200

    adding `public-website` class to body if applicable to allow layout modifications for the public pages
    https://trello.com/c/JZzRpifA/1201-single-page-layout-anpassungen-für-wi
    ngolforg-2017-xood7uid

--- app/helpers/body_helper.rb
+++ app/helpers/body_helper.rb
@@ -6,6 +6,7 @@ module BodyHelper
       "#{current_layout}-layout",
       "#{current_layout}_layout",
       @navable.try(:class).try(:name).try(:parameterize),
+      ("public-website" if @navable.kind_of?(Page) && @navable.public?),
       ("demo_mode" if demo_mode?),
       ("ios" if params[:app] == "ios"),
       options[:class]
