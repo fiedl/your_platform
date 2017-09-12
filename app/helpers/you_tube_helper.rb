ff49909dc63e8eb0c8edd58ff14f3b5a6c9de12d
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-09-12 08:51:22 +0200

    switching youtube back to autohtml
    due to issues with galleria/turbolinks/jquery when using with videos.

--- app/helpers/you_tube_helper.rb
+++ app/helpers/you_tube_helper.rb
@@ -1,21 +1,24 @@
 module YouTubeHelper
 
-  # Convert YouTube links into galleria youtube players.
-  #
-  def youtubify(content)
-    content.gsub(/https:\/\/www\.youtube\.com\/watch[^\n ]*/) do |you_tube_link|
-      video_gallery(you_tube_link)
-    end
-  end
-
-  # # Convert YouTube links to embedding iframes.
-  # # Using auto html: https://github.com/dejan/auto_html
+  # # Convert YouTube links into galleria youtube players.
+  # #
+  # # 2017-09-12: Has issues with jquery/turbolinks after
+  # # rails-5 update.
   # #
   # def youtubify(content)
-  #   AutoHtml.auto_html(content) do
-  #     youtube(autoplay: false)
+  #   content.gsub(/https:\/\/www\.youtube\.com\/watch[^\n ]*/) do |you_tube_link|
+  #     video_gallery(you_tube_link)
   #   end.html_safe
   # end
+
+  # Convert YouTube links to embedding iframes.
+  # Using auto html: https://github.com/dejan/auto_html
+  #
+  def youtubify(content)
+    AutoHtml.auto_html(content) do
+      youtube(autoplay: false)
+    end.html_safe
+  end
 
   def you_tube(content)
     youtubify content
