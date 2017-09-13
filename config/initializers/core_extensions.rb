0b4ebc304734cfa73145be3300cc2a8beedcdf76
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-09-13 12:45:29 +0200

    moving best_in_place overrides to core_ext
    no actual changes here, just migrating to the new mechanism.
    
    https://trello.com/c/K1GcaUG2/1199-fix-jahreszahl-in-datum-umwandeln

--- config/initializers/core_extensions.rb
+++ config/initializers/core_extensions.rb
@@ -5,3 +5,6 @@ require_relative '../../core_ext/action_dispatch/routing'
 require_relative '../../core_ext/active_record/relation'
 
 require_relative '../../core_ext/acts_as_dag'
+
+require_relative '../../core_ext/best_in_place/controller_extensions'
+require_relative '../../core_ext/best_in_place/helper'
--- lib/your_platform.rb
+++ lib/your_platform.rb
@@ -1,9 +1,5 @@
 # YourPlatform Dependencies
 require "your_platform/engine"
 
-# Overrides for gems
-require_dependency YourPlatform::Engine.root.join('lib/best_in_place/helper').to_s
-require_dependency YourPlatform::Engine.root.join('lib/best_in_place/controller_extensions').to_s
-
 module YourPlatform
 end
