a2877181cc90ba8731397cb0f63390c4ade4b43c
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-10-21 00:55:43 +0200

    graph database: adding memberships
    - allow to customize the attributes that are copied to neo4j
    - fix direction: use `HAS_SUBGROUP` rather than `IS_SUBGROUP_OF`
    - write memberships as nodes and add relations
    - remove duplicate relationships of memberships
    
    https://trello.com/c/kM0spWJH/1211-neo4j-via-rest-auf-docker

--- app/models/graph_database.rb
+++ app/models/graph_database.rb
@@ -5,20 +5,28 @@ class GraphDatabase
   end
 
   def self.write_user(user)
-    write_object user, "User"
+    write_object user, "User", id: user.id, name: user.name.to_s, title: user.title.to_s
   end
 
   def self.get_user_node(user)
     get_object_node(user, "User")
   end
 
-  def self.write_group(group)
-    write_object group, "Group"
+  def self.write_users
+    Group.alle_wingolfiten.members.each do |user|
+      write_user user
+    end
   end
 
-  def self.write_object(object, type)
+  def self.write_group(group)
+    write_object group, "Group", name: group.name.to_s, type: group.type.to_s
+  end
+
+  def self.write_object(object, type, attributes = nil)
     node = get_object_node(object, type) || neo.create_node
-    neo.reset_node_properties node, id: object.id, name: object.name.to_s, url: object.url, gid: object.gid
+    attributes ||= {name: object.name.to_s}
+    attributes = attributes.merge({id: object.id, url: object.url, gid: object.gid})
+    neo.reset_node_properties node, attributes
     neo.set_label node, type
     node
   end
@@ -32,7 +40,7 @@ class GraphDatabase
   end
 
   def self.create_subgroup_relation(parent, child)
-    neo.create_relationship "IS_SUBGROUP_OF", get_group_node(parent), get_group_node(child)
+    neo.create_relationship "HAS_SUBGROUP", get_group_node(parent), get_group_node(child)
   end
 
   def self.write_groups
@@ -49,7 +57,7 @@ class GraphDatabase
 
   def self.get_descendant_group_ids(group)
     neo.execute_query("
-      match (parent:Group {id: #{group.id}})-[:IS_SUBGROUP_OF*]->(groups:Group)
+      match (parent:Group {id: #{group.id}})-[:HAS_SUBGROUP*]->(groups:Group)
       return groups.id
     ")['data'].flatten
   end
@@ -61,6 +69,42 @@ class GraphDatabase
   def self.create_group_id_index
     # In order to make finding groups by id faster.
     neo.create_schema_index "Group", ["id"]
+  end
+
+  def self.get_membership_node(membership)
+    get_object_node(membership, "Membership")
+  end
+
+  def self.write_membership(membership)
+    write_object membership, "Membership", valid_from: membership.valid_from.to_s, valid_to: membership.valid_to.to_s
+  end
+
+  def self.create_membership(membership)
+    membership_node = write_membership(membership)
+    group_node = get_group_node(membership.group)
+    user_node = get_user_node(membership.user)
+    neo.execute_query("match (m:Membership {id: #{membership.id}})-[r]-() delete r")
+    neo.create_relationship "HAS_MEMBERSHIP", group_node, membership_node
+    neo.create_relationship "HAS_MEMBER", membership_node, user_node
+    membership_node
+  end
+
+  def self.write_memberships(group = nil)
+    memberships = DagLink.where(ancestor_type: "Group", descendant_type: "User", direct: true, valid_to: nil)
+    memberships = memberships.where(ancestor_id: [group.id] + group.descendant_groups.pluck(:id)) if group
+    memberships.each do |membership|
+      if membership.user.kind_of?(User) && membership.group.kind_of?(Group) && membership.user.wingolfit?
+        create_membership membership
+      end
+    end
+  end
+
+  def self.get_member_ids(group)
+    neo.execute_query("
+      match path = (group:Group {id: #{group.id}})-[:HAS_SUBGROUP*]->(g:Group)-[:HAS_MEMBERSHIP]->(m:Membership)-[:HAS_MEMBER]->(users:User)
+      where not g.type = 'OfficerGroup'
+      return distinct(users.id)
+    ")['data'].flatten
   end
 
 end
