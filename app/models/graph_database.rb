3b211eaa4a3e5f5632e3037b3186d455b8a24a8c
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-11-03 17:21:30 +0100

    neo4j: have memberships be represented by relations rather than nodes.
    With the new match syntax, this is as fast as before, but does match
    our data model much better.
    
    https://trello.com/c/kM0spWJH/1211-neo4j-via-rest-auf-docker

--- app/models/graph_database.rb
+++ app/models/graph_database.rb
@@ -4,6 +4,22 @@ class GraphDatabase
     @neo ||= Neography::Rest.new authentication: :basic, username: 'neo4j', password: 'trinity'
   end
 
+  def self.import(group)
+    write_group group
+    group.descendant_groups.each { |g| write_group g }
+    group.descendant_groups.each do |child|
+      child.parent_groups.each { |parent| create_subgroup_relation parent, child if parent.in?([group] + group.descendant_groups) }
+    end
+    write_users group
+    group.members.each do |user|
+      user.links_as_child.each do |m|
+        if m && m.direct && m.user && m.group && m.group.in?([group] + group.descendant_groups)
+          write_membership m
+        end
+      end
+    end
+  end
+
   def self.write_user(user)
     write_object user, "User", id: user.id, name: user.name.to_s, title: user.title.to_s
   end
@@ -12,8 +28,8 @@ class GraphDatabase
     get_object_node(user, "User")
   end
 
-  def self.write_users
-    Group.alle_wingolfiten.members.each do |user|
+  def self.write_users(group = nil)
+    (group || Group.alle_wingolfiten).members.each do |user|
       write_user user
     end
   end
@@ -40,7 +56,9 @@ class GraphDatabase
   end
 
   def self.create_subgroup_relation(parent, child)
-    neo.create_relationship "HAS_SUBGROUP", get_group_node(parent), get_group_node(child)
+    if neo.execute_query("match (:Group {id: #{parent.id}})-[r:HAS_SUBGROUP]->(:Group {id: #{child.id}}) return r")['data'].flatten.count == 0
+      neo.create_relationship "HAS_SUBGROUP", get_group_node(parent), get_group_node(child)
+    end
   end
 
   def self.write_groups
@@ -71,24 +89,29 @@ class GraphDatabase
     neo.create_schema_index "Group", ["id"]
   end
 
-  def self.get_membership_node(membership)
-    get_object_node(membership, "Membership")
+  def self.get_membership_relation(membership)
+    neo.execute_query("
+      match ()-[r:MEMBERSHIP]-()
+      where r.id = #{membership.id}
+      return r
+    ")['data'].flatten.first
+  end
+
+  def self.get_or_create_membership_relation(membership)
+    get_membership_relation(membership) || begin
+      group_node = get_group_node(membership.group)
+      user_node = get_user_node(membership.user)
+      if group_node && user_node
+        neo.create_relationship "MEMBERSHIP", group_node, user_node, id: membership.id
+      else
+        nil
+      end
+    end
   end
 
   def self.write_membership(membership)
-    write_object membership, "Membership", valid_from: membership.valid_from.to_s, valid_to: membership.valid_to.to_s
-  end
-
-  def self.create_membership(membership)
-    membership_node = write_membership(membership)
-    group_node = get_group_node(membership.group)
-    user_node = get_user_node(membership.user)
-    neo.execute_query("match (m:Membership {id: #{membership.id}})-[r]-() delete r")
-    if user_node && group_node
-      neo.create_relationship "HAS_MEMBERSHIP", group_node, membership_node
-      neo.create_relationship "HAS_MEMBER", membership_node, user_node
-    end
-    membership_node
+    properties = {valid_from: membership.valid_from.to_s, valid_to: membership.valid_to.to_s}
+    neo.set_relationship_properties get_or_create_membership_relation(membership)['metadata']['id'], properties
   end
 
   def self.write_memberships(group = nil)
@@ -96,15 +119,20 @@ class GraphDatabase
     memberships = memberships.where(ancestor_id: [group.id] + group.descendant_groups.pluck(:id)) if group
     memberships.each do |membership|
       if membership.user.kind_of?(User) && membership.group.kind_of?(Group) && membership.user.wingolfit?
-        create_membership membership
+        write_membership membership
       end
     end
   end
 
   def self.get_member_ids(group)
     neo.execute_query("
-      match path = (group:Group {id: #{group.id}})-[:HAS_SUBGROUP*]->(g:Group)-[:HAS_MEMBERSHIP]->(m:Membership)-[:HAS_MEMBER]->(users:User)
+      match path = (group:Group {id: #{group.id}})-[:HAS_SUBGROUP*]->(g:Group)-[m:MEMBERSHIP]->(users:User)
       where not g.type = 'OfficerGroup'
+      and m.valid_to = ''
+      return distinct(users.id)
+      union
+      match path = (group:Group {id: #{group.id}})-[m:MEMBERSHIP]->(users:User)
+      where m.valid_to = ''
       return distinct(users.id)
     ")['data'].flatten
   end
