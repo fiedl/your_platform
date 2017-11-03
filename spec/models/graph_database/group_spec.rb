576d15f1d4a911b3894bc82fd01a1fd33b7dcc2f
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-11-03 23:35:08 +0100

    neo4j: only run the specs if neo4j is configured.
    We don’t have it on travis, yet.
    
    https://trello.com/c/kM0spWJH/1211-neo4j-via-rest-auf-docker

--- spec/models/graph_database/group_spec.rb
+++ spec/models/graph_database/group_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 
-describe GraphDatabase::Group do
+if GraphDatabase::Base.configured?
+  describe GraphDatabase::Group do
 
-  before do
-    @group = create :group
-    @subgroup = @group.child_groups.create
-    @user = create :user
-    @subgroup.assign_user @user
+    before do
+      @group = create :group
+      @subgroup = @group.child_groups.create
+      @user = create :user
+      @subgroup.assign_user @user
+    end
+
+    describe ".get_member_ids" do
+      subject { GraphDatabase::Group.get_member_ids @group }
+      it { should == [@user.id] }
+    end
+
   end
-
-  describe ".get_member_ids" do
-    subject { GraphDatabase::Group.get_member_ids @group }
-    it { should == [@user.id] }
-  end
-
 end
