3d66290587f1ddb11ef903e21c9b39a7b5cc063a
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-11-10 15:18:10 +0100

    use graph database to determine `User#page_ids_of_pages_the_user_is_officer_of`.
    The dag link solution keeps producing unreasonable errors.
    
    https://trello.com/c/UVpa6ee7/1216-inbetriebnahme-des-homepage-moduls-fü
    r-wingolforg
    
    https://trello.com/c/kM0spWJH/1211-neo4j-via-rest-auf-docker

--- app/models/concerns/user_roles.rb
+++ app/models/concerns/user_roles.rb
@@ -203,7 +203,13 @@ concern :UserRoles do
   end
 
   def page_ids_of_pages_the_user_is_officer_of
-    self.groups.where(type: "OfficerGroup").collect(&:scope).select { |scope| scope.kind_of?(Page) }.collect { |page| page.sub_page_ids }.flatten.uniq
+    # # This is the proper command and it does work in the console.
+    # # But in the browser, as well in development as in production, this raises
+    # # "Mysql2::Error: Not unique table/alias".
+    # self.groups.where(type: "OfficerGroup").collect(&:scope).select { |scope| scope.kind_of?(Page) }.collect { |page| page.sub_page_ids }.flatten.uniq
+
+    # It's much easier with the graph database:
+    Graph::User.find(self).page_ids_of_pages_the_user_is_page_officer_of
   end
 
   def pages_the_user_is_officer_of
--- app/models/graph/user.rb
+++ app/models/graph/user.rb
@@ -12,4 +12,13 @@ class Graph::User < Graph::Node
     {id: user.id, name: user.name.to_s, title: user.title.to_s, first_name: user.first_name.to_s, last_name: user.last_name.to_s, name_affix: user.name_affix.to_s}
   end
 
+  def page_ids_of_pages_the_user_is_page_officer_of
+    query_ids("
+      match (user:User {id: #{user.id}})<-[m:MEMBERSHIP]-(:Group {type: 'OfficerGroup'})<-[:HAS_SUBGROUP*0..5]-(:Group {type: 'OfficerGroup'})<--(officers_parent:Group)<--(scope:Page)-[:HAS_SUBPAGE*]->(subpages:Page)
+      where not (subpages)<-[*]-(:Page {flags: 'intranet_root'})
+      and #{Graph::Membership.validity_range_condition}
+      return scope.id, subpages.id
+    ").uniq
+  end
+
 end
