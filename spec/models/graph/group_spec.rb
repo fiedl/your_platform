1ab9020cee649fdd5554a49f38eb356b6afac78f
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-11-20 22:52:18 +0100

    clean-up graph specs (no actual changes)

--- spec/models/graph/group_spec.rb
+++ spec/models/graph/group_spec.rb
@@ -1,70 +1,68 @@
 require 'spec_helper'
 
-if Graph::Base.configured?
-  describe Graph::Group do
+describe Graph::Group do
 
-    before do
-      @group = create :group
-      @subgroup = @group.child_groups.create
-      @user = create :user
-      @membership = @subgroup.assign_user @user
-    end
-
-    describe "#descendant_member_ids" do
-      subject { Graph::Group.find(@group).descendant_member_ids }
-      it { should == [@user.id] }
-
-      describe "when the group has current members" do
-        before do
-          @current_member = create :user
-          @current_membership = @subgroup.assign_user @current_member, at: 2.years.ago
-        end
-
-        it { should include @current_member.id }
-      end
-      describe "when the group has past members" do
-        before do
-          @past_member = create :user
-          @past_membership = @subgroup.assign_user @past_member, at: 10.years.ago
-          @past_membership.update_attributes valid_to: 1.year.ago
-        end
-
-        it { should_not include @past_member.id }
-      end
-      describe "when the group has current members with limited memberships" do
-        before do
-          @current_member_with_limited_membership = create :user
-          @current_but_limited_membership = @subgroup.assign_user @current_member_with_limited_membership, at: 10.years.ago
-          @current_but_limited_membership.update_attributes valid_to: 1.year.from_now
-        end
-
-        it { should include @current_member_with_limited_membership.id }
-      end
-    end
-
-    describe ".get_descendant_group_ids" do
-      subject { Graph::Group.find(@group).descendant_group_ids }
-      it { should == [@subgroup.id] }
-    end
-
-    describe ".get_membership_ids" do
-      subject { Graph::Group.find(@group).descendant_membership_ids }
-      it { should == [@membership.id]}
-    end
-
-    # describe "#descendant_event_ids" do
-    #   subject { Graph::Group.find(@group).descendant_event_ids }
-    #
-    #   describe "when the group has direct events" do
-    #     before { @event = @group.events.create name: "Some event" }
-    #     it { should include @event.id }
-    #   end
-    #
-    #   describe "when a subgroup has direct events" do
-    #     before { @subgroup_event = @subgroup.events.create name: "Subgroup event" }
-    #     it { should include @subgroup_event.id }
-    #   end
-    # end
-
+  before do
+    @group = create :group
+    @subgroup = @group.child_groups.create
+    @user = create :user
+    @membership = @subgroup.assign_user @user
   end
+
+  describe "#descendant_member_ids" do
+    subject { Graph::Group.find(@group).descendant_member_ids }
+    it { should == [@user.id] }
+
+    describe "when the group has current members" do
+      before do
+        @current_member = create :user
+        @current_membership = @subgroup.assign_user @current_member, at: 2.years.ago
+      end
+
+      it { should include @current_member.id }
+    end
+    describe "when the group has past members" do
+      before do
+        @past_member = create :user
+        @past_membership = @subgroup.assign_user @past_member, at: 10.years.ago
+        @past_membership.update_attributes valid_to: 1.year.ago
+      end
+
+      it { should_not include @past_member.id }
+    end
+    describe "when the group has current members with limited memberships" do
+      before do
+        @current_member_with_limited_membership = create :user
+        @current_but_limited_membership = @subgroup.assign_user @current_member_with_limited_membership, at: 10.years.ago
+        @current_but_limited_membership.update_attributes valid_to: 1.year.from_now
+      end
+
+      it { should include @current_member_with_limited_membership.id }
+    end
+  end
+
+  describe "#descendant_group_ids" do
+    subject { Graph::Group.find(@group).descendant_group_ids }
+    it { should == [@subgroup.id] }
+  end
+
+  describe "#descendant_membership_ids" do
+    subject { Graph::Group.find(@group).descendant_membership_ids }
+    it { should == [@membership.id]}
+  end
+
+  # describe "#descendant_event_ids" do
+  #   subject { Graph::Group.find(@group).descendant_event_ids }
+  #
+  #   describe "when the group has direct events" do
+  #     before { @event = @group.events.create name: "Some event" }
+  #     it { should include @event.id }
+  #   end
+  #
+  #   describe "when a subgroup has direct events" do
+  #     before { @subgroup_event = @subgroup.events.create name: "Subgroup event" }
+  #     it { should include @subgroup_event.id }
+  #   end
+  # end
+
 end
