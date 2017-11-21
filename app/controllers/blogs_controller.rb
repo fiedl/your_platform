d421c46d3bc24c363a18959183f868bbb357838e
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-11-21 19:56:08 +0100

    showing tags in sidebar of blogs#show

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,7 @@ class BlogsController < PagesController #ApplicationController
   def show
     @blog_root_page = @blog ||= @page
     @blog_posts = @blog.blog_entries.visible_to(current_user)
+    @tags = @blog_posts.collect { |entry| entry.tags }.flatten.uniq.sort_by(&:taggings_count)
 
     set_current_navable @blog_root_page
     set_current_title @blog_root_page.title
