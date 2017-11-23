c8344e0fbaf9477b7aec5e67aa5a610950db16bf
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-11-23 12:28:56 +0100

    fixing blog post tag cloud order

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,7 @@ class BlogsController < PagesController #ApplicationController
   def show
     @blog_root_page = @blog ||= @page
     @blog_posts = @blog.blog_entries.visible_to(current_user)
-    @tags = @blog_posts.collect { |entry| entry.tags }.flatten.uniq.sort_by(&:taggings_count)
+    @tags = @blog_posts.collect { |entry| entry.tags }.flatten.uniq.sort_by(&:taggings_count).reverse
 
     set_current_navable @blog_root_page
     set_current_title @blog_root_page.title
