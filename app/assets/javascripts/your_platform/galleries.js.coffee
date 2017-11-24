fiedl/your_platform

commit 6c5d5e6fb52f5c3f4e6c9a12cef01952d1bea273

galleria: using callback rather than ready binding

app/assets/javascripts/your_platform/gallery.js.coffee
@@ -3,7 +3,6 @@ class App.Gallery
   root_element: {}
   galleria_instance: {}
   current_image_data: {}
-  ready_bindings_done: false
 
   # Initialize a `Gallery` with a `$('.galleria')` element.
   #
@@ -11,10 +10,10 @@ class App.Gallery
   #
   constructor: (root_element)->
     @root_element = root_element
+
     @generate_uniqe_id()
     @initSettings()
     @initTheme()
-    @bindGalleriaReady()
     @runGalleria()
     @store_gallery_instance_in_data_attribute()
 
@@ -27,27 +26,29 @@ class App.Gallery
   # Basic galleria configuration.
   # See: http://galleria.io/docs/options/
   #
-  default_galleria_options: -> {
-    imageCrop: 'landscape',
-    transition: 'slide',
-    initialTransition: 'fade',
-    assets: false,  # The images are not serverd as assets but by the attachments controller.
-    _toggleInfo: true,
-    preload: 0,
-    #autoplay: 9000,
-    popupLinks: false,
-    trueFullscreen: false,
-    carousel: false,
-    thumbnails: false,
-    #swipe: 'auto',
-    responsive: true,
-    height: @default_height(),
-    debug: false,
-    ## height: $(this).find('img').attr('height')
-    lightbox: true,
-    thumbnails: false,
-    imageMargin: 0,
-  }
+  default_galleria_options: ->
+    self = this
+    {
+      imageCrop: 'landscape',
+      transition: 'slide',
+      initialTransition: 'fade',
+      assets: false,  # The images are not serverd as assets but by the attachments controller.
+      _toggleInfo: true,
+      preload: 0,
+      #autoplay: 9000,
+      popupLinks: false,
+      trueFullscreen: false,
+      carousel: false,
+      thumbnails: false,
+      #swipe: 'auto',
+      responsive: true,
+      height: @default_height(),
+      debug: true,
+      lightbox: true,
+      thumbnails: false,
+      imageMargin: 0,
+      extend: -> self.initCallback(self, this)
+    }
 
   default_height: ->
     1.0 / @aspect_ratio_of_first_image()
@@ -70,39 +71,35 @@ class App.Gallery
     self = this
     Galleria.run(self.unique_id(), self.default_galleria_options())
 
+  initCallback: (self, galleria_instance)->
+    # This is called by galleria through the `extend` option
+    # after finishing `Galleria.run`. In this callback, `galleria_instance`
+    # is refering to the galleria instance, `self` is refering
+    # to the `App.Gallery` instance.
+    #
+    self.galleria_instance = galleria_instance
+
+    # When loading a gallery image, also update the description
+    # shown below the image.
+    #
+    self.galleria_instance.bind 'loadfinish', (e)->
+      self.add_magnification_glass() # needs to be in loadfinish to detect video
+      self.active_video_on_mobile()
+      if e.galleriaData
+        self.current_image_data = e.galleriaData
+        self.find('.galleria-image').removeClass('active')
+        $(e.imageTarget).closest('.galleria-image').addClass('active')
+        self.show_description()
+
+    if self.root_element.hasClass('deactivate-auto-lightbox')
+      self.galleria_instance.setOptions 'lightbox', false
+
+    self.hide_thumbs_or_slideshow()
+    self.hide_errors_container()
+    self.bind_fullscreen_events()
+
   store_gallery_instance_in_data_attribute: ->
     @root_element.data('gallery', this)
-
-  bindGalleriaReady: ->
-    self = this
-    Galleria.ready ->
-      # The Galleria.ready is fired several times, since it's bound to
-      # each instance, again. Therefore, we need to find out, here, whether
-      # to process the following stuff, now.
-      #
-      if self.root_element and self.find('.galleria-thumbnails').length > 0 and self.root_element.attr('id') == this._target.id
-
-        self.ready_bindings_done = true
-        self.galleria_instance = this
-
-        # When loading a gallery image, also update the description
-        # shown below the image.
-        #
-        self.galleria_instance.bind 'loadfinish', (e)->
-          self.add_magnification_glass() # needs to be in loadfinish to detect video
-          self.active_video_on_mobile()
-          if e.galleriaData
-            self.current_image_data = e.galleriaData
-            self.find('.galleria-image').removeClass('active')
-            $(e.imageTarget).closest('.galleria-image').addClass('active')
-            self.show_description()
-
-        if self.root_element.hasClass('deactivate-auto-lightbox')
-          self.galleria_instance.setOptions 'lightbox', false
-
-        self.hide_thumbs_or_slideshow()
-        self.hide_errors_container()
-        self.bind_fullscreen_events()
 
   has_unique_id: ->
     (@root_element.attr('id') || "").indexOf('ui-id') > -1

commit 9c3c3081da6d3cf2538a8fc810ff83a1b0fcc9b4

fixing galleria initialization
this has been broken in
https://github.com/fiedl/your_platform/commit/6c5d5e6fb52f5c3f4e6c9a12ce
f01952d1bea273#diff-6acd520299eb04f8622493bc95ac0da6.

https://trello.com/c/UVpa6ee7/1216-inbetriebnahme-des-homepage-moduls-fü
r-wingolforg-2017-zu8ahm2i

app/assets/javascripts/your_platform/galleries.js.coffee
@@ -9,8 +9,10 @@ class App.Galleries
   process: (root_element)->
     if root_element.find('.galleria').count() > 0
       root_element.find('.galleria').each (index, galleria_element)->
-        $(galleria_element).uniqueId()
-        App.gallery_instances.push new App.Gallery($(galleria_element))
+
+        jquery_galleria_element = $(galleria_element)
+        jquery_galleria_element.uniqueId()
+        App.gallery_instances.push new App.Gallery(jquery_galleria_element)
 
   clean: ->
     for gallery in App.gallery_instances

app/assets/javascripts/your_platform/gallery.js.coffee
@@ -3,6 +3,7 @@ class App.Gallery
   root_element: {}
   galleria_instance: {}
   current_image_data: {}
+  ready_bindings_done: false
 
   # Initialize a `Gallery` with a `$('.galleria')` element.
   #
@@ -10,10 +11,10 @@ class App.Gallery
   #
   constructor: (root_element)->
     @root_element = root_element
-
     @generate_uniqe_id()
     @initSettings()
     @initTheme()
+    @bindGalleriaReady()
     @runGalleria()
     @store_gallery_instance_in_data_attribute()
 
@@ -46,8 +47,7 @@ class App.Gallery
       debug: true,
       lightbox: true,
       thumbnails: false,
-      imageMargin: 0,
-      extend: -> self.initCallback(self, this)
+      imageMargin: 0
     }
 
   default_height: ->
@@ -74,32 +74,36 @@ class App.Gallery
     self = this
     Galleria.run(self.unique_id(), self.default_galleria_options())
 
-  initCallback: (self, galleria_instance)->
-    # This is called by galleria through the `extend` option
-    # after finishing `Galleria.run`. In this callback, `galleria_instance`
-    # is refering to the galleria instance, `self` is refering
-    # to the `App.Gallery` instance.
-    #
-    self.galleria_instance = galleria_instance
+  bindGalleriaReady: ->
+    self = this
 
-    # When loading a gallery image, also update the description
-    # shown below the image.
-    #
-    self.galleria_instance.bind 'loadfinish', (e)->
-      self.add_magnification_glass() # needs to be in loadfinish to detect video
-      self.active_video_on_mobile()
-      if e.galleriaData
-        self.current_image_data = e.galleriaData
-        self.find('.galleria-image').removeClass('active')
-        $(e.imageTarget).closest('.galleria-image').addClass('active')
-        self.show_description()
+    Galleria.ready ->
+      # The Galleria.ready is fired several times, since it's bound to
+      # each instance, again. Therefore, we need to find out, here, whether
+      # to process the following stuff, now.
+      #
+      if self.root_element and self.find('.galleria-thumbnails').length > 0 and self.root_element.attr('id') == this._target.id
 
-    if self.root_element.hasClass('deactivate-auto-lightbox')
-      self.galleria_instance.setOptions 'lightbox', false
+        self.ready_bindings_done = true
+        self.galleria_instance = this
 
-    self.hide_thumbs_or_slideshow()
-    self.hide_errors_container()
-    self.bind_fullscreen_events()
+        # When loading a gallery image, also update the description
+        # shown below the image.
+        #
+        self.galleria_instance.bind 'loadfinish', (e)->
+          self.active_video_on_mobile()
+          if e.galleriaData
+            self.current_image_data = e.galleriaData
+            self.find('.galleria-image').removeClass('active')
+            $(e.imageTarget).closest('.galleria-image').addClass('active')
+            self.show_description()
+
+        if self.root_element.hasClass('deactivate-auto-lightbox')
+          self.galleria_instance.setOptions 'lightbox', false
+
+        self.hide_thumbs_or_slideshow()
+        self.hide_errors_container()
+        self.bind_fullscreen_events()
 
   store_gallery_instance_in_data_attribute: ->
     @root_element.data('gallery', this)