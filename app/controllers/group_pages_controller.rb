14930742658f1d3eced2abbedac7985c11c1a841
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-11-28 10:31:03 +0100

    deactivating group_pages#index for now
    This feature is not ready, yet.

--- app/controllers/group_pages_controller.rb
+++ app/controllers/group_pages_controller.rb
@@ -3,26 +3,31 @@ class GroupPagesController < ApplicationController
   expose :group
 
   def index
-    unless redirect_if_cannot_read_pages
-      authorize! :read_pages, group
-
-      set_current_navable group
-      set_current_title group.title
-      set_current_tab :pages
-    end
+    authorize! :read, group
+    redirect_to group_members_path(group_id: group.id)
   end
 
-  private
-
-  def redirect_if_cannot_read_pages
-    if not can? :read_pages, group
-      if not current_user.try(:global_admin?) # Because global admins could use override.
-        authorize! :read, group
-        redirect_to group_members_path(group_id: group.id)
-        return true
-      end
-    end
-    return false
-  end
+  # def index
+  #   unless redirect_if_cannot_read_pages
+  #     authorize! :read_pages, group
+  #
+  #     set_current_navable group
+  #     set_current_title group.title
+  #     set_current_tab :pages
+  #   end
+  # end
+  #
+  # private
+  #
+  # def redirect_if_cannot_read_pages
+  #   if not can? :read_pages, group
+  #     if not current_user.try(:global_admin?) # Because global admins could use override.
+  #       authorize! :read, group
+  #       redirect_to group_members_path(group_id: group.id)
+  #       return true
+  #     end
+  #   end
+  #   return false
+  # end
 
 end
