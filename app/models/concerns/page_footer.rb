aeab23143f4a8daa05059c5feff98d6c1059259c
Sebastian Fiedlschuster <github@fiedlschuster.de>
2017-12-14 23:23:33 +0100

    allow embedding pages in the footer.
    For example, for rendering contact information there.
    
    https://trello.com/c/UVpa6ee7/1216-inbetriebnahme-des-homepage-moduls-fü
    r-wingolforg-2017-zu8ahm2i

--- app/models/concerns/page_footer.rb
+++ app/models/concerns/page_footer.rb
@@ -16,4 +16,20 @@ module PageFooter
     end
   end
 
+  def footer_embedded
+    has_flag? :footer_embedded
+  end
+
+  def footer_embedded?
+    footer_embedded
+  end
+
+  def footer_embedded=(new_setting)
+    if new_setting
+      add_flag :footer_embedded
+    else
+      remove_flag :footer_embedded
+    end
+  end
+
 end
