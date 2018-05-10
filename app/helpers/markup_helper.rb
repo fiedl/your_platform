ea77ee7fd420534ee916795ae29d9f9368a8ea8c
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-05-10 18:10:13 +0200

    pages: add support for embedding video files via pasting the url.
    https://trello.com/c/yUIMikgs/1259-eingebettete-mp4-dateien

--- app/helpers/markup_helper.rb
+++ app/helpers/markup_helper.rb
@@ -12,7 +12,7 @@ module MarkupHelper
   def markup(text)
     if text
       unless text.downcase.include?("<html>") or text.downcase.include?("<html ")
-        emojify markdown replace_quick_link_tags mentionify youtubify text
+        emojify markdown replace_quick_link_tags mentionify replace_video_links youtubify text
       else
         strip_escaped_comments sanitize text
       end
--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -10,17 +10,40 @@ module VideoHelper
     # TODO: Fullscreen via double click.
     # https://github.com/moay/afterglow/issues/27
 
-    video_tag attachment.file.url, {
-      class: 'afterglow video',
+    video_tag attachment.file.url, video_tag_default_options.merge({
       id: "video-attachment-#{attachment.id}",
       width: attachment.width,
-      height: attachment.height,
+      height: attachment.height
+    })
+  end
+
+  def replace_video_links(markup)
+    markup.gsub(/(.*.(mp4|m4v))/) { |url|
+      #video_tag(url, video_tag_default_options)
+      content_tag :video, {controls: true, class: 'video'} do
+        content_tag :source, "", {src: url, type: "video/mp4"}
+      end
+    }
+  end
+
+  def video_tag_default_options
+    {
+      class: 'afterglow video',
       type: 'video/mp4',
-      controls: "true",
+      controls: true,
       data: {
         autoresize: 'fit'
       }
     }
   end
 
+end
+
+# In order to use the markup helper method with best_in_place's :display_with argument,
+# the ActionView::Base has to include the markup method.
+#
+module ActionView
+  class Base
+    include VideoHelper
+  end
 end
