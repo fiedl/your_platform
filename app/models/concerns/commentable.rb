09f6e7e83bab80e4b7cf6796c04c63d021b5fe36
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-05-12 22:02:14 +0200

    make blog-post comments optional
    i.e. the author can turn comments on and off.
    
    https://trello.com/c/9sTF3bTr/1260-blog-post-polishing

--- app/models/concerns/commentable.rb
+++ app/models/concerns/commentable.rb
@@ -4,4 +4,16 @@ concern :Commentable do
     has_many :comments, as: :commentable, dependent: :destroy
   end
 
+  def comments_enabled?
+    comments_enabled || false
+  end
+
+  def comments_enabled
+    self.settings.comments_enabled
+  end
+
+  def comments_enabled=(new_setting)
+    self.settings.comments_enabled = new_setting
+  end
+
 end
