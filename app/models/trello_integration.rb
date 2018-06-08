cc319241430c99061ffc235174d96892ad933926
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-06-08 20:26:43 +0200

    features: list Doing from trello there
    https://trello.com/c/v7qiuYIv/1279-features-seite

--- app/models/trello_integration.rb
+++ app/models/trello_integration.rb
@@ -16,28 +16,49 @@ class TrelloIntegration
     @connected
   end
 
-  def trello_bot_public_key
-    Setting.trello_bot_public_key
-  end
+  concerning :BotUser do
+    def trello_bot_public_key
+      Setting.trello_bot_public_key
+    end
 
-  def trello_bot_auth_token
-    Setting.trello_bot_auth_token
-  end
+    def trello_bot_auth_token
+      Setting.trello_bot_auth_token
+    end
 
-  def trello_user
-    begin
-      Trello::Member.find('me')
-    rescue
-      nil
+    def trello_user
+      begin
+        Trello::Member.find('me')
+      rescue
+        nil
+      end
+    end
+
+    def user_name
+      trello_user.try(:username)
+    end
+
+    def avatar_url
+      trello_user.try(:avatar_url)
     end
   end
 
-  def user_name
-    trello_user.try(:username)
-  end
+  concerning :FeaturesBoard do
+    def features_board
+      board_id = Setting.trello_board_url_for_features.split("/")[4]
+      Trello::Board.find(board_id)
+    end
 
-  def avatar_url
-    trello_user.try(:avatar_url)
+    def features_doing_lists
+      Rails.cache.fetch "TrelloIntegration#features_doing_lists", expires_in: 14.days do
+        features_board.lists.select { |list| list.name.include? "Doing" }
+      end
+    end
+
+    def features_doing_cards
+      Rails.cache.fetch "TrelloIntegration#features_doing_cards", expires_in: 15.minutes do
+        features_doing_lists.to_a.collect { |list| list.cards.target.to_a }.flatten
+      end
+    end
   end
 
 end
