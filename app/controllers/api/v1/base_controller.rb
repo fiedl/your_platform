e2c590a8e7760ff33e5da54d76cc6d4cf6ab28f8
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-11-03 01:39:19 +0100

    token auth: fixing issue where `current_user` is not available based on the http-header acess-token
    https://trello.com/c/Hj9p1WGu/1301-devise-token-auth
    https://trello.com/c/v6Aw9zNP/1-authentifizierung

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,6 +4,6 @@ class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session, only: -> { request.format.json? }
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  before_action :authenticate_user_account!
+  before_action :authenticate_api_v1_user_account!
 
 end
