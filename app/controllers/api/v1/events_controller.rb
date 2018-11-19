df05a3f49b7f7c4e475f328da419e0e1b086d456
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-11-19 01:26:44 +0100

    api: add switch for upcoming and recent events
    https://trello.com/c/JvFNUPXM/4-veranstaltungen-auflisten

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,10 +4,19 @@ class Api::V1::EventsController < Api::V1::BaseController
     authorize! :index, Event
 
     if params[:public]
-      @events = Event.upcoming.where(publish_on_global_website: true)
+      @events = Event.where(publish_on_global_website: true)
+    elsif params[:all]
+      @events = Event.all
     else
-      @events = current_user.upcoming_events
+      @events = current_user.events
     end
+
+    if params[:with_recent]
+      @events = @events.where('start_at > ?', 3.months.ago)
+    elsif params[:upcoming]
+      @events = @events.upcoming
+    end
+
     @events = @events.order 'events.start_at, events.created_at'
     @events = @events.select { |event| can? :read, event }
 
