1b6b67f2e1a7bf8c5a7ecf595e5cf9fb5cdd1cbe
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-11-25 01:41:38 +0100

    user search api: also list dummy user
    required for app-store review.
    
    https://trello.com/c/pUHs2dht/18-dummy-user

--- app/controllers/api/v1/search_users_controller.rb
+++ app/controllers/api/v1/search_users_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::SearchUsersController < Api::V1::BaseController
     authorize! :index, User
 
     @users = User.search(params[:query]) #, limit: params[:limit].try(:to_i))
-    @users = @users.select { |user| user.alive? && user.wingolfit? }
+    @users = @users.select { |user| (user.alive? && user.wingolfit?) || user.has_flag?(:dummy) }
     @users = @users.select { |user| can? :read, user }
 
     render json: @users.as_json(methods: [:name, :title, :avatar_url])
