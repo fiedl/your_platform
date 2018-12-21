bc6ec64349c605cc48364153da7d6487ad129f16
Sebastian Fiedlschuster <github@fiedlschuster.de>
2018-12-21 22:21:27 +0100

    making token authentication optional for the api.
    This way, our session cookie still works if the request originates in the web app.
    
    https://trello.com/c/Hj9p1WGu/1301-devise-token-auth

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,11 +4,18 @@ class Api::V1::BaseController < ApplicationController
   protect_from_forgery with: :null_session, only: -> { request.format.json? }
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  before_action :authenticate_api_v1_user_account!
+  before_action :authenticate_api_v1_user_account_if_needed
 
   # During the API test phase, we log the API requests, except for passwords.
   # `log_request` is defined in the `ApplicationController`.
   # The requests can be viewed at `/requests`.
   after_action :log_request
+
+  # The API can be used either by our mobile apps, using token authentication,
+  # or by our web app, using default session cookies.
+  #
+  def authenticate_api_v1_user_account_if_needed
+    authenticate_api_v1_user_account! if request.headers["HTTP_ACCESS_TOKEN"].present?
+  end
 
 end
