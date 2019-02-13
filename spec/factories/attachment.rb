2101ff65def7ce3eddcedc6d2f1c7b9c6050ea74
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-02-13 17:39:26 +0100

    trying to fix travis image error by modifying the attachment factory.
    In this hack, I'm setting the content type after the image processing has been run. Trying to resolve the error:
    
    ActiveRecord::RecordInvalid:
           Gültigkeitsprüfung ist fehlgeschlagen: Datei Failed to manipulate with MiniMagick, maybe it is not an image? Original Error: `identify /tmp/mini_magick20190213-25432-10wpzqr.pdf` failed with error:
                  identify.im6: Postscript delegate failed `/tmp/mini_magick20190213-25432-10wpzqr.pdf': No such file or directory @ error/pdf.c/ReadPDFImage/677.
    
    This issue only occurs on travis, not locally.

--- spec/factories/attachment.rb
+++ spec/factories/attachment.rb
@@ -2,10 +2,12 @@ FactoryGirl.define do
   factory :attachment do |f|
     f.title 'New Attachment'
     f.description 'Descriptive description of the new attachment.'
-    f.file { Rack::Test::UploadedFile.new(File.expand_path(File.join(__FILE__, '../../support/uploads/pdf-upload.pdf')), "application/pdf") }
+    f.file { Rack::Test::UploadedFile.new(File.expand_path(File.join(__FILE__, '../../support/uploads/pdf-upload.pdf'))) }
+    after(:create) { |attachment| attachment.update content_type: "application/pdf" }
 
     factory :image_attachment do |f|
       f.file { Rack::Test::UploadedFile.new(File.expand_path(File.join(__FILE__, '../../support/uploads/image-upload.png'))) }
+      after(:create) { |attachment| attachment.update content_type: "image/png" }
     end
 
   end
