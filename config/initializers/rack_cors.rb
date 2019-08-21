ec57c3c3e28cf79e0a810ffaa01598c98d68fce7
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-08-21 19:54:10 +0200

    rack_cors: Define exceptions for API as well as external login forms in one initializer.
    We had the latter in another intializer in the main application. But this had overwritten the first instance of the rack-cors middleware.
    
    https://trello.com/c/p7kSJGz5/1398-app-funktioniert-nicht-mehr-access-control-origin
    https://trello.com/c/kTgD0zCO/1383-login-von-yourplatformio-aus

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+
+  # Allow access through our mobile app via the api.
+  #
   allow do
     origins '*'
 
@@ -6,5 +9,19 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # https://github.com/neroniaky/angular-token/wiki/Common-Problems
     #
     resource '/api/*', headers: :any, methods: [:get, :post, :options], expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
+  end
+
+  # Allow sign-in from external forms.
+  # The domains of the websites the exteral sign-in forms are on
+  # need to go into this environment variable:
+  #
+  #     EXTERNAL_SIGN_IN_FORM_DOMAINS="example.com,example.org"
+  #
+  if ENV['EXTERNAL_SIGN_IN_FORM_DOMAINS']
+    allow do
+      origins ENV['EXTERNAL_SIGN_IN_FORM_DOMAINS'].split(",")
+
+      resource '*'
+    end
   end
 end
