023d7b039c23c0df9b20b8950e417717eb81fb4d
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-09-13 15:53:27 +0200

    removing whitespace (no changes)

--- app/models/concerns/group_dummy_users.rb
+++ app/models/concerns/group_dummy_users.rb
@@ -1,32 +1,32 @@
 concern :GroupDummyUsers do
-  
+
   def generate_dummy_users(number_of_users = 10)
     number_of_users.times { generate_dummy_user }
   end
-  
+
   def generate_dummy_user
     Faker::Config.locale = Setting.preferred_locale || 'de'
-    
+
     user = User.new
     user.first_name = Faker::Name.first_name
     user.last_name = Faker::Name.last_name
     user.email = Faker::Internet.email
     user.date_of_birth = Faker::Date.between(from: 30.years.ago, to: 18.years.ago)
     user.save
-    
+
     user.profile_fields.create(label: :personal_title, type: "ProfileFields::General")
     user.profile_fields.create(label: :academic_degree, type: "ProfileFields::AcademicDegree")
-    
-    user.profile_fields.create label: :home_address, 
+
+    user.profile_fields.create label: :home_address,
       value: "#{Faker::Address.street_address}\n#{Faker::Address.zip} #{Faker::Address.city}\n#{Faker::Address.country}",
       type: "ProfileFields::Address"
     user.profile_fields.create label: :study_address,
       value: "#{Faker::Address.street_address}\n#{Faker::Address.zip} #{Faker::Address.city}\n#{Faker::Address.country}",
       type: "ProfileFields::Address"
-    user.profile_fields.create label: :phone, 
+    user.profile_fields.create label: :phone,
       value: Faker::PhoneNumber.phone_number,
       type: "ProfileFields::Phone"
-    user.profile_fields.create label: :mobile, 
+    user.profile_fields.create label: :mobile,
       value: Faker::PhoneNumber.cell_phone,
       type: "ProfileFields::Phone"
     user.profile_fields.create label: :fax,
@@ -39,5 +39,5 @@ concern :GroupDummyUsers do
     self.assign_user user, at: Faker::Date.between(from: 6.years.ago, to: 10.days.ago)
     return user
   end
-  
+
 end
