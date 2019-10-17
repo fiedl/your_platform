358eae3ec967557bc4082fdcad0cb244c21e80a5
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-10-17 14:40:09 +0200

    datepicker: setting input name
    https://trello.com/c/jYgMC6J9/1441-hausbewohnerverwaltung
    https://trello.com/c/jARL9U1c/1445-vue-datepicker

--- app/helpers/datepicker_helper.rb
+++ app/helpers/datepicker_helper.rb
@@ -1,8 +1,8 @@
 module DatepickerHelper
 
-  def datepicker_tag
+  def datepicker_tag(name)
     content_tag :div, id: "vue-datepicker-app", class: "vue-app" do
-      content_tag :datepicker, "", ":language" => "de", ":monday-first" => true, "format" => "dd.MM.yyyy", "value" => Date.today
+      content_tag :datepicker, "", ":language" => "de", ":monday-first" => true, "format" => "dd.MM.yyyy", "value" => Date.today, "name" => name
     end
   end
 
