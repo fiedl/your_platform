b58162dc228e7c6a7f59f18e3472dc147ca64c70
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-10-22 01:10:39 +0200

    providing `user_with_avatar` helper.
    The helper accepts also a block, which will be rendered as subtitle. This may be used for offices etc.
    
    https://trello.com/c/jYgMC6J9/1441-hausbewohnerverwaltung

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -12,14 +12,26 @@ module UsersHelper
 
   def link_to_user_with_phone_number(user)
     if user.phone.present?
-      link_to(user.title, user, class: 'user') + ", " + I18n.t(:fon) + ": " + content_tag(:span, user.phone, class: 'phone')
+      link_to(user.title, user, class: 'user') + ", " + user_phone_number(user)
     else
       link_to(user.title, user, class: 'user')
     end
   end
 
+  def user_phone_number(user)
+    (I18n.t(:fon) + ": " + content_tag(:span, user.phone, class: 'phone')).html_safe
+  end
+
   def link_to_user_with_avatar(user, path = nil)
     link_to(user_avatar(user) + " " + content_tag(:span, user.title, class: 'user_title'), path || user_path(user))
+  end
+
+  def user_with_avatar(user, &block)
+    content_tag :span, class: 'user_with_avatar' do
+      user_avatar(user) + content_tag(:div) do
+        link_to_user(user) + (content_tag(:div, class: 'subtitle', &block) if block_given?)
+      end
+    end
   end
 
 end
