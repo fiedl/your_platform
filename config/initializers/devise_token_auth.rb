5b90804667d9e62fadfd6a0f4cc13dda35240c75
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-07-01 01:18:56 +0200

    api auth: try to utilize devise-token-auth batch requests
    because not changing the access token with each request leads to empty-string access tokens
    
    https://trello.com/c/jVZR1o1V/67-fix-zufälliges-ausloggen

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -5,7 +5,7 @@ DeviseTokenAuth.setup do |config|
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
   # each request.
-  config.change_headers_on_each_request = false
+  # config.change_headers_on_each_request = true
 
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
@@ -19,7 +19,7 @@ DeviseTokenAuth.setup do |config|
   # time. In this case, each request in the batch will need to share the same
   # auth token. This setting determines how far apart the requests can be while
   # still using the same auth token.
-  # config.batch_request_buffer_throttle = 5.seconds
+  config.batch_request_buffer_throttle = 1.minute
 
   # This route will be the prefix for all oauth2 redirect callbacks. For
   # example, using the default '/omniauth', the github oauth2 provider will
