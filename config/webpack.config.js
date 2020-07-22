763f17e4d59fdc80805cd267cdb3af9ef1b96733
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-07-22 02:05:15 +0200

    vue: adding erb loader
    (but does not work for helpers as expected so far)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
       {
         test: /\.coffee$/,
         use: [ 'coffee-loader' ]
+      },
+      {
+        test: /\.erb$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: [{
+          loader: 'rails-erb-loader',
+          options: {
+            runner: (/^win/.test(process.platform) ? 'ruby ' : '') + 'bin/rails runner'
+          }
+        }]
       }
     ]
   },
