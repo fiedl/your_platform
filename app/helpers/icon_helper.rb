eb2a3defe68f2a878fc86bf439f0a23c0dacdc0b
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-08-10 00:44:46 +0200

    tabler public website: social icons

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -320,4 +320,28 @@ module IconHelper
     }.html_safe
   end
 
+  def facebook_icon
+    %q{
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z"/>
+        <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+      </svg>
+    }.html_safe
+  end
+
+  def instagram_icon
+    %q{
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z"/>
+        <rect x="4" y="4" width="16" height="16" rx="4" />
+        <circle cx="12" cy="12" r="3" />
+        <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+      </svg>
+    }.html_safe
+  end
+
+  def youtube_icon
+    fa_icon :youtube
+  end
+
 end
--- app/models/concerns/group_profile.rb
+++ app/models/concerns/group_profile.rb
@@ -4,4 +4,16 @@ concern :GroupProfile do
     phone_profile_fields.first
   end
 
+  def facebook_url
+    profile_fields.where(label: "Facebook", type: "ProfileFields::Homepage").first.try(:value)
+  end
+
+  def youtube_url
+    profile_fields.where(label: "YouTube", type: "ProfileFields::Homepage").first.try(:value)
+  end
+
+  def instagram_url
+    profile_fields.where(label: "Instagram", type: "ProfileFields::Homepage").first.try(:value)
+  end
+
 end
