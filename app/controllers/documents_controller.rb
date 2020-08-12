1810fb7ba5d57881c95f316ae0a8d73dd06ce894
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-08-13 01:53:27 +0200

    documents#index by category
    also considering folder categories and document titles.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,13 +2,18 @@ class DocumentsController < ApplicationController
 
   expose :documents, -> {
     docs = current_user.documents_of_interest(limit: 50)
-    docs = docs.tagged(params[:tags]) if params[:tags].present?
-    docs
+    # docs = docs.tagged(params[:tags]) if params[:tags].present?
+    docs = docs.by_categories(params[:tags]) if params[:tags].present?
+    docs = docs.select { |doc| can? :read, doc }
+    docs = docs.sort_by { |doc| - doc.created_at.to_i }
+    docs = docs
   }
 
   def index
     authorize! :index, :documents
     set_current_tab :documents
+    set_current_title t :documents
+    set_current_title params[:tags].join(" & ") if params[:tags].present?
   end
 
   def new
--- app/models/concerns/document_tags.rb
+++ app/models/concerns/document_tags.rb
@@ -9,4 +9,14 @@ concern :DocumentTags do
     tag_list.add tag
   end
 
+  class_methods do
+    def by_categories(categories)
+      matching_parent_pages = Page.where("title like ?", "%#{categories.join('%')}%").pluck(:id)
+      matching_parent_pages_descendants = Page.where("title like ?", "%#{categories.join('%')}%").map(&:descendant_page_ids).flatten
+      tagged(categories) +
+      where("title like ?", "%#{categories.join('%')}%") +
+      where(parent_type: 'Page', parent_id: matching_parent_pages + matching_parent_pages_descendants)
+    end
+  end
+
 end
