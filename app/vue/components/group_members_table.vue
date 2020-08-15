56f344a695b19e843b292301daee74dc09bfb390
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-08-15 13:52:02 +0200

    fix group_members#index sorting
    due to vue-good-table rendering
    
    https://xaksis.github.io/vue-good-table/guide/advanced/#custom-row-template

--- app/vue/components/group_members_table.vue
+++ app/vue/components/group_members_table.vue
@@ -6,7 +6,21 @@
       :sort-options="sort_options"
       :search-options="search_options"
       styleClass="members child_users display table card-table table-vcenter"
-    ></vue-good-table>
+    >
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'last_name'">
+          <a :href="props.row.href" v-if="props.row.href">{{props.row.last_name}}</a>
+          <span v-if="!props.row.href">{{props.row.last_name}}</span>
+        </span>
+        <span v-else-if="props.column.field == 'status'">
+          <a :href="'/groups/' + props.row.status_group_id + '/members'">{{props.row.status}}</a>
+        </span>
+        <span v-else-if="props.column.field == 'direct_group'">
+          <a :href="'/groups/' + props.row.direct_group_id + '/members'">{{props.row.direct_group_name}}</a>
+        </span>
+        <span v-else v-html="props.formattedRow[props.column.field]"></span>
+      </template>
+    </vue-good-table>
   </div>
 </template>
 
@@ -24,11 +38,11 @@
       query: null
       columns: [
         { label: "", field: 'avatar', html: true, tdClass: 'w-1' }
-        { label: this.translate('last_name'), field: 'last_name_link', html: true }
+        { label: this.translate('last_name'), field: 'last_name' }
         { label: this.translate('first_name'), field: 'first_name' }
         { label: this.translate('name_affix'), field: 'name_affix' }
-        { label: this.translate('status'), field: 'status_link', html: true, hidden: (! this.has_status_entries()) }
-        { label: this.translate('group'), field: 'direct_group_link', html: true, hidden: (! this.has_direct_group_entries()) || this.has_status_entries() }
+        { label: this.translate('status'), field: 'status', hidden: (! this.has_status_entries()) }
+        { label: this.translate('group'), field: 'direct_group', hidden: (! this.has_direct_group_entries()) || this.has_status_entries() }
         { label: this.translate('since'), field: 'since', type: 'date', dateOutputFormat: 'dd.MM.yyyy', firstSortType: 'desc', dateInputFormat: 'dd.MM.yyyy' }
       ]
       sort_options:
@@ -61,15 +75,6 @@
           row.since = component.format_date(row.joined_at)
           if row.avatar_path
             row.avatar = "<span class=\"avatar\" style=\"background-image: url(#{row.avatar_path})\"></span>"
-          if row.last_name
-            if row.href
-              row.last_name_link = "<a href=\"#{row.href}\">#{row.last_name}</a>"
-            else
-              row.last_name_link = row.last_name
-          if row.status
-            row.status_link = "<a href=\"/groups/#{row.status_group_id}/members\">#{row.status}</a>"
-          if row.direct_group_id
-            row.direct_group_link = "<a href=\"/groups/#{row.direct_group_id}/members\">#{row.direct_group_name}</a>"
           row
       search_options: ->
         enabled: true
