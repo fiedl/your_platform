b5d6414c20318312eb4bed12dc0a8f68abbcde80
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-08-16 17:52:13 +0200

    fixing post delivery job

--- app/jobs/deliver_post_via_email_job.rb
+++ app/jobs/deliver_post_via_email_job.rb
@@ -3,6 +3,6 @@ class DeliverPostViaEmailJob < ApplicationJob
 
   def perform(post_id:, recipient_user_id:)
     recipient = User.find recipient_user_id
-    PostDelivery.where(post_id: post.id, user_id: recipient_user.id).first_or_create.deliver_if_due
+    PostDelivery.where(post_id: post_id, user_id: recipient_user_id).first_or_create.deliver_if_due
   end
 end
