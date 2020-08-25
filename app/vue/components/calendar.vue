29f81ae50240e9cbd871156936c2bf360b7555df
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-08-25 23:57:45 +0200

    copy over calendar component from
    https://github.com/z3rone/your_plattform_calendar/blob/master/src/components/calendar.vue
    
    credit: @z3rone
    
    https://trello.com/c/KN8Rowcv/1480-vue-kalender

--- app/vue/components/calendar.vue
+++ app/vue/components/calendar.vue
@@ -3,7 +3,14 @@
     <div class="col-md-9">
       <div class="card">
         <div class="card-body">
-          Coming soon ...
+          <vue-full-calendar
+            class='demo-app-calendar'
+            :options='calendar_options'>
+            <template v-slot:eventContent='arg'>
+              <b>{{ arg.timeText }}</b>
+              <i>{{ arg.event.title }}</i>
+            </template>
+          </vue-full-calendar>
         </div>
       </div>
     </div>
@@ -18,11 +25,68 @@
 </template>
 
 <script>
+  import FullCalendar from '@fullcalendar/vue'
+  import dayGridPlugin from '@fullcalendar/daygrid'
+  import timeGridPlugin from '@fullcalendar/timegrid'
+  import interactionPlugin from '@fullcalendar/interaction'
+  import Vue from 'vue'
+
+  Vue.component('vue-full-calendar', FullCalendar)
+
   export default {
     props: ['calendars', 'timezone'],
     data() { return {
-      current_calendars: this.calendars
-    } }
+      current_calendars: this.calendars,
+      calendar_options: {
+        plugins: [
+          dayGridPlugin,
+          timeGridPlugin,
+          //interactionPlugin // needed for dateClick
+        ],
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        initialView: 'dayGridMonth',
+        initialEvents: this.initial_events, //[], //INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+        editable: true,
+        selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true,
+        weekends: true,
+        //select: this.handleDateSelect,
+        //eventClick: this.handleEventClick,
+        //eventsSet: this.handleEvents
+        /* you can update a remote database when these fire:
+        eventAdd:
+        eventChange:
+        eventRemove:
+        */
+      }
+    } },
+    created() {
+
+    },
+    methods: {
+    },
+    computed: {
+      initial_events() {
+        let events = [];
+        for(let calendar of this.calendars) {
+          for(let e of calendar.events) {
+            events[e.id] = events[e.id] || e;
+            // Add calendars
+            if(!events[e.id].calendarIds) {
+              events[e.id].calendarIds = [];
+            }
+            events[e.id].calendarIds.push(calendar.id); // Calendars it belongs to
+            events[e.id]['title'] = e.name; // Rename according to the calendar
+          }
+        }
+        return events
+      }
+    }
   }
 </script>
 
