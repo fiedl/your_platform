8d35f388f51f2fd6e9cc92bf13651f11273673ef
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-08-27 20:12:15 +0200

    root#index: include event posts in posts

--- app/models/concerns/user_posts.rb
+++ app/models/concerns/user_posts.rb
@@ -8,20 +8,23 @@ concern :UserPosts do
     posts_from_me.draft
   end
 
+  # This method returns
+  #
+  # 1. Posts that are direclty shared with me by being a child_user of the post.
+  # 2. Posts that are by me by the author_user_id attribute.
+  # 3. Posts that are child_posts of my groups.
+  # 4. Posts that are posts of my groups. (Legacy)
+  # 5. Posts in events in my groups.
+  #
   def posts
-    # This query does not look nice, but is much more efficient.
-    #
-    # 1. Posts that are direclty shared with me by being a child_user of the post.
-    # 2. Posts that are by me by the author_user_id attribute.
-    # 3. Posts that are child_posts of my groups.
-    # 4. Posts that are posts of my groups. (Legacy)
-    #
     Post.where(id: child_posts).or(
       Post.where(id: posts_from_me)
     ).or(
       Post.where(id: Post.joins(:parent_groups).where(groups: {id: self.groups}))
     ).or(
       Post.where(group_id: self.groups)
+    ).or(
+      Post.where(id: Post.joins(:parent_events).merge(Event.where(group_id: self.groups)))
     )
   end
 
