a39eb26f3b8f27eaf4f3f02831343e891b6e7fce
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-09-02 00:34:25 +0200

    blogs: fixing "This action failed the check_authorization because it does not authorize_resource"

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < PagesController #ApplicationController
 
   def show
+    authorize! :read, page
+
     @blog_root_page = @blog ||= page
     @blog_posts = @blog.blog_entries.visible_to(current_user)
     @tags = @blog_posts.collect { |entry| entry.tags }.flatten.uniq.sort_by(&:taggings_count).reverse
@@ -10,10 +12,12 @@ class BlogsController < PagesController #ApplicationController
   end
 
   def update
+    authorize! :update, page
+
     params[:page] ||= params[:blog]
     params[:blog] ||= params[:page]
     @page ||= @blog
-    @blog ||= ppage
+    @blog ||= page
     super
   end
 
