9191b512cce9a49c41b7819a0f7aeee6af40b925
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-09-01 12:56:53 +0200

    `ProfileFields::Address` are free-text fields now.
    Remove accessors to sub fields.
    
    https://trello.com/c/ATtehkIl/1485-beta-migration

--- app/models/profile_fields/address.rb
+++ app/models/profile_fields/address.rb
@@ -19,133 +19,54 @@ module ProfileFields
       "ADR"
     end
 
-    concerning :SubFields do
-      included do
-        # has_child_profile_fields :first_address_line, :second_address_line, :postal_code, :city, :region, :country_code
-        has_child_profile_fields # TODO: Remove this after converting to free-text fields
+    def street_with_number
+      geo_information(:street)
+    end
 
-        def street_with_number
-          self.get_field(:first_address_line) || geo_information(:street)
-        end
+    def country
+      Biggs.country_names[country_code] if country_code
+    end
 
-        def country
-          Biggs.country_names[country_code] if country_code
-        end
+    def country_if_not_default
+      country if country_code != default_country_code
+    end
 
-        def country_if_not_default
-          country if country_code != default_country_code
-        end
+    def country_code
+      code = geo_information(:country_code) || default_country_code
+      code.present? ? code.to_s.downcase : nil
+    end
 
-        def country_code
-          code = self.get_field(:country_code) || geo_information(:country_code) || default_country_code
-          code.present? ? code.to_s.downcase : nil
-        end
+    def default_country_code
+      self.class.default_country_code
+    end
 
-        def default_country_code
-          self.class.default_country_code
-        end
+    def city
+      geo_information(:city)
+    end
+    def town
+      city
+    end
 
-        def city
-          self.get_field(:city) || geo_information(:city)
-        end
-        def town
-          city
-        end
+    def postal_code
+      geo_information(:postal_code)
+    end
 
-        def postal_code
-          self.get_field(:postal_code) || geo_information(:postal_code)
-        end
+    def plz
+      postal_code if country_code.try(:downcase) == 'de'
+    end
 
-        def plz
-          postal_code if country_code.try(:downcase) == 'de'
-        end
-
-        def province
-          region
-        end
-        def state
-          region
-        end
-        def region
-          self.get_field(:region) || geo_information(:state)
-        end
-        def state_shortcut(str)
-          GeoLocation.usa_state_shortcuts.each { |k, v| str.sub!(k, v) } if str.present?
-          str
-        end
-
-        def composed_address
-          first_and_second_address_line = (get_field(:first_address_line).to_s + "\n" + get_field(:second_address_line).to_s).strip
-          Biggs::Formatter.new(blank_country_on: default_country_code).format(
-            get_field(:country_code),
-            street: first_and_second_address_line,
-            city: get_field(:city),
-            zip: get_field(:postal_code),
-            state: state_shortcut(get_field(:region))
-          ).strip
-        end
-
-        def convert
-          convert_to_free_text_format
-        end
-
-        def convert_to_free_text_format
-          self.value = self.composed_address if self.composed_address.present?
-          # self.save
-          # self.children.destroy_all
-        end
-
-
-        # # First, we had all address fields store as free-text value.
-        # # This method migrates this format to the new one where street, city etc.
-        # # are stored as separate child profile fields.
-        # #
-        # def convert_to_format_with_separate_fields
-        #   # p "converting profile field #{id} of #{profileable.try(:class).try(:name)} #{profileable.try(:id)} ..."
-        #
-        #   old_value = self.value
-        #
-        #   if self.geo_information(:street).present?
-        #     self.first_address_line = self.geo_information(:street)
-        #     self.city = self.geo_information(:city)
-        #     self.postal_code = self.geo_information(:postal_code)
-        #     self.country_code = self.geo_information(:country_code)
-        #   else
-        #     # If we haven't processed this field already.
-        #     #
-        #     unless self.get_field(:first_address_line).present?
-        #       # If we can't extract the street name, copy the whole address into
-        #       # the second address line field. Otherwise, we'd lose this information
-        #       # in the ui.
-        #       #
-        #       self.second_address_line = self.read_attribute(:value)
-        #       self.add_flag :needs_review
-        #     end
-        #   end
-        #
-        #   # For foreign addresses, better include the state/region field, since
-        #   # we do not know if this field is needed there.
-        #   #
-        #   if self.geo_information(:country_code).present? && self.geo_information(:country_code).try(:downcase) != default_country_code.try(:downcase)
-        #     self.region = self.geo_information(:state)
-        #   end
-        #
-        #   self.save
-        #
-        #   if old_value && self.value && [old_value, self.value].collect { |v|
-        #       v.gsub("traße", "tr.").gsub("\n", "").gsub(",", "").gsub(" ", "")
-        #     }.uniq.count == 2
-        #     # The old and the new value differ. This could simply mean that
-        #     # "Frankreich" has been replaced by "France". But it could also mean
-        #     # that the address is not readable anymore. In any case, it should
-        #     # be checked manually.
-        #     #
-        #     self.add_flag :needs_review
-        #   end
-        #
-        #   return self
-        # end
-      end
+    def province
+      region
+    end
+    def state
+      region
+    end
+    def region
+      geo_information(:state)
+    end
+    def state_shortcut(str)
+      GeoLocation.usa_state_shortcuts.each { |k, v| str.sub!(k, v) } if str.present?
+      str
     end
 
     def self.default_country_code
