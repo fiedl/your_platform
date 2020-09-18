c27664172525dda7c98689a50c92c922f5e6a36f
Sebastian Fiedlschuster <github@fiedlschuster.de>
2020-09-18 15:10:01 +0200

    fixing platform news feed

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -24,7 +24,7 @@ class RootController < ApplicationController
 
   expose :platform_news_group, -> { Group.where(name: "PlattformBlog.com").first }
   expose :platform_news_posts, -> {
-    Post.where(id: platform_news_group.posts.published.where("published_at is null or published_at > ?", 1.year.ago).order(sticky: :asc, updated_at: :desc).limit(10).select { |post| post.parent_groups.count == 1 }.map(&:id))
+    Post.where(id: platform_news_group.posts.published.where("published_at is null or published_at > ?", 1.year.ago).order(sticky: :asc, published_at: :desc).limit(10).select { |post| post.parent_groups.count == 1 }.map(&:id)).order(sticky: :asc, published_at: :desc)
   }
   expose :platform_news_draft, -> {
     current_user.drafted_posts.where(sent_via: platform_news_sent_via_key).order(created_at: :desc).first_or_create  do |post|
